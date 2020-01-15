5a678a09f4523194da7f8a9cb3ce0594ac3e85d0
Smith <Devin.Smith@quartech.com>
2020-01-15 12:19:30 -0800

    temporarily use forked repo for openshift config.

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,6 +1,6 @@
 export PROJECT_NAMESPACE="jcxjin"
 
-export GIT_URI="https://github.com/bcgov/PIMS.git"
+export GIT_URI="https://github.com/devinleighsmith/PIMS.git"
 export GIT_REF="master"
 
 # The templates that should not have their GIT references (uri and ref) over-ridden
