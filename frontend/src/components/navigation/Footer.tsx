9ab0ad19feb042031e590f11d205d093b43edaca
Smith <Devin.Smith@quartech.com>
2020-01-21 18:13:42 -0800

    PIMS-322 update landing page.

---
+++ frontend/src/components/navigation/Footer.tsx
@@ -0,0 +1,20 @@
+import './Footer.scss';
+import React from 'react';
+
+function Footer() {
+
+  return (
+    <footer className="footer">
+      <div className="content">
+        <a href="http://www.gov.bc.ca/gov/content/home/disclaimer">Disclaimer</a>
+        <a href="http://www.gov.bc.ca/gov/content/home/privacy">Privacy</a>
+        <a href="http://www.gov.bc.ca/gov/content/home/accessible-government">Accessibility</a>
+        <a href="http://www.gov.bc.ca/gov/content/home/copyright">Copyright</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
+
