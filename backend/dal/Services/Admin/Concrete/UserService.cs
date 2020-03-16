e57dde2a6ca2b586842ba8c1414c2cc814cfa6d9
fosol <jeremymfoster@hotmail.com>
2020-03-16 08:46:09 -0700

    PIMS-977 Adding /api/admin/users endpoint.

--- backend/dal/Services/Admin/Concrete/UserService.cs
+++ backend/dal/Services/Admin/Concrete/UserService.cs
@@ -31,18 +31,48 @@ namespace Pims.Dal.Services.Admin
         #region Methods
         /// <summary>
         /// Get a page of users from the datasource.
+        /// The filter will allow queries to search for anything that starts with the following properties; DisplayName, FirstName, LastName, Email, Agencies.
         /// </summary>
         /// <param name="page"></param>
         /// <param name="quantity"></param>
-        /// <param name="sort"></param>
         /// <returns></returns>
-        public Paged<User> GetNoTracking(int page = 1, int quantity = 10, string sort = null)
+        public Paged<User> GetNoTracking(int page = 1, int quantity = 10)
+        {
+            return GetNoTracking(new UserFilter(page, quantity));
+        }
+
+        /// <summary>
+        /// Get a page of users from the datasource.
+        /// The filter will allow queries to search for the following property values; DisplayName, FirstName, LastName, Email, Agencies.
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public Paged<User> GetNoTracking(UserFilter filter = null)
         {
             this.User.ThrowIfNotAuthorized("system-administrator");
 
+            if (filter.Page < 1) filter.Page = 1;
+            if (filter.Quantity < 1) filter.Quantity = 1;
+            if (filter.Quantity > 50) filter.Quantity = 50;
+
             var query = this.Context.Users.AsNoTracking();
-            var users = query.Skip((page - 1) * quantity).Take(quantity);
-            return new Paged<User>(users.ToArray(), page, quantity, query.Count());
+            if (filter != null)
+            {
+                if (!string.IsNullOrWhiteSpace(filter.DisplayName))
+                    query = query.Where(u => EF.Functions.Like(u.DisplayName, $"{filter.DisplayName}"));
+                if (!string.IsNullOrWhiteSpace(filter.FirstName))
+                    query = query.Where(u => EF.Functions.Like(u.FirstName, $"{filter.FirstName}"));
+                if (!string.IsNullOrWhiteSpace(filter.LastName))
+                    query = query.Where(u => EF.Functions.Like(u.LastName, $"{filter.LastName}"));
+                if (!string.IsNullOrWhiteSpace(filter.Email))
+                    query = query.Where(u => EF.Functions.Like(u.Email, $"{filter.Email}"));
+                if (filter.Agencies?.Any() == true)
+                    query = query.Where(u => u.Agencies.Any(a => filter.Agencies.Contains(a.AgencyId)));
+                if (filter.Sort?.Any() == true)
+                    query = query.OrderByProperty(filter.Sort);
+            }
+            var users = query.Skip((filter.Page - 1) * filter.Quantity).Take(filter.Quantity);
+            return new Paged<User>(users.ToArray(), filter.Page, filter.Quantity, query.Count());
         }
 
         /// <summary>
