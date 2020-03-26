2870ae3d12bcc469c6967a491bc276bdeae685b5
Alejandro Sanchez <emailforasr@gmail.com>
2020-03-26 06:08:19 -0700

    PIMS-599 Map Filter
    - add building blocks for frontend forms
    - align frontend service calls with backend models
    - add icon button component

---
+++ frontend/src/components/common/form/Field.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import { classSet } from 'utils';
+import { DisplayError } from './DisplayError';
+
+type FieldProps = {
+  /** The field name (optional) */
+  field?: string;
+  /** An optional label used above the input element */
+  label?: string;
+  /** An optional element to render next to the label  */
+  afterLabel?: React.ReactNode;
+  /** A custom class to add to the <Form.Group> element of the <Field> component */
+  className?: string;
+  /** Whether the field is required. Shows an asterisk after the label. */
+  required?: boolean;
+};
+
+export const Field: React.FC<FieldProps> = ({
+  field,
+  label,
+  afterLabel,
+  className,
+  required,
+  children,
+  ...rest
+}) => {
+  // dynamically add CSS class "required" to form group
+  const cssClassNames = classSet({
+    required: !!required,
+    [className!]: className,
+  });
+  const controlId = !!field ? `input-${field}` : undefined;
+
+  return (
+    <Form.Group controlId={controlId} className={cssClassNames}>
+      {!!label && (
+        <Form.Label>
+          {label}
+          {afterLabel}
+        </Form.Label>
+      )}
+      {children}
+      <DisplayError field={field} />
+    </Form.Group>
+  );
+};
