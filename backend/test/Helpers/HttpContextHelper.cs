bc7b01cce729b9898b63824a146b60b5115c7a5c
Jeremy Foster <jeremymfoster@hotmail.com>
2020-03-26 15:08:24 -0700

    PIMS-1296 Adding unit tests (#87)

--- backend/test/Helpers/HttpContextHelper.cs
+++ backend/test/Helpers/HttpContextHelper.cs
@@ -1,6 +1,9 @@
+using System;
 using System.IO;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Moq;
 
 namespace Pims.Api.Test.Helpers
 {
@@ -10,21 +13,40 @@ namespace Pims.Api.Test.Helpers
         /// Provides a quick way to create a new HttpContext and initialize it with the specified properties.
         /// </summary>
         /// <param name="user"></param>
+        /// <param name="uri"></param>
         /// <returns></returns>
-        public static DefaultHttpContext CreateHttpContext (ClaimsPrincipal user)
+        public static HttpContext CreateHttpContext(this TestHelper helper, ClaimsPrincipal user, Uri uri = null)
         {
-            var context = new DefaultHttpContext ()
-            {
-                User = user
-            };
-            context.Request.Scheme="http";
-            context.Request.Host=new HostString("localhost");
-            context.Request.Path=new PathString("/test");
-            context.Request.PathBase=new PathString("/");
-            context.Request.Body=new MemoryStream();
-            context.Request.QueryString=new QueryString("?");
-            context.Request.Headers["device-id"] = "20317";
-            return context;
+            var request = new Mock<HttpRequest>();
+            helper.AddSingleton(request);
+            request.Setup(m => m.Scheme).Returns(uri?.Scheme ?? "http");
+            request.Setup(m => m.Host).Returns(String.IsNullOrWhiteSpace(uri?.Host) ? new HostString("localhost") : new HostString(uri.Host));
+            request.Setup(m => m.Path).Returns(String.IsNullOrWhiteSpace(uri?.AbsolutePath) ? new PathString("/test") : new PathString(uri.AbsolutePath));
+            request.Setup(m => m.PathBase).Returns(new PathString("/"));
+            request.Setup(m => m.Body).Returns(new MemoryStream());
+            request.Setup(m => m.QueryString).Returns(String.IsNullOrWhiteSpace(uri?.Query) ? new QueryString("?") : new QueryString(uri.Query));
+
+            var header = new Mock<IHeaderDictionary>();
+            helper.AddSingleton(header);
+            header.Setup(m => m["device-id"]).Returns("20317");
+            header.Setup(m => m["X-RequestedWith"]).Returns("XMLHttpRequest");
+            request.Setup(m => m.Headers).Returns(header.Object);
+
+            var features = new Mock<IFeatureCollection>();
+            helper.AddSingleton(features);
+            features.Setup(m => m.Get<HttpRequest>()).Returns(request.Object);
+            features.Setup(m => m.Get<ClaimsPrincipal>()).Returns(user);
+
+            var context = new Mock<HttpContext>();
+            context.Setup(m => m.Features).Returns(features.Object);
+            context.Setup(m => m.Request).Returns(request.Object);
+            context.Setup(m => m.RequestServices).Returns(helper.Provider);
+
+            var contextAccess = new Mock<IHttpContextAccessor>();
+            helper.AddSingleton(contextAccess);
+            contextAccess.Setup(m => m.HttpContext).Returns(context.Object);
+
+            return context.Object;
         }
     }
 }
---
+++ backend/test/Helpers/PimsAssert.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using Pims.Api.Policies;
+using Pims.Dal.Security;
+using Xunit;
+
+namespace Pims.Api.Test.Helpers
+{
+    /// <summary>
+    /// PimsAssert static class, provides extension methods for asserting.
+    /// </summary>
+    public static class PimsAssert
+    {
+        /// <summary>
+        /// Assert that the specified 'attribute' contains the specified 'permissions'.
+        /// </summary>
+        /// <param name="attribute"></param>
+        /// <param name="permissions"></param>
+        public static void HasPermissions(this HasPermissionAttribute attribute, params Permissions[] permissions)
+        {
+            Assert.Equal(permissions.Select(p => (object)p).ToArray(), attribute.Arguments);
+        }
+
+        /// <summary>
+        /// Assert that the route template matches the specified 'template'.
+        /// </summary>
+        /// <param name="attribute"></param>
+        /// <param name="template"></param>
+        public static void HasTemplate(this RouteAttribute attribute, string template)
+        {
+            Assert.Equal(template, attribute.Template);
+        }
+
+        /// <summary>
+        /// Assert that the HTTP method template matches the specified 'template'.
+        /// </summary>
+        /// <param name="attribute"></param>
+        /// <param name="template"></param>
+        public static void HasTemplate(this HttpMethodAttribute attribute, string template = null)
+        {
+            if (template == null)
+                Assert.Null(attribute.Template);
+            else
+                Assert.Equal(template, attribute.Template);
+        }
+
+        /// <summary>
+        /// Assert the specified 'controller' has the the authorize attribute.
+        /// </summary>
+        /// <param name="controller"></param>
+        public static void HasAuthorize(this Type controller)
+        {
+            var authorize = controller.GetCustomAttribute<AuthorizeAttribute>();
+            Assert.NotNull(authorize);
+        }
+
+        /// <summary>
+        /// Assert that the specified 'controller' has the specified 'permissions'.
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="permissions"></param>
+        public static void HasPermissions(this Type controller, params Permissions[] permissions)
+        {
+            var attribute = controller?.GetCustomAttribute<HasPermissionAttribute>();
+            Assert.NotNull(attribute);
+            attribute?.HasPermissions(permissions);
+        }
+
+        /// <summary>
+        /// Assert the specified 'controller' has the specified route 'template'.
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="template"></param>
+        public static void HasRoute(this Type controller, string template)
+        {
+            var route = controller.GetCustomAttribute<RouteAttribute>();
+            Assert.NotNull(route);
+            route.HasTemplate(template);
+        }
+
+        /// <summary>
+        /// Assert the specified 'endpoint' has the specified method 'template'.
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="template"></param>
+        public static void HasMethod(this MethodInfo endpoint, HttpMethod method, string template)
+        {
+            HttpMethodAttribute attribute = method switch
+            {
+                HttpMethod.Post => endpoint.GetCustomAttribute<HttpPostAttribute>(),
+                HttpMethod.Put => endpoint.GetCustomAttribute<HttpPutAttribute>(),
+                HttpMethod.Delete => endpoint.GetCustomAttribute<HttpDeleteAttribute>(),
+                HttpMethod.Get => endpoint.GetCustomAttribute<HttpGetAttribute>(),
+                _ => endpoint.GetCustomAttribute<HttpGetAttribute>()
+            };
+            Assert.NotNull(attribute);
+            attribute.HasTemplate(template);
+        }
+
+        /// <summary>
+        /// Assert the specified 'endpoint' has the specified method 'template'.
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="template"></param>
+        public static void HasGet(this MethodInfo endpoint, string template = null)
+        {
+            endpoint.HasMethod(HttpMethod.Get, template);
+        }
+
+        /// <summary>
+        /// Assert the specified 'endpoint' has the specified method 'template'.
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="template"></param>
+        public static void HasPost(this MethodInfo endpoint, string template = null)
+        {
+            endpoint.HasMethod(HttpMethod.Post, template);
+        }
+
+        /// <summary>
+        /// Assert the specified 'endpoint' has the specified method 'template'.
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="template"></param>
+        public static void HasPut(this MethodInfo endpoint, string template = null)
+        {
+            endpoint.HasMethod(HttpMethod.Put, template);
+        }
+
+        /// <summary>
+        /// Assert the specified 'endpoint' has the specified method 'template'.
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="template"></param>
+        public static void HasDelete(this MethodInfo endpoint, string template = null)
+        {
+            endpoint.HasMethod(HttpMethod.Delete, template);
+        }
+
+        /// <summary>
+        /// Assert that the specified 'endpoint' has the specified 'permissions'.
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="permissions"></param>
+        public static void HasPermissions(this MethodInfo endpoint, params Permissions[] permissions)
+        {
+            var attribute = endpoint?.GetCustomAttribute<HasPermissionAttribute>();
+            Assert.NotNull(attribute);
+            attribute?.HasPermissions(permissions);
+        }
+    }
+}
