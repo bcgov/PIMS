4452dd56ad1f022579c66751894f329dd4d96dae
Alejandro Sanchez <emailforasr@gmail.com>
2020-04-01 17:06:00 -0700

    PIMS-1407 GET - Properties (Parcels + Buildings)
    - replace `/api/parcels` calls with `/api/properties`
    - accommodate new type IBuilding. Add building popup
    - refactor map view to support buildings
    - fix API calls

---
+++ frontend/src/components/maps/PopupView.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { IBuilding, IParcel } from 'actions/parcelsActions';
+import { ParcelPopupView } from './ParcelPopupView';
+import { BuildingPopupView } from './BuildingPopupView';
+
+export type IPopupViewProps = {
+  propertyTypeId: 0 | 1; // 0 = Parcel, 1 = Building
+  propertyDetail: IParcel | IBuilding | null;
+};
+
+export const PopupView: React.FC<IPopupViewProps> = ({ propertyTypeId, propertyDetail }) => {
+  if (propertyTypeId === 0) {
+    return <ParcelPopupView parcel={propertyDetail as IParcel} />;
+  }
+  if (propertyTypeId === 1) {
+    return <BuildingPopupView building={propertyDetail as IBuilding} />;
+  }
+  return null;
+};
