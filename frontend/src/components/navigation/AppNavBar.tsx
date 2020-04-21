b2090c4263a594b532f305578b040f7426d62057
fosol <jeremymfoster@hotmail.com>
2020-04-21 16:32:54 -0700

    Cleaning up JSX

--- frontend/src/components/navigation/AppNavBar.tsx
+++ frontend/src/components/navigation/AppNavBar.tsx
@@ -7,50 +7,11 @@ import useKeycloakWrapper from 'hooks/useKeycloakWrapper';
 import { Claims } from 'constants/claims';
 
 /**
- * Nav bar with with user-related functionality.
+ * Nav bar with with role-based functionality.
  */
 function AppNavBar() {
   const keycloak = useKeycloakWrapper();
   const history = useHistory();
-
-  const SubmitProperty = keycloak.hasClaim(Claims.PROPERTY_ADD) ? (
-    <NavDropdown.Item onClick={() => history.push('/submitProperty')}>
-      Submit Property
-    </NavDropdown.Item>
-  ) : null;
-
-  const PropertyDropdown = keycloak.hasClaim(Claims.PROPERTY_VIEW) ? (
-    <NavDropdown title="Manage Property" id="manage-property-dropdown">
-      {SubmitProperty}
-      <NavDropdown.Item>View Inventory</NavDropdown.Item>
-    </NavDropdown>
-  ) : null;
-
-  const AdminDropdown = keycloak.isAdmin ? (
-    <NavDropdown title="Administration" id="administration">
-      <NavDropdown.Item onClick={() => history.push('/admin/users')}>Users</NavDropdown.Item>
-      <NavDropdown.Item onClick={() => history.push('/admin/access/requests')}>
-        Access Requests
-      </NavDropdown.Item>
-    </NavDropdown>
-  ) : null;
-
-  const ViewProjects = keycloak.hasClaim(Claims.PROPERTY_VIEW) ? (
-    <Nav.Link>View Projects</Nav.Link>
-  ) : null;
-
-  const DisposeRequest = keycloak.hasClaim(Claims.DISPOSE_REQUEST) ? (
-    <NavDropdown title="Start a Project" id="start-project-dropdown">
-      <NavDropdown.Item>Acquisition Project</NavDropdown.Item>
-      <NavDropdown.Item>Disposition Project</NavDropdown.Item>
-      <NavDropdown.Item>Request Exemption</NavDropdown.Item>
-    </NavDropdown>
-  ) : null;
-
-  const DisposeApprove = keycloak.hasClaim(Claims.DISPOSE_APPROVE) ? (
-    <Nav.Link>Approval Requests</Nav.Link>
-  ) : null;
-
   const displayName = keycloak.displayName || keycloak.firstName || 'default';
 
   return (
@@ -58,11 +19,11 @@ function AppNavBar() {
       <Navbar.Toggle aria-controls="collapse" className="navbar-dark" />
       <Nav>
         <Navbar.Collapse className="links">
-          {AdminDropdown}
-          {PropertyDropdown}
-          {ViewProjects}
-          {DisposeRequest}
-          {DisposeApprove}
+          <AdminDropdown />
+          <PropertyDropdown />
+          <ViewProjects />
+          <DisposeRequest />
+          <DisposeApprove />
         </Navbar.Collapse>
         <Nav.Item className="profile">
           <Image src={profileUrl} rounded />
@@ -81,6 +42,78 @@ function AppNavBar() {
       </Nav>
     </Navbar>
   );
+}
+
+/**
+ * Submit Property navigation dropdown item.
+ */
+function SubmitPropertyNav() {
+  const keycloak = useKeycloakWrapper();
+  const history = useHistory();
+  return keycloak.hasClaim(Claims.PROPERTY_ADD) ? (
+    <NavDropdown.Item onClick={() => history.push('/submitProperty')}>
+      Submit Property
+    </NavDropdown.Item>
+  ) : null;
+}
+
+/**
+ * Manage Property dropdown menu.
+ */
+function PropertyDropdown() {
+  const keycloak = useKeycloakWrapper();
+  return keycloak.hasClaim(Claims.PROPERTY_VIEW) ? (
+    <NavDropdown title="Manage Property" id="manage-property-dropdown">
+      <SubmitPropertyNav />
+      <NavDropdown.Item>View Inventory</NavDropdown.Item>
+    </NavDropdown>
+  ) : null;
+}
+
+/**
+ * Administration dropdown menu.
+ */
+function AdminDropdown() {
+  const keycloak = useKeycloakWrapper();
+  const history = useHistory();
+  return keycloak.isAdmin ? (
+    <NavDropdown title="Administration" id="administration">
+      <NavDropdown.Item onClick={() => history.push('/admin/users')}>Users</NavDropdown.Item>
+      <NavDropdown.Item onClick={() => history.push('/admin/access/requests')}>
+        Access Requests
+      </NavDropdown.Item>
+    </NavDropdown>
+  ) : null;
+}
+
+/**
+ * View Projects navigation menu link.
+ */
+function ViewProjects() {
+  const keycloak = useKeycloakWrapper();
+  return keycloak.hasClaim(Claims.PROPERTY_VIEW) ? <Nav.Link>View Projects</Nav.Link> : null;
+}
+
+/**
+ * Start a Project navigation dropdown menu.
+ */
+function DisposeRequest() {
+  const keycloak = useKeycloakWrapper();
+  return keycloak.hasClaim(Claims.DISPOSE_REQUEST) ? (
+    <NavDropdown title="Start a Project" id="start-project-dropdown">
+      <NavDropdown.Item>Acquisition Project</NavDropdown.Item>
+      <NavDropdown.Item>Disposition Project</NavDropdown.Item>
+      <NavDropdown.Item>Request Exemption</NavDropdown.Item>
+    </NavDropdown>
+  ) : null;
+}
+
+/**
+ * Approval Requests navigation menu link.
+ */
+function DisposeApprove() {
+  const keycloak = useKeycloakWrapper();
+  return keycloak.hasClaim(Claims.DISPOSE_APPROVE) ? <Nav.Link>Approval Requests</Nav.Link> : null;
 }
 
 export default AppNavBar;
