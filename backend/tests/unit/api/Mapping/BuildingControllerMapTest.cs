1113ae8141a9a3998931a8fc40e6c0e7e94d6f33
Jeremy Foster <jeremymfoster@hotmail.com>
2020-04-28 10:45:48 -0700

    PIMS-1625 Fixing /api/buildilng/{id} endpoint (#163)

---
+++ backend/tests/unit/api/Mapping/BuildingControllerMapTest.cs
@@ -0,0 +1,53 @@
+using Xunit;
+using System.Diagnostics.CodeAnalysis;
+using Pims.Core.Test;
+using Model = Pims.Api.Models.Building;
+
+namespace PimsApi.Test.Controllers
+{
+    [Trait("category", "unit")]
+    [Trait("category", "mapping")]
+    [Trait("group", "building")]
+    [ExcludeFromCodeCoverage]
+    public class BuildingControllerMapTest
+    {
+        #region Constructors
+        public BuildingControllerMapTest()
+        {
+        }
+        #endregion
+
+        #region Tests
+        #region UserInfo
+        [Fact]
+        public void BuildingModelMapping()
+        {
+            // Arrange
+            var helper = new TestHelper();
+            var mapper = helper.GetMapper();
+            var parcel = EntityHelper.CreateParcel(99);
+            var building = EntityHelper.CreateBuilding(parcel, 99);
+            building.Latitude = 1;
+            building.Longitude = 1;
+
+            // Act
+            var result = mapper.Map<Model.BuildingModel>(building);
+
+            // Assert
+            Assert.Equal(99, result.Id);
+            Assert.Equal(99, result.ParcelId);
+            Assert.Equal(1, result.Latitude);
+            Assert.Equal(1, result.Longitude);
+            Assert.Equal("description-99", result.Description);
+            Assert.Equal(1, result.StatusId);
+            Assert.Equal(1, result.ClassificationId);
+            Assert.Equal(1, result.AgencyId);
+            Assert.Equal(1, result.BuildingConstructionTypeId);
+            Assert.Equal(1, result.BuildingOccupantTypeId);
+            Assert.Equal(1, result.BuildingPredominateUseId);
+            // TODO: test all other properties.
+        }
+        #endregion
+        #endregion
+    }
+}
