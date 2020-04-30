b44fba8670995ab5952ec81f62aff2ec255f24f1
Jeremy Foster <jeremymfoster@hotmail.com>
2020-04-30 13:56:32 -0700

    PIMS-1427 Property List View Search (#174)

--- backend/api/Areas/Keycloak/Mapping/User/UserMap.cs
+++ backend/api/Areas/Keycloak/Mapping/User/UserMap.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using Mapster;
 using Model = Pims.Api.Areas.Keycloak.Models.User;
 using Entity = Pims.Dal.Entities;
@@ -26,15 +25,6 @@ namespace Pims.Api.Areas.Keycloak.Mapping.User
                 .Map(dest => dest.Roles, src => src.Roles)
                 .Inherits<Entity.BaseEntity, Api.Models.BaseModel>();
 
-            config.NewConfig<Entity.User, KModel.UserModel>()
-                .Map(dest => dest.Id, src => src.Id)
-                .Map(dest => dest.Username, src => src.Username)
-                .Map(dest => dest.FirstName, src => src.FirstName)
-                .Map(dest => dest.LastName, src => src.LastName)
-                .Map(dest => dest.Email, src => src.Email)
-                .Map(dest => dest.Enabled, src => !src.IsDisabled)
-                .Inherits<Entity.BaseEntity, Object>();
-
             config.NewConfig<Model.UserModel, Entity.User>()
                 .Map(dest => dest.Id, src => src.Id)
                 .Map(dest => dest.IsDisabled, src => src.IsDisabled)
@@ -49,6 +39,47 @@ namespace Pims.Api.Areas.Keycloak.Mapping.User
                 .Map(dest => dest.Agencies, src => src.Agencies)
                 .Map(dest => dest.Roles, src => src.Roles)
                 .Inherits<Api.Models.BaseModel, Entity.BaseEntity>();
+
+
+            config.NewConfig<Entity.User, KModel.UserModel>()
+                .Map(dest => dest.Id, src => src.Id)
+                .Map(dest => dest.Username, src => src.Username)
+                .Map(dest => dest.FirstName, src => src.FirstName)
+                .Map(dest => dest.LastName, src => src.LastName)
+                .Map(dest => dest.Email, src => src.Email)
+                .Map(dest => dest.Enabled, src => !src.IsDisabled)
+                .Inherits<Entity.BaseEntity, Object>();
+
+
+            config.NewConfig<Entity.User, Model.Update.UserModel>()
+                .Map(dest => dest.Id, src => src.Id)
+                .Map(dest => dest.IsDisabled, src => src.IsDisabled)
+                .Map(dest => dest.Username, src => src.Username)
+                .Map(dest => dest.Position, src => src.Position)
+                .Map(dest => dest.DisplayName, src => src.DisplayName)
+                .Map(dest => dest.FirstName, src => src.FirstName)
+                .Map(dest => dest.MiddleName, src => src.MiddleName)
+                .Map(dest => dest.LastName, src => src.LastName)
+                .Map(dest => dest.Email, src => src.Email)
+                .Map(dest => dest.Note, src => src.Note)
+                .Map(dest => dest.Agencies, src => src.Agencies)
+                .Map(dest => dest.Roles, src => src.Roles)
+                .Inherits<Entity.BaseEntity, Model.Update.BaseModel>();
+
+            config.NewConfig<Model.Update.UserModel, Entity.User>()
+                .Map(dest => dest.Id, src => src.Id)
+                .Map(dest => dest.IsDisabled, src => src.IsDisabled)
+                .Map(dest => dest.Username, src => src.Username)
+                .Map(dest => dest.Position, src => src.Position)
+                .Map(dest => dest.DisplayName, src => src.DisplayName)
+                .Map(dest => dest.FirstName, src => src.FirstName)
+                .Map(dest => dest.MiddleName, src => src.MiddleName)
+                .Map(dest => dest.LastName, src => src.LastName)
+                .Map(dest => dest.Email, src => src.Email)
+                .Map(dest => dest.Note, src => src.Note)
+                .Map(dest => dest.Agencies, src => src.Agencies)
+                .Map(dest => dest.Roles, src => src.Roles)
+                .Inherits<Model.Update.BaseModel, Entity.BaseEntity>();
         }
     }
 }
--- backend/dal/Migrations/Initial/PostDeploy/01-ViewProperties.sql
+++ backend/dal/Migrations/Initial/PostDeploy/01-ViewProperties.sql
@@ -1,71 +1,84 @@
+--DROP VIEW dbo.View_Properties
 -- This script provides a way to union both parcels and buildings into a single result of properties.
-CREATE VIEW View_Properties AS
+CREATE VIEW dbo.[View_Properties] AS
 SELECT
-    p.Id
-    , PropertyTypeId = 0
-    , p.StatusId
-    , Status = s.Name
-    , p.ClassificationId
-    , Classification = c.Name
-    , p.AgencyId
-    , Agency = a.Name
-    , p.AddressId
-    , Address = adr.Address1 + ' ' + adr.Address2
-    , City = ac.Name
-    , Province = ap.Name
-    , adr.Postal
-    , p.ProjectNumber
-    , p.Description
-    , p.Latitude
-    , p.Longitude
-    , p.IsSensitive
+    p.[Id]
+    , [PropertyTypeId] = 0
+    , p.[StatusId]
+    , [Status] = s.[Name]
+    , p.[ClassificationId]
+    , [Classification] = c.[Name]
+    , p.[AgencyId]
+    , [Agency] = ISNULL(pa.[Name], a.[Name])
+    , [AgencyCode] = ISNULL(pa.[Code], a.[Code])
+    , [SubAgency] = (
+        SELECT CASE WHEN pa.[Id] IS NOT NULL
+            THEN a.[Name]
+            ELSE NULL
+        END)
+    , [SubAgencyCode] = (
+        SELECT CASE WHEN pa.[Id] IS NOT NULL
+            THEN a.[Code]
+            ELSE NULL
+        END)
+    , p.[AddressId]
+    , [Address] = TRIM(ISNULL(adr.[Address1], '') + ' ' + ISNULL(adr.[Address2], ''))
+    , [City] = ac.[Name]
+    , [Province] = ap.[Name]
+    , adr.[Postal]
+    , p.[ProjectNumber]
+    , p.[Description]
+    , p.[Latitude]
+    , p.[Longitude]
+    , p.[IsSensitive]
 
     -- Parcel Properties
-    , p.PID
-    , p.PIN
-    , LandArea = p.LandArea
-    , LandLegalDescription = p.LandLegalDescription
-    , Municipality = p.Municipality
-    , Zoning = p.Zoning
-    , ZoningPotential = p.ZoningPotential
+    , p.[PID]
+    , p.[PIN]
+    , [LandArea] = p.[LandArea]
+    , [LandLegalDescription] = p.[LandLegalDescription]
+    , [Municipality] = p.[Municipality]
+    , [Zoning] = p.[Zoning]
+    , [ZoningPotential] = p.[ZoningPotential]
 
     -- Building Properties
-    , LocalId = null
-    , BuildingConstructionTypeId = 0
+    , [LocalId] = null
+    , [BuildingConstructionTypeId] = 0
     , BuildingConstructionType = null
-    , BuildingFloorCount = 0
-    , BuildingPredominateUseId = 0
+    , [BuildingFloorCount] = 0
+    , [BuildingPredominateUseId] = 0
     , BuildingPredominateUse = null
-    , BuildingOccupantTypeId = 0
+    , [BuildingOccupantTypeId] = 0
     , BuildingOccupantType = null
-    , BuildingTenancy = null
-    , RentableArea = 0
-    , LeaseExpiry = null
-    , OccupantName = null
-    , TransferLeaseOnSale = CAST(0 AS BIT)
+    , [BuildingTenancy] = null
+    , [RentableArea] = 0
+    , [LeaseExpiry] = null
+    , [OccupantName] = null
+    , [TransferLeaseOnSale] = CAST(0 AS BIT)
 
-    , Assessed = ISNULL(eas.[Value], 0)
-    , AssessedDate = eas.[Date]
-    , Appraised = ISNULL(eap.[Value], 0)
-    , AppraisedDate = eap.[Date]
-    , Estimated = ISNULL(fe.[Value], 0)
-    , EstimatedFiscalYear = fe.[FiscalYear]
-    , NetBook = ISNULL(fn.[Value], 0)
-    , NetBookFiscalYear = fn.[FiscalYear]
+    , [Assessed] = ISNULL(eas.[Value], 0)
+    , [AssessedDate] = eas.[Date]
+    , [Appraised] = ISNULL(eap.[Value], 0)
+    , [AppraisedDate] = eap.[Date]
+    , [Estimated] = ISNULL(fe.[Value], 0)
+    , [EstimatedFiscalYear] = fe.[FiscalYear]
+    , [NetBook] = ISNULL(fn.[Value], 0)
+    , [NetBookFiscalYear] = fn.[FiscalYear]
 FROM dbo.[Parcels] p
-JOIN dbo.[PropertyStatus] s ON p.StatusId = s.Id
-JOIN dbo.[PropertyClassifications] c ON p.ClassificationId = c.Id
-JOIN dbo.[Agencies] a ON p.AgencyId = a.Id
-JOIN dbo.[Addresses] adr ON p.AddressId = adr.Id
-JOIN dbo.[Cities] ac ON adr.CityId = ac.Id
-JOIN dbo.[Provinces] ap ON adr.ProvinceId = ap.Id
+JOIN dbo.[PropertyStatus] s ON p.[StatusId] = s.[Id]
+JOIN dbo.[PropertyClassifications] c ON p.[ClassificationId] = c.[Id]
+JOIN dbo.[Agencies] a ON p.[AgencyId] = a.[Id]
+LEFT JOIN dbo.[Agencies] pa ON a.[ParentId] = pa.[Id]
+JOIN dbo.[Addresses] adr ON p.[AddressId] = adr.[Id]
+JOIN dbo.[Cities] ac ON adr.[CityId] = ac.[Id]
+JOIN dbo.[Provinces] ap ON adr.[ProvinceId] = ap.[Id]
 OUTER APPLY (
     SELECT TOP 1
         [Value]
         , [Date]
     FROM dbo.[ParcelEvaluations]
-    WHERE [ParcelId] = p.Id
-        AND [Key] = 0 -- Assessed
+    WHERE [ParcelId] = p.[Id]
+        AND [Key] = 0 -- [Assessed]
     ORDER BY [Date] DESC
 ) AS eas
 OUTER APPLY (
@@ -73,7 +86,7 @@ OUTER APPLY (
         [Value]
         , [Date]
     FROM dbo.[ParcelEvaluations]
-    WHERE [ParcelId] = p.Id
+    WHERE [ParcelId] = p.[Id]
         AND [Key] = 1 -- Appraised
     ORDER BY [Date] DESC
 ) AS eap
@@ -82,7 +95,7 @@ OUTER APPLY (
         [Value]
         , [FiscalYear]
     FROM dbo.[ParcelFiscals]
-    WHERE [ParcelId] = p.Id
+    WHERE [ParcelId] = p.[Id]
         AND [Key] = 1 -- Estimated
     ORDER BY [FiscalYear] DESC
 ) AS fe
@@ -91,81 +104,93 @@ OUTER APPLY (
         [Value]
         , [FiscalYear]
     FROM dbo.[ParcelFiscals]
-    WHERE [ParcelId] = p.Id
+    WHERE [ParcelId] = p.[Id]
         AND [Key] = 0 -- NetBook
     ORDER BY [FiscalYear] DESC
 ) AS fn
 UNION
 SELECT
-    b.Id
-    , PropertyTypeId = 1
-    , b.StatusId
-    , Status = s.Name
-    , b.ClassificationId
-    , Classification = c.Name
-    , b.AgencyId
-    , Agency = a.Name
-    , b.AddressId
-    , Address = adr.Address1 + ' ' + adr.Address2
-    , City = ac.Name
-    , Province = ap.Name
-    , adr.Postal
-    , b.ProjectNumber
-    , b.Description
-    , b.Latitude
-    , b.Longitude
-    , b.IsSensitive
+    b.[Id]
+    , [PropertyTypeId] = 1
+    , b.[StatusId]
+    , [Status] = s.[Name]
+    , b.[ClassificationId]
+    , [Classification] = c.[Name]
+    , b.[AgencyId]
+    , [Agency] = ISNULL(pa.[Name], a.[Name])
+    , [AgencyCode] = ISNULL(pa.[Code], a.[Code])
+    , [SubAgency] = (
+        SELECT CASE WHEN pa.[Id] IS NOT NULL
+            THEN a.[Name]
+            ELSE NULL
+        END)
+    , [SubAgencyCode] = (
+        SELECT CASE WHEN pa.[Id] IS NOT NULL
+            THEN a.[Code]
+            ELSE NULL
+        END)
+    , b.[AddressId]
+    , [Address] = TRIM(ISNULL(adr.[Address1], '') + ' ' + ISNULL(adr.[Address2], ''))
+    , [City] = ac.[Name]
+    , [Province] = ap.[Name]
+    , adr.[Postal]
+    , b.[ProjectNumber]
+    , b.[Description]
+    , b.[Latitude]
+    , b.[Longitude]
+    , b.[IsSensitive]
 
     -- Parcel Properties
-    , PID = p.PID
-    , PIN = p.PIN
-    , LandArea = p.LandArea
-    , LandLegalDescription = p.LandLegalDescription
-    , Municipality = p.Municipality
-    , Zoning = p.Zoning
-    , ZoningPotential = p.ZoningPotential
+    , [PID] = p.[PID]
+    , [PIN] = p.[PIN]
+    , [LandArea] = p.[LandArea]
+    , [LandLegalDescription] = p.[LandLegalDescription]
+    , [Municipality] = p.[Municipality]
+    , [Zoning] = p.[Zoning]
+    , [ZoningPotential] = p.[ZoningPotential]
 
     -- Building Properties
-    , LocalId = b.LocalId
-    , BuildingConstructionTypeId = b.BuildingConstructionTypeId
-    , BuildingConstructionType = bct.Name
-    , BuildingFloorCount = b.BuildingFloorCount
-    , BuildingPredominateUseId = b.BuildingPredominateUseId
-    , BuildingPredominateUse = bpu.Name
-    , BuildingOccupantTypeId = b.BuildingOccupantTypeId
-    , BuildingOccupantType = bot.Name
-    , BuildingTenancy = b.BuildingTenancy
-    , RentableArea = b.RentableArea
-    , LeaseExpiry = b.LeaseExpiry
-    , OccupantName = b.OccupantName
-    , TransferLeaseOnSale = b.TransferLeaseOnSale
+    , [LocalId] = b.[LocalId]
+    , [BuildingConstructionTypeId] = b.[BuildingConstructionTypeId]
+    , BuildingConstructionType = bct.[Name]
+    , [BuildingFloorCount] = b.[BuildingFloorCount]
+    , [BuildingPredominateUseId] = b.[BuildingPredominateUseId]
+    , BuildingPredominateUse = bpu.[Name]
+    , [BuildingOccupantTypeId] = b.[BuildingOccupantTypeId]
+    , BuildingOccupantType = bot.[Name]
+    , [BuildingTenancy] = b.[BuildingTenancy]
+    , [RentableArea] = b.[RentableArea]
+    , [LeaseExpiry] = b.[LeaseExpiry]
+    , [OccupantName] = b.[OccupantName]
+    , [TransferLeaseOnSale] = b.[TransferLeaseOnSale]
 
-    , Assessed = ISNULL(eas.[Value], 0)
-    , AssessedDate = eas.[Date]
-    , Appraised = ISNULL(eap.[Value], 0)
-    , AppraisedDate = eap.[Date]
-    , Estimated = ISNULL(fe.[Value], 0)
-    , EstimatedFiscalYear = fe.[FiscalYear]
-    , NetBook = ISNULL(fn.[Value], 0)
-    , NetBookFiscalYear = fn.[FiscalYear]
+    , [Assessed] = ISNULL(eas.[Value], 0)
+    , [AssessedDate] = eas.[Date]
+    , [Appraised] = ISNULL(eap.[Value], 0)
+    , [AppraisedDate] = eap.[Date]
+    , [Estimated] = ISNULL(fe.[Value], 0)
+    , [EstimatedFiscalYear] = fe.[FiscalYear]
+    , [NetBook] = ISNULL(fn.[Value], 0)
+    , [NetBookFiscalYear] = fn.[FiscalYear]
 FROM dbo.[Buildings] b
-JOIN dbo.[Parcels] p ON b.ParcelId = p.Id
-JOIN dbo.[PropertyStatus] s ON b.StatusId = s.Id
-JOIN dbo.[PropertyClassifications] c ON b.ClassificationId = c.Id
-JOIN dbo.[Agencies] a ON b.AgencyId = a.Id
-JOIN dbo.[Addresses] adr ON b.AddressId = adr.Id
-JOIN dbo.[Cities] ac ON adr.CityId = ac.Id
-JOIN dbo.[Provinces] ap ON adr.ProvinceId = ap.Id
-JOIN dbo.[BuildingConstructionTypes] bct ON b.BuildingConstructionTypeId = bct.Id
-JOIN dbo.[BuildingOccupantTypes] bot ON b.BuildingOccupantTypeId = bot.Id
-JOIN dbo.[BuildingPredominateUses] bpu ON b.BuildingPredominateUseId = bpu.Id
+JOIN dbo.[Parcels] p ON b.[ParcelId] = p.[Id]
+JOIN dbo.[PropertyStatus] s ON b.[StatusId] = s.[Id]
+JOIN dbo.[PropertyClassifications] c ON b.[ClassificationId] = c.[Id]
+JOIN dbo.[Agencies] a ON b.[AgencyId] = a.[Id]
+LEFT JOIN dbo.[Agencies] pa ON a.[ParentId] = pa.[Id]
+JOIN dbo.[Addresses] adr ON b.[AddressId] = adr.[Id]
+JOIN dbo.[Cities] ac ON adr.[CityId] = ac.[Id]
+JOIN dbo.[Provinces] ap ON adr.[ProvinceId] = ap.[Id]
+JOIN dbo.[BuildingConstructionTypes] bct ON b.[BuildingConstructionTypeId] = bct.[Id]
+JOIN dbo.[BuildingOccupantTypes] bot ON b.[BuildingOccupantTypeId] = bot.[Id]
+JOIN dbo.[BuildingPredominateUses] bpu ON b.[BuildingPredominateUseId] = bpu.[Id]
 OUTER APPLY (
     SELECT TOP 1
         [Value]
         , [Date]
     FROM dbo.[ParcelEvaluations]
-    WHERE [ParcelId] = p.Id
-        AND [Key] = 0 -- Assessed
+    WHERE [ParcelId] = p.[Id]
+        AND [Key] = 0 -- [Assessed]
     ORDER BY [Date] DESC
 ) AS eas
 OUTER APPLY (
@@ -173,7 +198,7 @@ OUTER APPLY (
         [Value]
         , [Date]
     FROM dbo.[ParcelEvaluations]
-    WHERE [ParcelId] = p.Id
+    WHERE [ParcelId] = p.[Id]
         AND [Key] = 1 -- Appraised
     ORDER BY [Date] DESC
 ) AS eap
@@ -182,7 +207,7 @@ OUTER APPLY (
         [Value]
         , [FiscalYear]
     FROM dbo.[ParcelFiscals]
-    WHERE [ParcelId] = p.Id
+    WHERE [ParcelId] = p.[Id]
         AND [Key] = 1 -- Estimated
     ORDER BY [FiscalYear] DESC
 ) AS fe
@@ -191,8 +216,7 @@ OUTER APPLY (
         [Value]
         , [FiscalYear]
     FROM dbo.[ParcelFiscals]
-    WHERE [ParcelId] = p.Id
+    WHERE [ParcelId] = p.[Id]
         AND [Key] = 0 -- NetBook
     ORDER BY [FiscalYear] DESC
 ) AS fn
-
