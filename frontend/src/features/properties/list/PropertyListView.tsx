5639ee53784a599e1157d8af77f5954d6f3d98ee
Alejandro Sanchez <emailforasr@gmail.com>
2020-05-02 07:24:51 -0700

    PIMS-1472 Property List Pagination (#181)
    * Better typing support for react-table
    
    * Refactor table components
    
    * Add pagination control
    
    * Implement server pagination

--- frontend/src/features/properties/list/PropertyListView.tsx
+++ frontend/src/features/properties/list/PropertyListView.tsx
@@ -1,10 +1,10 @@
 import './PropertyListView.scss';
 
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useState, useMemo, useRef, useCallback } from 'react';
 import queryString from 'query-string';
 import CustomAxios from 'customAxios';
 import { ENVIRONMENT } from 'constants/environment';
-import Table from 'components/Table';
+import { Table } from 'components/Table';
 import { IPagedItems } from 'interfaces';
 import { IPropertyFilter, IProperty } from '.';
 import { columns as cols } from './columns';
@@ -18,27 +18,55 @@ const getPropertyListUrl = (filter: IPropertyFilter) =>
 const getPropertyReportUrl = (filter: IPropertyFilter) =>
   `${ENVIRONMENT.apiUrl}/reports/properties?${filter ? queryString.stringify(filter) : ''}`;
 
-const filter: IPropertyFilter = {
+const defaultFilter: IPropertyFilter = {
   page: 1,
-  quantity: 50,
+  quantity: 10,
   agencies: [1, 2, 3, 4, 5, 6],
 };
 
 const PropertyListView: React.FC = () => {
   const columns = useMemo(() => cols, []);
-  const [data, setData] = useState<IProperty[]>([]);
 
-  useEffect(() => {
-    const fetch = async () => {
-      try {
-        const result = await CustomAxios().get<IPagedItems<IProperty>>(getPropertyListUrl(filter));
-        setData(result.data.items);
-      } catch (e) {
-        setData([]);
+  // We'll start our table without any data
+  const [data, setData] = useState<IProperty[]>([]);
+  const [filter, setFilter] = useState<IPropertyFilter>({ ...defaultFilter });
+  // const [loading, setLoading] = useState(false);
+  const [pageCount, setPageCount] = useState(0);
+  const fetchIdRef = useRef(0);
+
+  // This will get called when the table needs new data
+  const fetchData = useCallback(
+    async ({ pageSize, pageIndex }: { pageSize: number; pageIndex: number }) => {
+      // Give this fetch an ID
+      const fetchId = ++fetchIdRef.current;
+
+      // TODO: Set the loading state
+      // setLoading(true);
+
+      // Only update the data if this is the latest fetch
+      if (fetchId === fetchIdRef.current) {
+        const newFilter = {
+          ...filter,
+          page: pageIndex + 1,
+          quantity: pageSize,
+        };
+
+        const response = await CustomAxios().get<IPagedItems<IProperty>>(
+          getPropertyListUrl(newFilter),
+        );
+
+        // The server could send back total page count.
+        // For now we'll just calculate it.
+        setData(response.data.items);
+        setPageCount(Math.ceil(response.data.total / pageSize));
+        setFilter(newFilter);
+
+        // setLoading(false);
       }
-    };
-    fetch();
-  }, []);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
 
   const dispatch = useDispatch();
 
@@ -67,31 +95,23 @@ const PropertyListView: React.FC = () => {
         </Container>
       </Container>
       <div className="ScrollContainer">
-        <Container fluid className="content-heading">
+        <Container fluid className="TableToolbar">
           <h3 className="mr-auto">Results</h3>
           <Button className="mr-2" onClick={() => fetch('excel')}>
             Export as Excel
           </Button>
           <Button onClick={() => fetch('csv')}>Export as CSV</Button>
         </Container>
-        <Table columns={columns} data={data} />
+        <Table<IProperty>
+          name="propertiesTable"
+          columns={columns}
+          data={data}
+          fetchData={fetchData}
+          pageCount={pageCount}
+        />
       </div>
     </Container>
   );
 };
-
-// TODO: REMOVE <---------------------
-//
-// export const getPropertyList = (filter: IPropertyFilter) => {
-//   return CustomAxios()
-//     .get(getPropertyListUrl(filter))
-//     .then((response: AxiosResponse) => {
-//       return response.data;
-//     })
-//     .catch((axiosError: AxiosError) => {
-//       return [];
-//     })
-//     .finally(() => {});
-// };
 
 export default PropertyListView;
