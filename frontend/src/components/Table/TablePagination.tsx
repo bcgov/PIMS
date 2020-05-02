f9971e004459c9859fb23de9ed22085225f0fb54
Alejandro Sanchez <emailforasr@gmail.com>
2020-05-02 07:24:51 -0700

    PIMS-1472 Property List Pagination (#181)
    * Better typing support for react-table
    
    * Refactor table components
    
    * Add pagination control
    
    * Implement server pagination

---
+++ frontend/src/components/Table/TablePagination.tsx
@@ -0,0 +1,72 @@
+import './TablePagination.scss';
+
+import React, { useCallback, ReactElement, PropsWithChildren } from 'react';
+import { TableInstance } from 'react-table';
+import ReactPaginate from 'react-paginate';
+
+export type TablePaginationProps<T extends object> = PropsWithChildren<{
+  instance: TableInstance<T>;
+}>;
+
+const TablePagination = <T extends object>(props: TablePaginationProps<T>): ReactElement | null => {
+  const {
+    state: {
+      pageIndex,
+      // pageSize,  // TODO:: Change page size
+      rowCount = props.instance.rows.length,
+    },
+    gotoPage,
+    nextPage,
+    previousPage,
+    // setPageSize,  // TODO: Change page size
+    pageCount,
+  } = props.instance;
+
+  const handleChangePage = useCallback(
+    (event: { selected: number }) => {
+      const newPage = event.selected;
+      if (newPage === pageIndex + 1) {
+        nextPage();
+      } else if (newPage === pageIndex - 1) {
+        previousPage();
+      } else {
+        gotoPage(newPage);
+      }
+    },
+    [gotoPage, nextPage, pageIndex, previousPage],
+  );
+
+  // TODO: Change page size
+  // const rowsPerPageOptions = [5, 10, 25, 50]
+  // const onChangeRowsPerPage = useCallback(
+  //   e => {
+  //     setPageSize(Number(e.target.value));
+  //   },
+  //   [setPageSize],
+  // );
+
+  return rowCount ? (
+    <ReactPaginate
+      previousLabel={'<'}
+      nextLabel={'>'}
+      breakLabel={'...'}
+      pageCount={pageCount}
+      marginPagesDisplayed={2}
+      pageRangeDisplayed={5}
+      onPageChange={handleChangePage}
+      // css
+      activeClassName="active"
+      breakClassName="page-item"
+      breakLinkClassName="page-link"
+      containerClassName="pagination"
+      pageClassName="page-item"
+      pageLinkClassName="page-link"
+      previousClassName="page-item"
+      previousLinkClassName="page-link"
+      nextClassName="page-item"
+      nextLinkClassName="page-link"
+    />
+  ) : null;
+};
+
+export default TablePagination;
---
+++ frontend/src/components/Table/index.ts
@@ -0,0 +1,2 @@
+export { default as Table } from './Table';
+export { default as TablePagination } from './TablePagination';
