98f6f20c0815c4e7788564e23b7789497ac98d70
Jeremy Foster <jeremymfoster@hotmail.com>
2020-05-08 15:54:41 -0700

    PIMS-1525 Adding unit tests to pipeline (#203)

--- openshift/pipelines/scripts/common.groovy
+++ openshift/pipelines/scripts/common.groovy
@@ -103,6 +103,39 @@ def build(buildconfigs, int waitTimeout) {
   echo "Builds complete ..."
 }
 
+// Abort the specified build.
+@NonCPS
+private void abortBuild(build){
+    boolean aborted=false;
+    if (build instanceof org.jenkinsci.plugins.workflow.job.WorkflowRun){
+        int counter=0
+        while (counter<60 && build.isInProgress()){
+            for (org.jenkinsci.plugins.workflow.support.steps.input.InputAction inputAction:build.getActions(org.jenkinsci.plugins.workflow.support.steps.input.InputAction.class)){
+                for (org.jenkinsci.plugins.workflow.support.steps.input.InputStepExecution inputStep:inputAction.getExecutions()){
+                    if (!inputStep.isSettled()){
+                        inputStep.doAbort()
+                    }
+                }
+            }
+
+            counter++
+            Thread.sleep(1000) //milliseconds
+        }
+    }
+
+    if (build.isInProgress()){
+        build.doKill()
+    }
+
+}
+
+// Abort all previous builds in progress for the specified build.
+def abortAllPreviousBuildInProgress(currentBuild) {
+    while(currentBuild.rawBuild.getPreviousBuildInProgress() != null) {
+        abortBuild(currentBuild.rawBuild.getPreviousBuildInProgress())
+    }
+}
+
 // --------------------
 // Supporting Functions
 // --------------------
