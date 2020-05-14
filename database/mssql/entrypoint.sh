e64712246bb6b64f5ba430d3f362c69a4254d4c7
Jeremy Foster <jeremymfoster@hotmail.com>
2020-05-14 11:35:35 -0700

    PIMS-1272 MS SQL Image (#219)

--- database/mssql/entrypoint.sh
+++ database/mssql/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 # Start SQL Server
-/opt/mssql/bin/sqlservr & /usr/config/setup.sh
+echo "Start MS SQL Server and run scripts"
 
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-sqlservr}:x:$(id -u):0:${USER_NAME:-sqlservr} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+# exec "$@"
+
+/usr/config/setup.sh & /opt/mssql/bin/sqlservr
--- database/mssql/scripts/init.sql
+++ database/mssql/scripts/init.sql
@@ -1,7 +1,9 @@
+PRINT 'Creating default database'
 CREATE DATABASE $(DB_NAME);
 GO
 USE $(DB_NAME);
 GO
+PRINT 'Adding default admin account'
 CREATE LOGIN $(DB_USER) WITH PASSWORD = '$(DB_PASSWORD)';
 GO
 CREATE USER $(DB_USER) FOR LOGIN $(DB_USER);
