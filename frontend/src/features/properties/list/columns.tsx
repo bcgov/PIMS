1c0a74abd4f2a2f8e41d652d401cca46779d60ec
Alejandro Sanchez <emailforasr@gmail.com>
2020-05-19 10:39:49 -0700

    Add additional column with link to property detail form

--- frontend/src/features/properties/list/columns.tsx
+++ frontend/src/features/properties/list/columns.tsx
@@ -4,6 +4,7 @@ import LandSvg from 'assets/images/icon-lot.svg';
 import React from 'react';
 import { Column, CellProps } from 'react-table';
 import { Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { formatMoney, formatNumber } from 'utils';
 import { IProperty } from '.';
 
@@ -22,6 +23,7 @@ export const columns: ColumnWithProps<IProperty>[] = [
     Header: 'Agency',
     accessor: 'agencyCode', // accessor is the "key" in the data
     align: 'left',
+    width: 65,
   },
   {
     Header: 'Sub Agency',
@@ -38,6 +40,7 @@ export const columns: ColumnWithProps<IProperty>[] = [
     Header: 'Classification',
     accessor: 'classification',
     align: 'left',
+    width: 120,
   },
   {
     Header: 'Street Address',
@@ -48,34 +51,39 @@ export const columns: ColumnWithProps<IProperty>[] = [
     Header: 'City',
     accessor: 'city',
     align: 'left',
+    width: 65,
   },
   {
     Header: 'Municipality',
     accessor: 'municipality',
     align: 'left',
+    width: 95,
   },
   {
     Header: 'Assessed Value',
     accessor: 'assessed',
     Cell: MoneyCell,
     align: 'left',
+    width: 100,
   },
   {
     Header: 'Netbook Value',
     accessor: 'netBook',
     Cell: MoneyCell,
     align: 'left',
+    width: 100,
   },
   {
     Header: 'Estimated Value',
     accessor: 'estimated',
     Cell: MoneyCell,
     align: 'left',
+    width: 100,
   },
   {
     Header: 'Type',
     accessor: 'propertyTypeId',
-    width: 60,
+    width: 50,
     Cell: ({ cell: { value } }: CellProps<IProperty, number>) => {
       const icon = value === 0 ? LandSvg : BuildingSvg;
       return <Image src={icon} />;
@@ -86,5 +94,22 @@ export const columns: ColumnWithProps<IProperty>[] = [
     accessor: 'landArea',
     width: 80,
     Cell: NumberCell,
+  },
+  {
+    Header: '',
+    width: 65,
+    id: 'view-link-column',
+    // return the parcel ID associated with this row
+    // for buildings we need the parent `parcelId` property
+    accessor: row => {
+      const id = row.propertyTypeId === 0 ? row.id : row.parcelId;
+      return id ?? -1;
+    },
+    Cell: ({ cell: { value } }: CellProps<IProperty, number>) => {
+      if (value > 0) {
+        return <Link to={`/submitProperty/${value}?disabled=true`}>View</Link>;
+      }
+      return null;
+    },
   },
 ];
