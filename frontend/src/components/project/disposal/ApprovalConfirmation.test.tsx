2e9492de72af829cfb67e7e49486bd1fbdd31f62
Goeme Nthomiwa <gnthomiwa@live.ca>
2020-05-26 13:07:39 -0700

    PIMS-1841 Approval Confirmation component and step actions component (#249)
    Co-authored-by: Goeme Nthomiwa <goementhomiwa@Goemes-MacBook-Pro.local>

---
+++ frontend/src/components/project/disposal/ApprovalConfirmation.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ApprovalConfirmationCheckbox, OnChangeType } from './ApprovalConfirmation';
+import { Formik } from 'formik';
+import { Form } from 'react-bootstrap';
+import { noop } from 'lodash';
+
+const renderComponent = (checked: boolean, onChange: OnChangeType) => {
+  const userDisplayName = 'PIMS User';
+  const fieldName = 'confirmation';
+  return renderer.create(
+    <Formik initialValues={{ [fieldName]: checked }} onSubmit={console.log}>
+      <Form>
+        <ApprovalConfirmationCheckbox
+          field={fieldName}
+          onChange={onChange}
+          userDisplayName={userDisplayName}
+        />
+      </Form>
+    </Formik>,
+  );
+};
+
+describe('Approval Confirmation', () => {
+  it('Matches Snapshot', () => {
+    const component = renderComponent(false, noop);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('Input is checked', () => {
+    const component = renderComponent(true, noop);
+    expect(component.root.findByType('input').props.checked).toBeTruthy();
+  });
+
+  it('Input is not checked', () => {
+    const component = renderComponent(false, noop);
+    expect(component.root.findByType('input').props.checked).toBeFalsy();
+  });
+});
