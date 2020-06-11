e869757073c7ed25a20b815a553e8860bc2bc7c5
jdtoombs <jdtoombs@gmail.com>
2020-06-10 17:02:37 -0700

    PIMS-2389

--- frontend/src/features/projects/dispose/ProjectDisposeLayout.tsx
+++ frontend/src/features/projects/dispose/ProjectDisposeLayout.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './ProjectDisposeView.scss';
 import { Container, Spinner } from 'react-bootstrap';
 import { Route, match as Match, useHistory, Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'reducers/rootReducer';
 import _ from 'lodash';
 import {
@@ -11,6 +11,8 @@ import {
   useStepper,
   StepActions,
   IStatus,
+  IProject,
+  saveProject,
 } from '.';
 import { FormikValues } from 'formik';
 import { IGenericNetworkAction } from 'actions/genericActions';
@@ -18,12 +20,16 @@ import { ProjectActions } from 'constants/actionTypes';
 import GeneratedDisposeStepper from './components/GeneratedDisposeStepper';
 import SresManual from './components/SresManual';
 import ReviewApproveStep from './steps/ReviewApproveStep';
+import ProjectDraftStep from './steps/ProjectDraftStep';
+import { createProject } from 'features/projects/dispose/projectsActionCreator';
+import queryString from 'query-string';
 
 /**
  * Top level component facilitates 'wizard' style multi-step form for disposing of projects.
  * @param param0 default react router props
  */
 const ProjectDisposeLayout = ({ match, location }: { match: Match; location: Location }) => {
+  const [preDraft, setPreDraft] = useState(true);
   const history = useHistory();
   const formikRef = useRef<FormikValues>();
   const workflowStatuses = useSelector<RootState, IStatus[]>(state => state.projectWorkflow as any);
@@ -31,8 +37,17 @@ const ProjectDisposeLayout = ({ match, location }: { match: Match; location: Loc
   const getProjectRequest = useSelector<RootState, IGenericNetworkAction>(
     state => (state.network as any)[ProjectActions.GET_PROJECT] as any,
   );
+  const dispatch = useDispatch();
+  const query = location?.search ?? {};
+  const projectNumber = queryString.parse(query).projectNumber;
+  const historyReplace = history.replace;
 
-  const onNext = () =>
+  // TODO: UX flow - since this is a pre draft state hitting next does not step through the stepper as once they create the project then they are in 'Draft'
+  // May be something we want to change
+  const onNext = async () => {
+    if (!projectNumber) {
+      await handleSubmit();
+    }
     formikRef.current?.submitForm().then((value: any) => {
       if (!formikRef?.current?.errors || !Object.keys(formikRef?.current?.errors).length) {
         // do not go to the next step if the form has validation errors.
@@ -42,13 +57,35 @@ const ProjectDisposeLayout = ({ match, location }: { match: Match; location: Loc
         }
       }
     });
+  };
 
   const getComponentPath = (wfc: ProjectWorkflowComponent) => {
     return `${match.url}${_.find(workflowStatuses, { id: wfc.workflowStatus })?.route}`;
   };
 
-  //Controls the route based on the current status.
-  const historyReplace = history.replace;
+  const handleSubmit = async () => {
+    const preDraftValues: any = {
+      name: formikRef.current?.values.name,
+      note: formikRef.current?.values.note,
+      description: formikRef.current?.values.description,
+      properties: [],
+      tierLevelId: 1,
+      statusId: 0,
+      agencyId: 0,
+      tasks: [],
+    };
+    if (!projectNumber) {
+      await (dispatch(createProject(preDraftValues)) as any).then((project: IProject) => {
+        dispatch(saveProject(project));
+        historyReplace(
+          `${match.url}${workflowStatuses[0].route}?projectNumber=${project.projectNumber}`,
+        );
+      });
+    } else {
+      formikRef.current?.handleSubmit();
+    }
+  };
+
   useEffect(() => {
     let statusAtRoute = _.find(workflowStatuses, ({ route }) => location.pathname.includes(route));
     if (setCurrentStatus) setCurrentStatus(statusAtRoute);
@@ -65,10 +102,20 @@ const ProjectDisposeLayout = ({ match, location }: { match: Match; location: Loc
 
   //If the current route isn't set, set based on the project status.
   useEffect(() => {
-    if (location.pathname === '/dispose' && project.status?.route !== undefined) {
+    if (
+      location.pathname === '/dispose' &&
+      project.status?.route !== undefined &&
+      project.projectNumber !== undefined
+    ) {
       historyReplace(`/dispose${project.status?.route}?projectNumber=${project.projectNumber}`);
     }
   }, [historyReplace, project.status, project.projectNumber, location.pathname, match.url]);
+
+  if (projectNumber !== null && projectNumber !== undefined && getProjectRequest?.error) {
+    throw Error(`Unable to load project number ${projectNumber}`);
+  }
+
+  projectNumber && preDraft && setPreDraft(false);
 
   return (
     <>
@@ -76,7 +123,7 @@ const ProjectDisposeLayout = ({ match, location }: { match: Match; location: Loc
         <Container fluid className="ProjectDisposeLayout">
           <SresManual />
           <h1>Surplus Property Program Project</h1>
-          {currentStatus !== undefined ? (
+          {currentStatus !== undefined || preDraft ? (
             <GeneratedDisposeStepper
               activeStep={currentStatus?.sortOrder ?? 0}
               basePath={match.url}
@@ -86,6 +133,7 @@ const ProjectDisposeLayout = ({ match, location }: { match: Match; location: Loc
             <Container fluid className="step-content">
               {/*TODO: this will probably need to be update to a map of routes/components as well.*/}
               <Route path="/dispose/projects/assess/properties" component={ReviewApproveStep} />
+              {preDraft ? <ProjectDraftStep formikRef={formikRef} /> : null}
               {projectWorkflowComponents.map(wfc => (
                 <Route
                   key={wfc.workflowStatus.toString()}
@@ -93,10 +141,10 @@ const ProjectDisposeLayout = ({ match, location }: { match: Match; location: Loc
                   render={() => <wfc.component formikRef={formikRef} />}
                 />
               ))}
-              {currentStatus && (
+              {(currentStatus || preDraft) && (
                 <StepActions
                   getNextStep={getNextStep}
-                  onSave={() => formikRef.current?.handleSubmit()}
+                  onSave={() => handleSubmit()}
                   onNext={onNext}
                 />
               )}
