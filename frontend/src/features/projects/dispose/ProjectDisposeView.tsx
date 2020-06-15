394cd1f5cc8da57fee1d80930a89274d2ac9d216
Smith <Devin.Smith@quartech.com>
2020-06-15 15:01:32 -0700

    Clear the project between top level project disposal page loads.

--- frontend/src/features/projects/dispose/ProjectDisposeView.tsx
+++ frontend/src/features/projects/dispose/ProjectDisposeView.tsx
@@ -4,12 +4,16 @@ import { Container } from 'react-bootstrap';
 import { match as Match } from 'react-router-dom';
 import { StepContextProvider } from '.';
 import ProjectDisposeLayout from './ProjectDisposeLayout';
+import { useDispatch } from 'react-redux';
+import { clearProject } from './slices/projectSlice';
 
 /**
  * Top level component facilitates 'wizard' style multi-step form for disposing of projects.
  * @param param0 default react router props
  */
 const ProjectDisposeView = ({ match, location }: { match: Match; location: Location }) => {
+  const dispatch = useDispatch();
+  dispatch(clearProject());
   return (
     <StepContextProvider>
       <Container fluid className="ProjectDisposeView">
