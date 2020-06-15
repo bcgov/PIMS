11608308aa77256f4feb8991a2322e98278f2b94
Smith <Devin.Smith@quartech.com>
2020-06-15 15:49:15 -0700

    Move up some of the project logic to ensure that the correct project is loaded for the layout.

--- frontend/src/features/projects/dispose/ProjectDisposeView.tsx
+++ frontend/src/features/projects/dispose/ProjectDisposeView.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './ProjectDisposeView.scss';
 import { Container } from 'react-bootstrap';
 import { match as Match } from 'react-router-dom';
-import { StepContextProvider } from '.';
+import { StepContextProvider, clearProject } from '.';
 import ProjectDisposeLayout from './ProjectDisposeLayout';
-import { useDispatch } from 'react-redux';
-import { clearProject } from './slices/projectSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import queryString from 'query-string';
+import { fetchProject } from './projectsActionCreator';
+import { RootState } from 'reducers/rootReducer';
+import { IGenericNetworkAction } from 'actions/genericActions';
+import { ProjectActions } from 'constants/actionTypes';
 
 /**
  * Top level component facilitates 'wizard' style multi-step form for disposing of projects.
  * @param param0 default react router props
  */
 const ProjectDisposeView = ({ match, location }: { match: Match; location: Location }) => {
+  const query = location?.search ?? {};
+  const projectNumber = queryString.parse(query).projectNumber;
   const dispatch = useDispatch();
-  dispatch(clearProject());
+  const getProjectRequest = useSelector<RootState, IGenericNetworkAction>(
+    state => (state.network as any)[ProjectActions.GET_PROJECT] as any,
+  );
+  useEffect(() => {
+    if (projectNumber !== null && projectNumber !== undefined) {
+      dispatch(clearProject());
+      dispatch(fetchProject(projectNumber as string));
+    }
+  }, [dispatch, projectNumber]);
+
+  if (projectNumber !== null && projectNumber !== undefined && getProjectRequest?.error) {
+    throw Error(`Unable to load project number ${projectNumber}`);
+  }
   return (
     <StepContextProvider>
       <Container fluid className="ProjectDisposeView">
