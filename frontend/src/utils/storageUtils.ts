55c0d73e02080a90b9d5dfe600f1e5878fb46123
Goeme Nthomiwa <gnthomiwa@live.ca>
2020-06-18 12:01:49 -0700

    PIMS-1713 Show parcel pin movement confirmation popup (#334)
    Co-authored-by: Nthomiwa <Goeme.Nthomiwa@quartech.com>

--- frontend/src/utils/storageUtils.ts
+++ frontend/src/utils/storageUtils.ts
@@ -1,4 +1,5 @@
 export const PARCEL_STORAGE_NAME = 'parcelDetailForm';
+export const PIN_MOVEMENT_CONFIRM = 'pinMoveConfirm';
 export const isStorageInUse = (storageName: string): boolean =>
   !!window.localStorage.getItem(storageName);
 export const clearStorage = (storageName: string) => window.localStorage.removeItem(storageName);
