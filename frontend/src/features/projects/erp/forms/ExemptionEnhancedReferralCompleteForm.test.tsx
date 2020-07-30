66797e22ed5ee59f7581f09fd175752447e22557
Alejandro Sanchez <emailforasr@gmail.com>
2020-07-29 18:05:09 -0700

    Unit test to check required field on ExemptionCompleteForm

--- frontend/src/features/projects/erp/forms/ExemptionEnhancedReferralCompleteForm.test.tsx
+++ frontend/src/features/projects/erp/forms/ExemptionEnhancedReferralCompleteForm.test.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { IProject } from 'features/projects/common/interfaces';
-import { Formik, Form } from 'formik';
+import { Formik, Form, FormikProps } from 'formik';
 import ExemptionEnhancedReferralCompleteForm from './ExemptionEnhancedReferralCompleteForm';
 import renderer from 'react-test-renderer';
 import { noop } from 'lodash';
-import { render, wait, fireEvent } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, { mount } from 'enzyme';
 import { Button } from 'react-bootstrap';
@@ -18,6 +18,7 @@ const history = createMemoryHistory();
 const element = (func: Function) => (
   <Router history={history}>
     <FormComponent
+      values={testProject}
       addToErp={noop}
       onClickGreTransferred={noop}
       onClickNotInSpl={noop}
@@ -49,12 +50,20 @@ const testProject: IProject = {
 };
 
 const FormComponent: React.FC<{
+  values: object;
   addToErp: any;
   onClickGreTransferred: any;
   onClickProceedToSpl: any;
   onClickNotInSpl: any;
+  formRef?: React.MutableRefObject<FormikProps<any> | undefined>;
 }> = props => (
-  <Formik initialValues={testProject} onSubmit={values => {}}>
+  <Formik
+    innerRef={instance => {
+      props.formRef && (props.formRef.current = instance);
+    }}
+    initialValues={props.values}
+    onSubmit={values => {}}
+  >
     <Form>
       <ExemptionEnhancedReferralCompleteForm
         onClickAddToErp={props.addToErp}
@@ -67,10 +76,11 @@ const FormComponent: React.FC<{
 );
 
 describe('ExemptionEnhancedReferralCompleteForm', () => {
-  it('Should render successfully', () => {
+  it('renders successfully', () => {
     const tree = renderer
       .create(
         <FormComponent
+          values={testProject}
           addToErp={noop}
           onClickGreTransferred={noop}
           onClickNotInSpl={noop}
@@ -78,14 +88,60 @@ describe('ExemptionEnhancedReferralCompleteForm', () => {
         />,
       )
       .toJSON();
-
     expect(tree).toMatchSnapshot();
   });
 
-  it('Should execute onClickGreTransferred', async () => {
-    const onClickGreTransferred = jest.fn();
-    const { getByText } = render(
+  it.only('requires Clearance Notification Date to be set before enabling action buttons', async () => {
+    // need a ref here to change field values programmatically
+    const formikRef = React.createRef<FormikProps<any>>();
+    const { findByRole } = render(
       <FormComponent
+        formRef={formikRef as React.MutableRefObject<FormikProps<any>>}
+        values={testProject}
+        addToErp={noop}
+        onClickGreTransferred={noop}
+        onClickNotInSpl={noop}
+        onClickProceedToSpl={noop}
+      />,
+    );
+
+    const findButton = (name: string) => findByRole('button', { name: new RegExp(name, 'i') });
+    const setClearanceDate = (value: Date | '') => {
+      formikRef?.current?.setFieldValue('clearanceNotificationSentOn', value);
+    };
+    const setTransferredGreDate = (value: Date | '') => {
+      formikRef?.current?.setFieldValue('transferredWithinGreOn', value);
+    };
+
+    // ASSERT - all 4 buttons should be disabled when clearance notification date is empty
+    act(() => {
+      setClearanceDate('');
+    });
+    expect(await findButton('Add to Enhanced Referral Process')).toBeDisabled();
+    expect(await findButton('Update Property Information')).toBeDisabled();
+    expect(await findButton('Not Included in the SPL')).toBeDisabled();
+    expect(await findButton('Proceed to SPL')).toBeDisabled();
+
+    // ASSERT - 3 buttons should be enabled after clearance notification date is set
+    act(() => {
+      setClearanceDate(new Date(Date.UTC(2020, 5, 9, 8)));
+    });
+    expect(await findButton('Add to Enhanced Referral Process')).toBeEnabled();
+    expect(await findButton('Not Included in the SPL')).toBeEnabled();
+    expect(await findButton('Proceed to SPL')).toBeEnabled();
+
+    // ASSERT - GRE button should be enabled ONLY when GRE date set
+    act(() => {
+      setTransferredGreDate(new Date(Date.UTC(2020, 5, 9, 8)));
+    });
+    expect(await findButton('Update Property Information')).toBeEnabled();
+  });
+
+  it('executes onClickGreTransferred callback', async () => {
+    const onClickGreTransferred = jest.fn();
+    const { getByRole } = render(
+      <FormComponent
+        values={testProject}
         addToErp={noop}
         onClickGreTransferred={onClickGreTransferred}
         onClickNotInSpl={noop}
@@ -93,15 +149,16 @@ describe('ExemptionEnhancedReferralCompleteForm', () => {
       />,
     );
 
-    const updatePropertiesButton = getByText('Update Property Information');
-    await wait(() => fireEvent.click(updatePropertiesButton));
+    const updatePropertiesButton = getByRole('button', { name: /Update Property Information/i });
+    fireEvent.click(updatePropertiesButton);
     expect(onClickGreTransferred).toHaveBeenCalledTimes(1);
   });
 
-  it('Should execute onClickAddToErp', async () => {
+  it('executes onClickAddToErp callback', async () => {
     const onClickAddToErp = jest.fn();
-    const { getByText } = render(
+    const { getByRole } = render(
       <FormComponent
+        values={testProject}
         addToErp={onClickAddToErp}
         onClickGreTransferred={noop}
         onClickNotInSpl={noop}
@@ -109,15 +166,16 @@ describe('ExemptionEnhancedReferralCompleteForm', () => {
       />,
     );
 
-    const addToErpButton = getByText('Add to Enhanced Referral Process');
-    await wait(() => fireEvent.click(addToErpButton));
+    const addToErpButton = getByRole('button', { name: /Add to Enhanced Referral Process/i });
+    fireEvent.click(addToErpButton);
     expect(onClickAddToErp).toHaveBeenCalledTimes(1);
   });
 
-  it('Should execute onClickNotInSpl', async () => {
+  it('executes onClickNotInSpl callback', async () => {
     const onClickNotInSpl = jest.fn();
-    const { getByText } = render(
+    const { getByRole } = render(
       <FormComponent
+        values={testProject}
         addToErp={noop}
         onClickGreTransferred={noop}
         onClickNotInSpl={onClickNotInSpl}
@@ -125,12 +183,14 @@ describe('ExemptionEnhancedReferralCompleteForm', () => {
       />,
     );
 
-    const notInSplButton = getByText('Not Included in the SPL');
-    await wait(() => fireEvent.click(notInSplButton));
+    const notInSplButton = getByRole('button', { name: /Not Included in the SPL/i });
+    fireEvent.click(notInSplButton);
     expect(onClickNotInSpl).toHaveBeenCalledTimes(1);
   });
+
   const splClick = jest.fn();
   const component = mount(element(splClick));
+
   it('displays confirmation when clicking proceed to spl', () => {
     const button = component.findWhere((node: { type: () => any; text: () => string }) => {
       return node.type() === Button && node.text() === 'Proceed to SPL';
