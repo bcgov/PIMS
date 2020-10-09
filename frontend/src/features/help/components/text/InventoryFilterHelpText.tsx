6e77e760b4d8a34fe26731600b5d539b4f53e5a0
Devin Smith <devinleighsmith@gmail.com>
2020-10-09 16:40:13 -0700

    PA-944 Add Help Functionality to PIMS.

---
+++ frontend/src/features/help/components/text/InventoryFilterHelpText.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+/**
+ * Help Text for the Property Inventory Filter topic.
+ */
+const InventoryFilterHelpText = () => {
+  return (
+    <p>
+      The filter provides a way to search for properties with the specified attributes. The filter
+      is cumulative ("AND"), which means each value will refine the results.
+      <br />
+      <strong>Address:</strong> The address contains the value.
+      <br />
+      <strong>City:</strong> The city containing the value.
+      <br />
+      <strong>Municipality:</strong> The municipality containing the value.
+      <br />
+      <strong>PID/PIN:</strong> The property has the specified PID or PIN.
+      <br />
+      <strong>SPP No.:</strong> The project containing the value.
+      <br />
+      <strong>Agency:</strong> The property belongs to the specified agency.
+      <br />
+      <strong>Classification:</strong> The property has the specified classification.
+      <br />
+      <strong>Min Lot Size:</strong> The property is greater than or equal to the value.
+      <br />
+      <strong>Max Lot Size:</strong> The property is less than or equal to the value.
+      <br />
+    </p>
+  );
+};
+
+export default InventoryFilterHelpText;
---
+++ frontend/src/features/help/components/text/InventoryNavigationHelpText.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+/**
+ * Help Text for the Property Inventory Help topic.
+ */
+const InventoryNavigationHelpText = () => {
+  return (
+    <p>
+      The navigation bar provides the available menu options for your role.
+      <br />
+      <strong>Home:</strong> Map to view and find properties.
+      <br />
+      <strong>Administration:</strong> Manage users and Access Requests.
+      <br />
+      <strong>Manage Property:</strong> View Inventory or Submit Property to inventory.
+      <br />
+      <strong>View Projects:</strong> Manage Disposal Projects.
+      <br />
+      <strong>Dispose Properties:</strong> Submit properties to Surplus Property Program.
+      <br />
+      <strong>Approval Requests:</strong> Review and access submitted Disposal Projects.
+    </p>
+  );
+};
+
+export default InventoryNavigationHelpText;
