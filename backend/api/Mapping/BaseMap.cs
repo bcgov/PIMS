66d968953a31f099c99ef16fd8e9af5313a08c49
Smith <Devin.Smith@quartech.com>
2020-10-14 16:43:42 -0700

    Display last updated user, email, time on parcel detail form.

--- backend/api/Mapping/BaseMap.cs
+++ backend/api/Mapping/BaseMap.cs
@@ -11,6 +11,8 @@ namespace Pims.Api.Mapping
             config.NewConfig<Entity.BaseEntity, Models.BaseModel>()
                 .Map(dest => dest.CreatedOn, src => src.CreatedOn)
                 .Map(dest => dest.UpdatedOn, src => src.UpdatedOn)
+                .Map(dest => dest.UpdatedByName, src => src.UpdatedBy != null ? src.UpdatedBy.DisplayName : src.CreatedBy != null ? src.CreatedBy.DisplayName : "unknown")
+                .Map(dest => dest.UpdatedByEmail, src => src.UpdatedBy != null ? src.UpdatedBy.Email : src.CreatedBy != null ? src.CreatedBy.Email : "unknown")
                 .Map(dest => dest.RowVersion, src => src.RowVersion == null ? null : Convert.ToBase64String(src.RowVersion));
 
             config.NewConfig<Models.BaseModel, Entity.BaseEntity>()
