589314b6d2ad33c0734d2b81b3acc718e95dbe97
Devin Smith <devinleighsmith@gmail.com>
2020-10-16 13:46:35 -0700

    PA-1026 remove duplicate agencies. Update importer.

---
+++ backend/dal/Migrations/20201016195846_correctAgencies.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Pims.Dal.Helpers.Migrations;
+
+namespace Pims.Dal.Migrations
+{
+    public partial class correctAgencies : SeedMigration
+    {
+    }
+}
