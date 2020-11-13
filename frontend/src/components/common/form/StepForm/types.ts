71eb4b217ac06dad66ef6a5ad68a7cdfb54e2452
Goeme Nthomiwa <gnthomiwa@live.ca>
2020-11-13 09:47:35 -0800

    PA-1667 using formik persist to local storage (#608)

--- frontend/src/components/common/form/StepForm/types.ts
+++ frontend/src/components/common/form/StepForm/types.ts
@@ -1,4 +1,3 @@
-import { PersistProps } from 'components/common/FormikPersist';
 import { IStep } from 'components/common/Stepper';
 
 export interface IStepperFormContextProps {
@@ -13,10 +12,20 @@ export interface ISteppedFormValues<T extends object = {}> {
   data: T;
 }
 
+export interface IPersistProps {
+  // Name of of the localstorage key
+  name: string;
+  // Secret used to encrypt/decrypt the form data
+  secret: string;
+  persistCallback: (values: any) => void;
+}
+
 export interface ISteppedFormProps {
   steps: IStep[];
+  // set to true to make to the form persist to localstorage
   persistable?: boolean;
-  persistProps?: PersistProps;
+  // options to set persist component
+  persistProps?: IPersistProps;
 }
 
 export interface IStepperFieldProps {
