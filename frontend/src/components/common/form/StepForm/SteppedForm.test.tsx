a5fb5db72bcf8668234b05c9af43006394058060
Goeme Nthomiwa <gnthomiwa@live.ca>
2020-11-13 09:47:35 -0800

    PA-1667 using formik persist to local storage (#608)

---
+++ frontend/src/components/common/form/StepForm/SteppedForm.test.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
+import { Container, Button } from 'react-bootstrap';
+import { SteppedForm, useFormStepper } from '.';
+import { Input } from '..';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const FormContentComponent = () => {
+  const stepper = useFormStepper();
+  return (
+    <Container>
+      <Input field="data.name" placeholder="Name" />
+
+      <p>Form inputs and control button here</p>
+      <Button onClick={() => stepper.gotoStep(3)} variant="outline-info">
+        Go to
+      </Button>
+      <Button onClick={stepper.goBack} variant="outline-info">
+        Back
+      </Button>
+      <Button onClick={stepper.gotoNext} variant="outline-info">
+        Next Step
+      </Button>
+      <Button type="submit">Save</Button>
+    </Container>
+  );
+};
+
+const Component = () => {
+  return (
+    <SteppedForm
+      steps={[
+        { route: 'building-id', title: 'Building ID', completed: false, canGoToStep: true },
+        { route: 'tenancy', title: 'Tenancy', completed: false, canGoToStep: true },
+        { route: 'valuation', title: 'Valuation', completed: false, canGoToStep: true },
+        { route: 'parcel', title: 'Parcel', completed: false, canGoToStep: true },
+        { route: 'review', title: 'Review', completed: false, canGoToStep: true },
+      ]}
+      initialValues={{
+        activeStep: 0,
+        data: { name: 'Quartech HQ' },
+      }}
+      persistable
+      persistProps={{
+        name: 'testForm',
+        secret: 'secret',
+        persistCallback: console.log,
+      }}
+      onSubmit={console.log}
+    >
+      <FormContentComponent />
+    </SteppedForm>
+  );
+};
+
+describe('SteppedForm', () => {
+  it('component renders correctly', () => {
+    const tree = renderer.create(<Component />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
