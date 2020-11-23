ece7b87fa43f916dbe70a1c97308215b9ee77edc
Goeme Nthomiwa <gnthomiwa@live.ca>
2020-11-23 11:03:57 -0800

    PA-1660 component to edit/add/delete polygons for properties on a map (#620)

--- frontend/styleguide.config.js
+++ frontend/styleguide.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+
 module.exports = {
   components: 'src/components/**/*.{jsx,tsx}',
   propsParser: (filePath, source, resolver, handlers) => {
@@ -6,5 +7,15 @@ module.exports = {
     return ext === '.tsx'
       ? require('react-docgen-typescript').parse(filePath, source, resolver, handlers)
       : require('react-docgen').parse(source, resolver, handlers);
+  },
+  template: {
+    head: {
+      links: [
+        {
+          rel: 'stylesheet',
+          href: '//cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.css',
+        },
+      ],
+    },
   },
 };
