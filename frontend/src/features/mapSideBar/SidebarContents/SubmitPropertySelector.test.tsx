b8949ce0f51097b4ccc7a071865ce36520187389
Alex Galbraith <33818575+agalbrai@users.noreply.github.com>
2021-01-04 15:24:13 -0800

    pa-2408 Change Raw Land to Bare land (#742)

--- frontend/src/features/mapSideBar/SidebarContents/SubmitPropertySelector.test.tsx
+++ frontend/src/features/mapSideBar/SidebarContents/SubmitPropertySelector.test.tsx
@@ -6,7 +6,7 @@ import noop from 'lodash/noop';
 describe('SubmitPropertySelector', () => {
   it('component renders correctly', () => {
     const tree = renderer
-      .create(<SubmitPropertySelector addBuilding={noop} addRawLand={noop} />)
+      .create(<SubmitPropertySelector addBuilding={noop} addBareLand={noop} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
