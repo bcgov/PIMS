8f4d05d8659e4acd6f27c08538cded68e468d782
Alex Galbraith <33818575+agalbrai@users.noreply.github.com>
2021-01-15 11:48:32 -0800

    PA-2244 Add Marina to Building Predominate use (#767)
    fixed helper
    
    undo change to initial set up
    
    fix for rollback case
    
    fixed for rollback again
    
    remove extra begin/end

---
+++ backend/dal/Migrations/20210114232914_v01.06.01.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pims.Dal.Migrations
+{
+    public partial class v010601 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
