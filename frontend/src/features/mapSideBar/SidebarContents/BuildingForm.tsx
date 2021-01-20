9dc78c0e919e154af02663c4a90348e6dcbadf90
Devin Smith <devinleighsmith@gmail.com>
2021-01-20 12:54:11 -0800

    PA-2352 ensure that inventory scrolls appropriately on small screens.

--- frontend/src/features/mapSideBar/SidebarContents/BuildingForm.tsx
+++ frontend/src/features/mapSideBar/SidebarContents/BuildingForm.tsx
@@ -74,7 +74,7 @@ const FormFooter = styled.div`
   align-items: center;
   position: sticky;
   background-color: #f2f2f2;
-  bottom: 40px;
+  bottom: 25px;
 `;
 
 const FillRemainingSpace = styled.span`
