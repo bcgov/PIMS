e43fd913ab61a9eb45d751f62010a0d923e0185d
devinleighsmith <41091511+devinleighsmith@users.noreply.github.com>
2021-01-26 14:31:14 -0800

    PA-2527 add new erp note type to approval form and add that note to ERP notifications. (#834)
    * PA-2527 add new erp note type to approval form and add that note to ERP notifications.
    
    * test fixes.
    
    * PA-1698 Totals in project should not default to $0.
    
    * PA-2526 erp notifications updated to use project financials instead of property financials.

---
+++ backend/dal/Migrations/v01.06.02/Down/PreDown/01-Projects.sql
@@ -0,0 +1,3 @@
+UPDATE dbo.[Projects] set [Assessed] = 0 WHERE [Assessed] IS NULL
+UPDATE dbo.[Projects] set [Market] = 0 WHERE [Market] IS NULL
+UPDATE dbo.[Projects] set [Netbook] = 0 WHERE [Netbook] IS NULL
