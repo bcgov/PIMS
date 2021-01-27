479841364197dd9076a0e6f5de88b9ccdaffd209
Devin Smith <devinleighsmith@gmail.com>
2021-01-26 19:45:56 -0800

    PA-2565 show current parcel for draft properties.

--- frontend/src/components/maps/hooks/useActiveFeatureLayer.tsx
+++ frontend/src/components/maps/hooks/useActiveFeatureLayer.tsx
@@ -6,6 +6,9 @@ import { MapProps as LeafletMapProps, Map as ReactLeafletMap } from 'react-leafl
 import { useLayerQuery, PARCELS_LAYER_URL, parcelLayerPopupConfig } from '../leaflet/LayerPopup';
 import useDeepCompareEffect from 'hooks/useDeepCompareEffect';
 import { GeoJsonObject } from 'geojson';
+import { PointFeature } from '../types';
+import { useSelector } from 'react-redux';
+import { RootState } from 'reducers/rootReducer';
 
 interface IUseActiveParcelMapLayer {
   /** the current leaflet map reference. This hook will add layers to this map reference. */
@@ -33,6 +36,9 @@ const useActiveFeatureLayer = ({
 }: IUseActiveParcelMapLayer) => {
   const [activeFeatureLayer, setActiveFeatureLayer] = useState<GeoJSON>();
   const parcelsService = useLayerQuery(PARCELS_LAYER_URL);
+  const draftProperties: PointFeature[] = useSelector<RootState, PointFeature[]>(
+    state => state.parcel.draftParcels,
+  );
 
   if (!!mapRef.current && !activeFeatureLayer) {
     setActiveFeatureLayer(geoJSON().addTo(mapRef.current.leafletElement));
@@ -96,6 +102,27 @@ const useActiveFeatureLayer = ({
       highlightSelectedProperty();
     }
   }, [selectedProperty]);
+
+  /**
+   * If there is a draft property on the map, attempt to retrieve the corresponding parcel. If we find matching parcel data, use that to draw the active parcel.
+   * Note: currently this is limited to finding one parent in the case of a building. in the future, we may need to find/display all matching parcels.
+   */
+  useDeepCompareEffect(() => {
+    const draftProperty = draftProperties[0];
+    const highlightSelectedProperty = async () => {
+      const parcelLayerData = await parcelsService.findOneWhereContains({
+        lat: draftProperty.geometry.coordinates[1] || 0,
+        lng: draftProperty.geometry.coordinates[0] || 0,
+      } as LatLng);
+      if (parcelLayerData?.features?.length > 0) {
+        activeFeatureLayer?.addData(parcelLayerData.features[0]);
+      }
+    };
+    if (!!activeFeatureLayer) {
+      activeFeatureLayer.clearLayers();
+      highlightSelectedProperty();
+    }
+  }, [draftProperties]);
 
   return { activeFeatureLayer };
 };
