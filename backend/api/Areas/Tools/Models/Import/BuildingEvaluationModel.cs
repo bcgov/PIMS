593f5e25f221003611384d02a87b0ef606024415
jdtoombs <jdtoombs@gmail.com>
2021-02-09 15:12:12 -0800

    PA-2679: Fix backend erroneously rounding values

--- backend/api/Areas/Tools/Models/Import/BuildingEvaluationModel.cs
+++ backend/api/Areas/Tools/Models/Import/BuildingEvaluationModel.cs
@@ -11,7 +11,7 @@ namespace Pims.Api.Areas.Tools.Models.Import
 
         public string Key { get; set; }
 
-        public float Value { get; set; }
+        public decimal Value { get; set; }
 
         public string Note { get; set; }
         #endregion
