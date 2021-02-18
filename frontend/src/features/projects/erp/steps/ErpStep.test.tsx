38c16f6a14a469df968073f25549679b0c25d9c5
Alex Galbraith <alex.d.galbraith@gmail.com>
2021-02-18 10:52:20 -0800

    PA 2761 Add read only copy of ERP email note

--- frontend/src/features/projects/erp/steps/ErpStep.test.tsx
+++ frontend/src/features/projects/erp/steps/ErpStep.test.tsx
@@ -107,13 +107,20 @@ describe('ERP Approval Step', () => {
       expect(proceedToSplButton).toBeVisible();
       expect(proceedToSplButton).toBeDisabled();
     });
-    it('form fields are not disabled', () => {
+    it('correct form fields are disabled', () => {
       const { queryAllByRole } = render(getApprovalStep());
       const textboxes = queryAllByRole('textbox');
       textboxes.forEach(textbox => {
         expect(textbox).toBeVisible();
-        if (!textbox.className.includes('date-picker') && textbox.id !== 'input-note') {
-          expect(textbox).not.toBeDisabled();
+        if (textbox.id.includes('Spl')) {
+          //only disabled textboxes are SPL related datepickers and erp emails text
+          expect(textbox).toBeDisabled();
+        } else {
+          if (textbox.id.includes('[22]')) {
+            expect(textbox).toBeDisabled();
+          } else {
+            expect(textbox).not.toBeDisabled();
+          }
         }
       });
     });
@@ -142,7 +149,7 @@ describe('ERP Approval Step', () => {
       const proceedToSplButton = component.queryByText(/Not Included in the SPL/);
       expect(proceedToSplButton).toBeDisabled();
     });
-    it('form fields are not disabled', () => {
+    it('form fields are disabled', () => {
       const component = render(getApprovalStep());
       const textboxes = component.queryAllByRole('textbox');
       textboxes.forEach(textbox => {
@@ -183,19 +190,20 @@ describe('ERP Approval Step', () => {
       const proceedToSplButton = component.queryByText(/Not Included in the SPL/);
       expect(proceedToSplButton).toBeDisabled();
     });
-    it('form fields are not disabled', () => {
+    it('correct form fields are disabled', () => {
       const component = render(getApprovalStep(getStore(project)));
       const textboxes = component.queryAllByRole('textbox');
       textboxes.forEach(textbox => {
-        if (
-          textbox.id === 'datepicker-requestForSplReceivedOn' ||
-          textbox.id === 'datepicker-approvedForSplOn'
-        ) {
-          expect(textbox).toBeVisible();
+        expect(textbox).toBeVisible();
+        if (textbox.id.includes('Spl')) {
+          //only disabled textboxes are SPL related datepickers and erp emails text
           expect(textbox).toBeDisabled();
         } else {
-          expect(textbox).toBeVisible();
-          expect(textbox).not.toBeDisabled();
+          if (textbox.id.includes('[22]')) {
+            expect(textbox).toBeDisabled();
+          } else {
+            expect(textbox).not.toBeDisabled();
+          }
         }
       });
     });
