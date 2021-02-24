d62de266493497d1b5603dce79f73529c44ee0c1
Alex Galbraith <33818575+agalbrai@users.noreply.github.com>
2021-02-24 09:48:00 -0800

    PA 2797 Show full agency name (#976)
    move function to usecodeLookups

--- frontend/src/components/maps/leaflet/InfoSlideOut/InfoContent.tsx
+++ frontend/src/components/maps/leaflet/InfoSlideOut/InfoContent.tsx
@@ -10,6 +10,7 @@ import styled from 'styled-components';
 import { ThreeColumnItem } from './ThreeColumnItem';
 import variables from '_variables.module.scss';
 import { PropertyTypes } from 'constants/propertyTypes';
+import useCodeLookups from 'hooks/useLookupCodes';
 
 /**
  * Compare two dates to evaluation which is earlier.
@@ -80,6 +81,9 @@ export const InfoContent: React.FC<IInfoContent> = ({
   const isParcel =
     propertyTypeId !== null &&
     [PropertyTypes.PARCEL, PropertyTypes.SUBDIVISION].includes(propertyTypeId);
+
+  const lookupCodes = useCodeLookups();
+
   return (
     <>
       <ListGroup>
@@ -95,17 +99,17 @@ export const InfoContent: React.FC<IInfoContent> = ({
                 <>
                   <ThreeColumnItem
                     leftSideLabel={'Ministry'}
-                    rightSideItem={propertyInfo?.agency}
+                    rightSideItem={lookupCodes.getAgencyFullName(propertyInfo?.agency)}
                   />
                   <ThreeColumnItem
                     leftSideLabel={'Owning agency'}
-                    rightSideItem={propertyInfo.subAgency}
+                    rightSideItem={lookupCodes.getAgencyFullName(propertyInfo.subAgency)}
                   />
                 </>
               ) : (
                 <ThreeColumnItem
                   leftSideLabel={'Owning ministry'}
-                  rightSideItem={propertyInfo?.agency}
+                  rightSideItem={lookupCodes.getAgencyFullName(propertyInfo?.agency)}
                 />
               )}
             </>
