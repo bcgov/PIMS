ece43075d5162505741a37a4eb30fcf04640d3d4
Jeremy Foster <jeremymfoster@hotmail.com>
2021-03-01 13:26:08 -0800

    PA-2848 Fixed Not in SPL Workflow (#1004)

---
+++ backend/dal/Migrations/v01.08.07/Up/PostUp/01-ProjectStatusTransitions.sql
@@ -0,0 +1,19 @@
+PRINT 'Adding ProjectStatusTransitions'
+
+INSERT INTO dbo.[ProjectStatusTransitions] (
+    [FromWorkflowId]
+    , [FromStatusId]
+    , [Action]
+    , [ToWorkflowId]
+    , [ToStatusId]
+    , [ValidateTasks]
+) VALUES
+-- Allow progression from Not in SPL to Transfer within GRE
+(
+    4 -- 'ASSESS-EX-DISPOSAL'
+    , 22 -- Not in SPL
+    , 'Transfer within GRE'
+    , 5 -- ERP
+    , 20 -- Transferred within GRE
+    , 0
+)
