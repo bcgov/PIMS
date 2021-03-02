671b5bc1bf4cf5eb76213da68723f902037ca0e3
fosol <jeremymfoster@hotmail.com>
2021-03-01 16:01:28 -0800

    DB Migration v01.08.00 Merge

--- backend/dal/Migrations/v01.08.00/Up/Postup/01-ProjectStatusTransitions.sql
+++ backend/dal/Migrations/v01.08.00/Up/Postup/01-ProjectStatusTransitions.sql
@@ -8,6 +8,15 @@ INSERT INTO dbo.[ProjectStatusTransitions] (
     , [ToStatusId]
     , [ValidateTasks]
 ) VALUES
+-- Allow progression from Not in SPL to Disposed
+(
+    5 -- ERP
+    , 22 -- Not in SPL
+    , 'Dispose Project'
+    , 5 -- ERP
+    , 32 -- Disposed
+    , 0
+),
 -- Allow progression from Not in SPL to Transfer within GRE
 (
     5 -- ERP
@@ -16,4 +25,14 @@ INSERT INTO dbo.[ProjectStatusTransitions] (
     , 5 -- ERP
     , 20 -- Transferred within GRE
     , 0
+),
+-- Allow progression from Not in SPL to Transfer within GRE
+(
+    4 -- 'ASSESS-EX-DISPOSAL'
+    , 22 -- Not in SPL
+    , 'Transfer within GRE'
+    , 5 -- ERP
+    , 20 -- Transferred within GRE
+    , 0
 )
+
