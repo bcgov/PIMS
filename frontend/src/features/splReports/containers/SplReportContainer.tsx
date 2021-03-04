4407497cd7f18d2ad536b505ab416c4b65679376
Goeme Nthomiwa <gnthomiwa@live.ca>
2021-03-04 09:56:32 -0800

    Add filtering to SPL reports (#1020)

--- frontend/src/features/splReports/containers/SplReportContainer.tsx
+++ frontend/src/features/splReports/containers/SplReportContainer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import SplReportLayout from '../components/SplReportLayout';
 import { useState, useEffect } from 'react';
-import { IReport, ISnapshot } from '../interfaces';
+import { IReport, ISnapshot, ISnapshotFilter } from '../interfaces';
 import { useProjectSnapshotApi } from '../hooks/useProjectSnapshotApi';
 import _ from 'lodash';
 import { generateUtcNowDateTime } from 'utils';
@@ -11,6 +11,19 @@ import { toast } from 'react-toastify';
 import { useRouterReport } from '../hooks/useRouterReport';
 
 interface ISplReportContainerProps {}
+
+const SplReportContainerContext = React.createContext<{
+  snapshotFilter: ISnapshotFilter;
+  setSnapshotFilter: (filter: ISnapshotFilter) => void;
+}>({
+  snapshotFilter: {
+    fiscalYear: '',
+    agency: '',
+    projectNumber: '',
+    sortBy: { projectId: 'asc', snapshotOn: 'desc' },
+  },
+  setSnapshotFilter: _.noop,
+});
 
 /**
  * Container providing api and event handling logic for spl reports.
@@ -33,17 +46,55 @@ const SplReportContainer: React.FunctionComponent<ISplReportContainerProps> = ()
     updateProjectReport,
     exportReport,
   } = useProjectSnapshotApi();
+  const [snapshotFilter, setSnapshotFilter] = React.useState<ISnapshotFilter>({
+    projectNumber: '',
+    agency: '',
+    fiscalYear: '',
+    sortBy: { projectId: 'asc', snapshotOn: 'desc' },
+  });
 
   const id = currentReport?.id;
   useEffect(() => {
     const fetch = async () => {
       if (id) {
-        const data = await getProjectReportSnapshotsById(id);
-        setSnapshots(data);
+        let snapshots = await getProjectReportSnapshotsById(id);
+        snapshots = snapshots.map(
+          s =>
+            ({
+              ...s,
+              project: { ...s.project, agencyName: s.project?.agency || s.project?.subAgency },
+            } as ISnapshot),
+        );
+        snapshots = _.orderBy(
+          snapshots,
+          _.keys(snapshotFilter.sortBy),
+          _.keys(snapshotFilter.sortBy).map(k => snapshotFilter.sortBy[k]) as ('asc' | 'desc')[],
+        );
+
+        if (snapshotFilter.projectNumber) {
+          snapshots = _.filter(
+            snapshots,
+            snapshot => snapshot.project?.projectNumber === snapshotFilter.projectNumber,
+          );
+        }
+        if (snapshotFilter.agency) {
+          snapshots = _.filter(
+            snapshots,
+            snapshot => snapshot.project?.agencyId === snapshotFilter.agency,
+          );
+        }
+        if (snapshotFilter.fiscalYear) {
+          snapshots = _.filter(
+            snapshots,
+            snapshot =>
+              Number(snapshot.project?.actualFiscalYear) === Number(snapshotFilter.fiscalYear),
+          );
+        }
+        setSnapshots(snapshots);
       }
     };
     fetch();
-  }, [id, getProjectReportSnapshotsById]);
+  }, [id, getProjectReportSnapshotsById, snapshotFilter]);
 
   const getReports = React.useCallback(async () => {
     const data = await getProjectReports();
@@ -123,54 +174,60 @@ const SplReportContainer: React.FunctionComponent<ISplReportContainerProps> = ()
   };
 
   return (
-    <SplReportLayout
-      showSidebar={showSidebar}
-      setShowSidebar={setShowSidebar}
-      onOpen={report => setCurrentReport(report)}
-      onDelete={onDelete}
-      onFinal={onFinal}
-      onRefresh={refreshSnapshots}
-      onSave={onSave}
-      onAdd={addReport}
-      onExport={exportReport}
-      onFromChange={(id: number) => {
-        changeFrom({ ...currentReport, from: _.find(reports, { id: id })?.to } as IReport);
-      }}
-      reports={reports}
-      snapshots={snapshots}
-      currentReport={currentReport}
+    <SplReportContainerContext.Provider
+      value={{ snapshotFilter: snapshotFilter, setSnapshotFilter }}
     >
-      <GenericModal
-        display={!!reportIdToDelete}
-        title="Confirm Delete"
-        okButtonText="Delete"
-        okButtonVariant="danger"
-        cancelButtonText="Cancel"
-        cancelButtonVariant="secondary"
-        message="Are you sure you want to delete this report? this action cannot be undone."
-        handleOk={() => {
-          const report = _.find(reports, { id: reportIdToDelete });
-          report && deleteReport(report);
-          setReportIdToDelete(undefined);
+      <SplReportLayout
+        showSidebar={showSidebar}
+        setShowSidebar={setShowSidebar}
+        onOpen={report => setCurrentReport(report)}
+        onDelete={onDelete}
+        onFinal={onFinal}
+        onRefresh={refreshSnapshots}
+        onSave={onSave}
+        onAdd={addReport}
+        onExport={exportReport}
+        onFromChange={(id: number) => {
+          changeFrom({ ...currentReport, from: _.find(reports, { id: id })?.to } as IReport);
         }}
-        handleCancel={() => setReportIdToDelete(undefined)}
-      ></GenericModal>
-      <GenericModal
-        display={!!reportToSave}
-        title="Really Remove 'Final' Flag?"
-        okButtonText="Remove 'Final'"
-        okButtonVariant="danger"
-        cancelButtonText="Cancel"
-        cancelButtonVariant="secondary"
-        message="Are you sure you want to remove the 'Final' flag on this report? Removing the 'Final' flag will allow this report to be modified or deleted"
-        handleOk={() => {
-          reportToSave && updateReport(reportToSave);
-          setReportToSave(undefined);
-        }}
-        handleCancel={() => setReportToSave(undefined)}
-      ></GenericModal>
-    </SplReportLayout>
+        reports={reports}
+        snapshots={snapshots}
+        currentReport={currentReport}
+      >
+        <GenericModal
+          display={!!reportIdToDelete}
+          title="Confirm Delete"
+          okButtonText="Delete"
+          okButtonVariant="danger"
+          cancelButtonText="Cancel"
+          cancelButtonVariant="secondary"
+          message="Are you sure you want to delete this report? this action cannot be undone."
+          handleOk={() => {
+            const report = _.find(reports, { id: reportIdToDelete });
+            report && deleteReport(report);
+            setReportIdToDelete(undefined);
+          }}
+          handleCancel={() => setReportIdToDelete(undefined)}
+        ></GenericModal>
+        <GenericModal
+          display={!!reportToSave}
+          title="Really Remove 'Final' Flag?"
+          okButtonText="Remove 'Final'"
+          okButtonVariant="danger"
+          cancelButtonText="Cancel"
+          cancelButtonVariant="secondary"
+          message="Are you sure you want to remove the 'Final' flag on this report? Removing the 'Final' flag will allow this report to be modified or deleted"
+          handleOk={() => {
+            reportToSave && updateReport(reportToSave);
+            setReportToSave(undefined);
+          }}
+          handleCancel={() => setReportToSave(undefined)}
+        ></GenericModal>
+      </SplReportLayout>
+    </SplReportContainerContext.Provider>
   );
 };
+
+export const useSplReportContext = () => React.useContext(SplReportContainerContext);
 
 export default SplReportContainer;
