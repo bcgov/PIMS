a329f5c989ea30e7f679decc9fb7cc5655b6faba
Jeremy Foster <jeremymfoster@hotmail.com>
2021-03-08 16:42:56 -0800

    PA-2885 Updated Notification AddressTo (#1040)

--- backend/dal/Migrations/v01.09.03/Up/PostUp/01-NotificationTemplates.sql
+++ backend/dal/Migrations/v01.09.03/Up/PostUp/01-NotificationTemplates.sql
@@ -2,9 +2,13 @@ PRINT 'Update NotificationTemplates'
 
 -- Add a default if the address to is null.
 UPDATE dbo.[NotificationTemplates]
-SET [Body] = REPLACE([Body], 'Dear @Model.ToAgency.AddressTo', '@(Model.ToAgency.AddressTo ?? "Good Morning")')
+SET [Body] = REPLACE([Body], 'Dear @Model.ToAgency.AddressTo', '@(Model.ToAgency.AddressTo ?? "Good morning / Good afternoon")')
+
+-- Add a default if the address to is null.
+UPDATE dbo.[NotificationTemplates]
+SET [Body] = REPLACE([Body], 'Good afternoon', '@(Model.ToAgency.AddressTo ?? "Good morning / Good afternoon")')
 
 -- Default all emails addressed to.
 UPDATE dbo.[Agencies]
-SET [AddressTo] = 'Good Morning'
+SET [AddressTo] = 'Good morning / Good afternoon'
 
