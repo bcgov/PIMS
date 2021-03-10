ca402cc3b35a82c516c6c2a28523a767a536bbc3
devinleighsmith <41091511+devinleighsmith@users.noreply.github.com>
2021-03-09 16:20:58 -0800

    PA-2899 corrections from test/dev config. (#1023)

--- backend/api/Helpers/Logging/LoggerExtension.cs
+++ backend/api/Helpers/Logging/LoggerExtension.cs
@@ -3,7 +3,6 @@ using System;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-using Serilog.Sinks.Elasticsearch;
 
 namespace Pims.Api.Helpers.Logging
 {
@@ -14,31 +13,19 @@ namespace Pims.Api.Helpers.Logging
         )
         {
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            if (environment != null && !environment.EndsWith("Local")) {
+            if (environment != null && !environment.EndsWith("Local"))
+            {
 
                 Log.Logger = new LoggerConfiguration()
                     .Enrich.FromLogContext()
                     .Enrich.WithMachineName()
                     .WriteTo.Debug()
                     .WriteTo.Console()
-                    .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
                     .Enrich.WithProperty("Environment", environment)
                     .ReadFrom.Configuration(configuration)
                     .CreateLogger();
             }
             return services;
-        }
-
-        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfiguration configuration, string environment)
-        {
-            return new ElasticsearchSinkOptions(new Uri(configuration["Elastic:Uri"]))
-            {
-                ModifyConnectionSettings = x => x.BasicAuthentication(
-                    configuration["Elastic:Username"],
-                    configuration["Elastic:Password"]),
-                AutoRegisterTemplate = true,
-                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
-            };
         }
     }
 }
