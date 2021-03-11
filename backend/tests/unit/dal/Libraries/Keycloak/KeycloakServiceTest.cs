bb69bb420b0f11665c2f7ba66ff765386f75f373
Jeremy Foster <jeremymfoster@hotmail.com>
2021-03-11 11:53:34 -0800

    PA-2708 Fixed Keycloak User Administration (#1063)

--- backend/tests/unit/dal/Libraries/Keycloak/KeycloakServiceTest.cs
+++ backend/tests/unit/dal/Libraries/Keycloak/KeycloakServiceTest.cs
@@ -11,7 +11,7 @@ using System.Net;
 using System.Net.Http;
 using Xunit;
 
-namespace Pims.Dal.Test.Libraries.Ches
+namespace Pims.Dal.Test.Libraries.Keycloak
 {
     [Trait("category", "unit")]
     [Trait("category", "keycloak")]
