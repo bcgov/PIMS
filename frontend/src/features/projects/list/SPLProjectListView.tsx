9967ab122091ed86d2ad76d06f8bdafd9dd9a244
Lawrence Lau <lawrence.lau@gov.bc.ca>
2021-06-22 15:54:24 -0700

    Initial files for PIMS-21 need to fix the test

---
+++ frontend/src/features/projects/list/SPLProjectListView.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ProjectListView } from '.';
+
+/**
+ * A list view paging table to search for projects that have been submitted for assessment.
+ * @returns SPLProjectListView component.
+ */
+
+export const SPLProjectListView = () => {
+  console.info('test');
+  return (
+    <ProjectListView
+      filterable={false}
+      title="Surplus Property Program Projects - SPL Projects"
+      defaultFilter={{ statusId: '21,40,41,42,43' }}
+    />
+  );
+};
+
+export default SPLProjectListView;
