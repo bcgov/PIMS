583d6b416c4d8760c6e54a7713d298313622067e
Lawrence Lau <lawrence.lau@gov.bc.ca>
2021-09-21 14:06:58 -0700

    Changing the RPD_IMIT email to the [To] instead of the CC field and adding v01.13.01 migration

---
+++ backend/dal/Migrations/v01.13.01/Up/PostUp/01-NotificationTemplates.sql
@@ -0,0 +1,6 @@
+PRINT 'Updating NotificationTemplates'
+
+-- Include the CITZ_RPD_IMIT_HELP@gov.bc.ca email to the Access Request notification
+UPDATE dbo.[NotificationTemplates]
+SET [To] = 'CITZ_RPD_IMIT_HELP@gov.bc.ca' 
+WHERE [Id] = 15 and [Name] = 'Access Request'
