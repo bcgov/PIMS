5505fb39cbed9c12da81c155521574e7f9a7994e
jdtoombs <jdtoombs@gmail.com>
2021-11-12 14:57:49 -0800

    Maintain checked state on save

--- frontend/src/features/projects/common/components/NotificationCheck.tsx
+++ frontend/src/features/projects/common/components/NotificationCheck.tsx
@@ -4,16 +4,28 @@ import * as React from 'react';
 export interface INotificationCheckProps {
   label?: string;
   field: string;
+  checkedState?: boolean;
+  setCheckedState?: Function;
 }
 
-export default function NotificationCheck({ label, field }: INotificationCheckProps) {
+export default function NotificationCheck({
+  label,
+  field,
+  checkedState,
+  setCheckedState,
+}: INotificationCheckProps) {
   return (
     <>
       <Form.Row>
         <h3>Notifications</h3>
       </Form.Row>
       <Form.Row>
-        <Check postLabel={label} field={field} />
+        <Check
+          postLabel={label}
+          field={field}
+          setCheckedState={setCheckedState}
+          checkedState={checkedState}
+        />
       </Form.Row>
     </>
   );
