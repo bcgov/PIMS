d0bfa72a4b8894f35ea4890a1c5da4b70b2b86fa
Sharala-Perumal <Sharala.Perumal@gov.bc.ca>
2022-03-01 13:52:15 -0800

    updating sqft with Sq. M

--- backend/dal/Migrations/v01.15.00/Up/PostUp/01-NotificationTemplates.sql
+++ backend/dal/Migrations/v01.15.00/Up/PostUp/01-NotificationTemplates.sql
@@ -5,7 +5,3 @@ UPDATE [pims].dbo.[NotificationTemplates]
 SET [Body] = REPLACE([Body], 'sqft', 'Sq. M') 
 WHERE Body LIKE '%sqft%';
 
--- Replace the instances thats shows sqM with Sq. M
-UPDATE [pims].dbo.[NotificationTemplates]
-SET [Body] = REPLACE([Body], 'sqM', 'Sq. M')  
-WHERE Body LIKE '%sqM%';
