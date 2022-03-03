7c14ee666a59dcb44601ea8fdd5c3ed246f78076
LawrenceLau2020 <lawrence.lau@gov.bc.ca>
2022-03-03 08:49:56 -0800

    adding building and land assessed dates, adding UpdatedOn, new db migration, changes to the property model and mapping, removing market value from report & fixing dates

--- backend/dal/Migrations/20220308190231_v01.16.00.Designer.cs
+++ backend/dal/Migrations/20220308190231_v01.16.00.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Pims.Dal;
@@ -10,9 +11,10 @@ using Pims.Dal;
 namespace Pims.Dal.Migrations
 {
     [DbContext(typeof(PimsContext))]
-    partial class PimsContextModelSnapshot : ModelSnapshot
+    [Migration("20220308190231_v01.16.00")]
+    partial class v011600
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
