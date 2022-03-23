4729782659f7da5a51456daa2024d11835dfc3bf
LawrenceLau2020 <68400651+LawrenceLau2020@users.noreply.github.com>
2022-03-23 08:01:07 -0700

    PIMS-77: Add filter to SPL view of projects (#1223)
    * changes to the property filters
    
    * fixes on the parent select and status filter
    
    * Fixing bug and updating snapshot

--- frontend/src/features/projects/list/ProjectApprovalRequestListView.tsx
+++ frontend/src/features/projects/list/ProjectApprovalRequestListView.tsx
@@ -5,13 +5,30 @@ import { ProjectListView } from '.';
  * A list view paging table to search for projects that have been submitted for assessment.
  * @returns ProjectApprovalRequestListView component.
  */
+
+interface IProjectFilterState {
+  name?: string;
+  statusId?: string[];
+  agencyId?: string;
+  agencies?: number[];
+  fiscalYear?: number | '';
+}
+
+const initialValues: IProjectFilterState = {
+  name: '',
+  statusId: ['7', '8'],
+  agencyId: '',
+  agencies: [],
+  fiscalYear: '',
+};
+
 export const ProjectApprovalRequestListView = () => {
   console.info('test');
   return (
     <ProjectListView
       filterable={false}
       title="Surplus Property Program Projects - Approval Requests"
-      defaultFilter={{ statusId: '7,8' }}
+      defaultFilter={initialValues}
     />
   );
 };
