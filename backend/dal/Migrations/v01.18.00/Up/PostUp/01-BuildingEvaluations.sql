254e5171e8993a38daac0ea1d4909cbc52b4a6d1
Lawrence Lau <lawrence.lau@gov.bc.ca>
2022-04-05 14:57:38 -0700

    preventing a foreign key constraint

--- backend/dal/Migrations/v01.18.00/Up/PostUp/01-BuildingEvaluations.sql
+++ backend/dal/Migrations/v01.18.00/Up/PostUp/01-BuildingEvaluations.sql
@@ -7,6 +7,5 @@ WHERE (CreatedOn >= '2022-04-01' AND Date = '2021-01-01') OR (UpdatedOn >= '2022
 
 UPDATE [pims].[dbo].[ParcelEvaluations]
 SET Date = DATEADD(year, 1, Date)
-  WHERE ((CreatedOn >= '2022-04-01' AND Date = '2021-01-01') OR (UpdatedOn >= '2022-04-01' AND Date = '2021-01-01')
-    OR (UpdatedById = 'C290F478-B483-4199-9F4E-7D2939C584F5' AND ParcelId IN (415, 416)))
+  WHERE ((CreatedOn >= '2022-04-01' AND Date = '2021-01-01') OR (UpdatedOn >= '2022-04-01' AND Date = '2021-01-01'))
     AND (ParcelId NOT IN (9734, 6427));
