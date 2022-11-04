21a3d7a63b3129d72349e1f2d52a0d32dc022578
Zachary Bourque <Zachary.Bourque@gov.bc.ca>
2022-11-04 11:12:56 -0700

    Modified frontend to use Keycloak Gold.

--- frontend/src/utils/createKeycloakInstance.ts
+++ frontend/src/utils/createKeycloakInstance.ts
@@ -1,20 +1,16 @@
 import IKeycloakConfig from 'interfaces/IKeycloakConfig';
 import Keycloak from 'keycloak-js';
 
-/**
- * Fetches the configuration file `/keycloak.json` and creates a KeycloakInstance for it.
- * Overrides file configuration with environment variables.
- * @returns Promise<KeycloakInstance>
- */
 export const createKeycloakInstance = async () => {
   const response = await fetch('/keycloak.json');
   const data = await response.json();
   const config = data as IKeycloakConfig;
-
   //@ts-ignore
-  return new Keycloak({
-    url: process.env.REACT_APP_KEYCLOAK_AUTH_SERVER_URL ?? config['auth-server-url'],
-    realm: config.realm,
-    clientId: config.resource,
-  } as Keycloak.KeycloakConfig);
+  const kc = await new Keycloak({
+    url: 'https://dev.loginproxy.gov.bc.ca/auth',
+    realm: 'standard',
+    clientId: 'pims-local-test-4292',
+  });
+  await kc.init({ pkceMethod: 'S256', redirectUri: window.location.href, idpHint: 'idir' });
+  return kc;
 };
