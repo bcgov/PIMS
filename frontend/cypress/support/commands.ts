d82358d64d3ada76ebc00d5783a7b07af004dd30
Brady Mitchell <braden.jr.mitch@gmail.com>
2022-11-15 08:35:31 -0800

    Basic setup with keycloak login working

---
+++ frontend/cypress/support/commands.ts
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+// ***********************************************
+// For comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(): Chainable<Element>;
+//     }
+//   }
+// }
+
+// Cypress.Commands.add('login', () => {
+//   cy.kcLogin('admin');
+// });
+
+export = {};
