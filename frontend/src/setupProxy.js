365ae191c01e054f19a2d127b52d3f6915ef6a57
Brady Mitchell <braden.jr.mitch@gmail.com>
2022-11-17 09:35:45 -0800

    Upgraded http-proxy-middleware from 0.21.0 to 2.0.6

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,9 +1,9 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
     '/api',
-    proxy({
+    createProxyMiddleware({
       target: process.env.API_URL || 'http://backend:8080/',
       changeOrigin: true,
       secure: false,
