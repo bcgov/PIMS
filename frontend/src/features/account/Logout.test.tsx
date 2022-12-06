b5ec9dd09e833d4fdc52c9d793603c2d2701ef7e
Brady Mitchell <braden.jr.mitch@gmail.com>
2022-12-06 00:31:06 -0800

    Fixed Logout test

--- frontend/src/features/account/Logout.test.tsx
+++ frontend/src/features/account/Logout.test.tsx
@@ -1,28 +1,20 @@
 import { useKeycloak } from '@react-keycloak/web';
-import { cleanup, render, waitFor } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
+import { cleanup, render } from '@testing-library/react';
 import { useConfiguration } from 'hooks/useConfiguration';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import * as Router from 'react-router';
 
 import { LogoutPage } from './Logout';
 
 jest.mock('@react-keycloak/web');
 jest.mock('hooks/useConfiguration');
-// @ts-ignore
-delete window.location;
-// @ts-ignore
-window.location = { replace: jest.fn() };
 
 describe('logout', () => {
-  const history = createMemoryHistory();
-  let { location } = window;
+  const navigate = jest.fn();
 
-  beforeAll(() => {
-    window.location = { replace: jest.fn() } as any;
+  beforeEach(() => {
+    jest.spyOn(Router, 'useNavigate').mockImplementation(() => navigate);
   });
-
-  afterAll(() => (window.location = location));
 
   afterEach(() => {
     cleanup();
@@ -32,23 +24,29 @@ describe('logout', () => {
     (useKeycloak as jest.Mock).mockReturnValue({ keycloak: { authenticated: false } });
     (useConfiguration as jest.Mock).mockReturnValue({ siteMinderLogoutUrl: undefined });
 
-    render(<LogoutPage />);
+    render(
+      <Router.MemoryRouter>
+        <LogoutPage />
+      </Router.MemoryRouter>,
+    );
 
-    expect(location.pathname).toBe('/login');
+    expect(navigate).toHaveBeenCalledWith('/login');
   });
 
-  it('should redirect to siteminder logout page', async () => {
+  it('should redirect to siteminder logout page', () => {
     (useKeycloak as jest.Mock).mockReturnValue({ keycloak: { authenticated: false } });
     (useConfiguration as jest.Mock).mockReturnValue({
       siteMinderLogoutUrl: 'http://fakesiteminder.com',
     });
 
     render(
-      <MemoryRouter initialEntries={[history.location]}>
+      <Router.MemoryRouter>
         <LogoutPage />
-      </MemoryRouter>,
+      </Router.MemoryRouter>,
     );
 
-    await waitFor(() => expect(window.location.replace).toHaveBeenCalledTimes(1));
+    expect(navigate).toHaveBeenCalledWith(
+      'http://fakesiteminder.com?returl=undefined/login&retnow=1',
+    );
   });
 });
