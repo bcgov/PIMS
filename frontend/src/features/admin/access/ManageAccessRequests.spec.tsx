3324136f737210d956f70d983bb1989c80f7ade4
Brady Mitchell <braden.jr.mitch@gmail.com>
2023-01-17 16:18:00 -0800

    Resolved 3 of 17 failed test suites

--- frontend/src/features/admin/access/ManageAccessRequests.spec.tsx
+++ frontend/src/features/admin/access/ManageAccessRequests.spec.tsx
@@ -44,6 +44,19 @@ const lCodes = {
   ] as ILookupCode[],
 };
 
+Enzyme.configure({ adapter: new Adapter() });
+
+const history = createMemoryHistory();
+history.push('admin');
+const mockStore = configureMockStore([thunk]);
+
+const lCodes = {
+  lookupCodes: [
+    { name: 'agencyVal', id: '1', isDisabled: false, type: API.AGENCY_CODE_SET_NAME },
+    { name: 'roleVal', id: '1', isDisabled: false, type: API.ROLE_CODE_SET_NAME },
+  ] as ILookupCode[],
+};
+
 const accessRequests = {
   page: 1,
   total: 2,
