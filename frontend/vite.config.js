de9fefde2fda8ec391ecbe72a3fa54f7429f06b0
Brady Mitchell <braden.jr.mitch@gmail.com>
2023-06-02 09:09:17 -0700

    Vite for Production Build (#1515)

---
+++ frontend/vite.config.js
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgrPlugin from 'vite-plugin-svgr';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  build: {
+    rollupOptions: {
+      input: '/public/index.html',
+    },
+    outDir: 'build',
+  },
+});
