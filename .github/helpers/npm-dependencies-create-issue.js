2549b79909ccd93c4bcf39134488f674ca1adec6
Brady Mitchell <braden.jr.mitch@gmail.com>
2023-06-07 14:27:24 -0700

    PIMS-654

--- .github/helpers/npm-dependencies-create-issue.js
+++ .github/helpers/npm-dependencies-create-issue.js
@@ -1,15 +1,15 @@
 const { createIssue, closeIssue, findIssueByTitle } = require('./github-api-requests');
-const { GITHUB_OWNER, GITHUB_REPO, ISSUE_TITLE, ISSUE_BODY } = process.env;
+const { ISSUE_TITLE, ISSUE_BODY } = process.env;
 
 (async () => {
   // Check for existing Issue.
-  const existingIssueNumber = await findIssueByTitle(GITHUB_OWNER, GITHUB_REPO, ISSUE_TITLE);
+  const existingIssueNumber = await findIssueByTitle(ISSUE_TITLE);
 
   if (existingIssueNumber && Number(existingIssueNumber) !== 'NaN') {
     // Close old Issue.
-    await closeIssue(GITHUB_OWNER, GITHUB_REPO, Number(existingIssueNumber));
+    await closeIssue(Number(existingIssueNumber));
   }
 
   // Create new Issue.
-  await createIssue(GITHUB_OWNER, GITHUB_REPO, ISSUE_TITLE, ISSUE_BODY);
+  await createIssue(ISSUE_TITLE, ISSUE_BODY);
 })();
