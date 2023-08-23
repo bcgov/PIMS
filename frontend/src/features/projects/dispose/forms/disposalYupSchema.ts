65e63da9cdf7631fc6b9c9991f3ffa773a42b521
dbarkowsky <dylanbarkowsky@gmail.com>
2023-08-23 11:17:31 -0700

    update yup syntax for disposalYupSchema

--- frontend/src/features/projects/dispose/forms/disposalYupSchema.ts
+++ frontend/src/features/projects/dispose/forms/disposalYupSchema.ts
@@ -1,20 +1,21 @@
 import { Classifications } from 'constants/classifications';
+import { ObjectSchema } from 'yup';
 import * as Yup from 'yup';
 
-export const ApprovalConfirmationStepSchema = Yup.object().shape({
+export const ApprovalConfirmationStepSchema = Yup.object({
   confirmation: Yup.boolean()
     .oneOf([true], 'You must confirm approval before continuing.')
     .required('Required'),
 });
 
-export const DocumentationStepSchema = Yup.object().shape({
+export const DocumentationStepSchema = Yup.object({
   email: Yup.string().email().max(100, 'Email must be less than 100 characters'),
   firstName: Yup.string().max(100, 'First Name must be less than 100 characters'),
   middleName: Yup.string().max(100, 'Middle Name must be less than 100 characters'),
   lastName: Yup.string().max(100, 'Last Name must be less than 100 characters'),
 });
 
-export const UpdateInfoStepYupSchema = Yup.object().shape({
+export const UpdateInfoStepYupSchema = Yup.object({
   netBook: Yup.number().required('Required'),
   market: Yup.number().required('Required'),
   assessed: Yup.number().required('Required'),
@@ -30,15 +31,15 @@ export const UpdateInfoStepYupSchema = Yup.object().shape({
   ),
 });
 
-export const DenyProjectYupSchema = Yup.object().shape({
+export const DenyProjectYupSchema = Yup.object({
   publicNote: Yup.string().required('Shared note must contain a reason before denying project.'),
 });
 
-export const ApproveExemptionRequestSchema = Yup.object().shape({
+export const ApproveExemptionRequestSchema = Yup.object({
   exemptionApprovedOn: Yup.date().required('Required'),
 });
 
-export const GreTransferStepYupSchema = Yup.object().shape({
+export const GreTransferStepYupSchema = Yup.object({
   properties: Yup.array().of(
     Yup.object().shape({
       classificationId: Yup.number().test(
@@ -51,24 +52,24 @@ export const GreTransferStepYupSchema = Yup.object().shape({
   ),
 });
 
-export const SelectProjectPropertiesStepYupSchema = Yup.object().shape({
+export const SelectProjectPropertiesStepYupSchema = Yup.object({
   properties: Yup.array()
     .required('You must select at least one property')
     .min(1, 'You must select at least one property'),
 });
 
-export const ProjectDraftStepYupSchema = Yup.object().shape({
+export const ProjectDraftStepYupSchema = Yup.object({
   name: Yup.string().max(100, 'Name allows a maximum of 100 characters.').required('Required'),
   description: Yup.string().max(1000, 'Description allows a maximum of 1000 characters.'),
   note: Yup.string().max(2000, 'Note allows a maximum of 2000 characters.'),
 });
 
-export const EnhancedReferralExemptionSchema = Yup.object().shape({
-  exemptionRationale: Yup.string().when(
-    'exemptionRequested',
-    (exemptionRequested: boolean, schema: any) =>
-      exemptionRequested
-        ? schema.required('Rationale is required when applying for an exemption.')
-        : schema,
-  ),
+export const EnhancedReferralExemptionSchema = Yup.object({
+  exemptionRationale: Yup.string().when('exemptionRequested', ((
+    exemptionRequested: boolean,
+    schema: ObjectSchema<any>,
+  ) =>
+    exemptionRequested
+      ? schema.required('Rationale is required when applying for an exemption.')
+      : schema) as () => ObjectSchema<any>),
 });
