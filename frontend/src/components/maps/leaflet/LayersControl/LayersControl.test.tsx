52c9487000b861747796bfb9ae1da449f94548fe
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2023-08-30 08:56:06 -0700

    [PIMS-140] Update react-leaflet (#1682)

--- frontend/src/components/maps/leaflet/LayersControl/LayersControl.test.tsx
+++ frontend/src/components/maps/leaflet/LayersControl/LayersControl.test.tsx
@@ -99,6 +99,7 @@ describe('LayersControl View', () => {
     const { ready } = setup(<Template />, setMap);
     await waitFor(() => ready);
     await waitFor(() => expect(mapInstance).toBeDefined());
+
     expect(isLayerVisible('parcelBoundaries', mapInstance)).toBeDefined();
     expect(isLayerVisible('municipalities', mapInstance)).toBeUndefined();
   });
