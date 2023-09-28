42044d1889e79e80b608c6e612cd861bf9a4b3b5
Lawrence Lau <lawrence.lau@gov.bc.ca>
2023-09-28 14:54:56 -0700

    PIMS-253

---
+++ backend/dal/Migrations/20230928182637_v02.06.14.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Pims.Dal.Helpers.Migrations;
+
+#nullable disable
+
+namespace Pims.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class v020614 : SeedMigration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            PreUp(migrationBuilder);
+
+            PostUp(migrationBuilder);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            PreDown(migrationBuilder);
+
+            PostDown(migrationBuilder);
+        }
+    }
+}
