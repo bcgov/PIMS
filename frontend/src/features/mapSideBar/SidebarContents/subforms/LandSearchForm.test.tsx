07c5b0e1356a5179df0c9bc1f79f788e05faec0d
dbarkowsky <dylanbarkowsky@gmail.com>
2023-10-05 16:36:41 -0700

    Added blur tests

---
+++ frontend/src/features/mapSideBar/SidebarContents/subforms/LandSearchForm.test.tsx
@@ -0,0 +1,181 @@
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import { ILookupCode } from 'actions/ILookupCode';
+import * as API from 'constants/API';
+import Claims from 'constants/claims';
+import { createMemoryHistory } from 'history';
+import useKeycloakWrapper from 'hooks/useKeycloakWrapper';
+import noop from 'lodash/noop';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import useKeycloakMock from 'useKeycloakWrapperMock';
+
+import LandForm from 'features/mapSideBar/SidebarContents/LandForm';
+import { IParcel } from 'actions/parcelsActions';
+import { userEvent } from '@testing-library/user-event';
+
+const mockStore = configureMockStore([thunk]);
+const history = createMemoryHistory();
+
+const lCodes = {
+  lookupCodes: [
+    { name: 'agencyVal', id: '1', isDisabled: false, type: API.AGENCY_CODE_SET_NAME },
+    { name: 'disabledAgency', id: '2', isDisabled: true, type: API.AGENCY_CODE_SET_NAME },
+    { name: 'roleVal', id: '1', isDisabled: false, type: API.ROLE_CODE_SET_NAME },
+    { name: 'disabledRole', id: '2', isDisabled: true, type: API.ROLE_CODE_SET_NAME },
+    {
+      name: 'Core Operational',
+      id: '0',
+      isDisabled: false,
+      type: API.PROPERTY_CLASSIFICATION_CODE_SET_NAME,
+    },
+    {
+      name: 'Core Strategic',
+      id: '1',
+      isDisabled: false,
+      type: API.PROPERTY_CLASSIFICATION_CODE_SET_NAME,
+    },
+    {
+      name: 'Surplus Active',
+      id: '2',
+      isDisabled: false,
+      type: API.PROPERTY_CLASSIFICATION_CODE_SET_NAME,
+    },
+    {
+      name: 'Surplus Encumbered',
+      id: '3',
+      isDisabled: false,
+      type: API.PROPERTY_CLASSIFICATION_CODE_SET_NAME,
+    },
+    {
+      name: 'Disposed',
+      id: '4',
+      isDisabled: false,
+      type: API.PROPERTY_CLASSIFICATION_CODE_SET_NAME,
+    },
+  ] as ILookupCode[],
+};
+
+const store = mockStore({
+  lookupCode: lCodes,
+  parcel: { propeties: [], draftProperties: [] },
+  usersAgencies: [
+    { id: '1', name: 'agencyVal' },
+    { id: '2', name: 'disabledAgency' },
+  ],
+});
+
+const promise = Promise.resolve();
+
+const userRoles: string[] | Claims[] = ['admin-properties'];
+const userAgencies: number[] = [1];
+const userAgency: number = 1;
+
+jest.mock('hooks/useKeycloakWrapper');
+(useKeycloakWrapper as jest.Mock).mockReturnValue(
+  new (useKeycloakMock as any)(userRoles, userAgencies, userAgency),
+);
+
+// Using LandForm because it contains LandSearchForm
+const getLandForm = (disabled?: boolean, initialValues?: IParcel) => (
+  <Provider store={store}>
+    <MemoryRouter initialEntries={[history.location]}>
+      <LandForm
+        handleGeocoderChanges={() => promise as unknown as Promise<void>}
+        setMovingPinNameSpace={noop}
+        handlePidChange={noop}
+        handlePinChange={noop}
+        isPropertyAdmin={true}
+        disabled={disabled}
+        initialValues={initialValues as any}
+        setLandComplete={noop}
+        setLandUpdateComplete={noop}
+        findMatchingPid={noop as any}
+      />
+    </MemoryRouter>
+  </Provider>
+);
+
+describe('Land Form', () => {
+  it('component renders correctly', () => {
+    const { container } = render(getLandForm());
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('Can switch between tabs in LandSearchForm', async () => {
+    const { queryAllByText, getByText, container } = render(getLandForm());
+    // Should be able to see "PID, PIN" twice each
+    expect(queryAllByText(/PID/).length).toBe(2);
+    expect(queryAllByText(/PIN/).length).toBe(2);
+
+    // Click on second tab
+    const secondTab = container.querySelector('#parcel-marker-tab');
+    await waitFor(() => {
+      fireEvent.click(secondTab!);
+      getByText(/Find a parcel on the map/);
+    });
+    // And should see this phrase
+    expect(queryAllByText(/Find a parcel on the map/).length).toBe(1);
+  });
+
+  // Cannot currently get a value loaded in the clipboard
+  xit('Paste buttons work in LandSearchForm', async () => {
+    const readTextMock = jest.fn(() => '123123123');
+
+    Object.defineProperty(global.navigator, 'clipboard', {
+      value: {
+        readText: readTextMock,
+        then: () => {
+          userEvent.paste('123123123');
+        },
+      },
+    });
+
+    const { container } = render(getLandForm());
+    // const pidField = container.querySelector('#pid-field');
+    const pidPaste = container.querySelector('#pid-paste');
+
+    await waitFor(() => {
+      fireEvent.click(pidPaste!);
+    });
+    // expect(pidField?.innerHTML).toContain('hi');
+    expect(true).toBeTruthy();
+  });
+
+  it('Formatting occurs on field blurs', async () => {
+    const { container } = render(getLandForm());
+    const pidField = container.querySelector('#pid-field');
+    const pinField = container.querySelector('#pin-field');
+
+    // Test PID with empty field
+    fireEvent.blur(pidField!);
+    expect((pidField as HTMLInputElement).value).toBe('');
+
+    // Test PID with value
+    await userEvent.type(pidField!, '123123123');
+    fireEvent.blur(pidField!);
+    expect((pidField as HTMLInputElement).value).toBe('123-123-123');
+
+    // Test PIN with empty field
+    fireEvent.blur(pinField!);
+    expect((pinField as HTMLInputElement).value).toBe('');
+
+    // Test PIN with value
+    await userEvent.type(pinField!, '123');
+    fireEvent.blur(pinField!);
+    expect((pinField as HTMLInputElement).value).toBe('123');
+  });
+
+  // Can't seem to get addressField. It is always null.
+  xit('Address field populates selections when typing', async () => {
+    const { container, queryByText, getByText } = render(getLandForm());
+    const addressField = container.querySelector('#input-data.searchAddress');
+    await userEvent.type(addressField!, '1407 Haultain');
+    await waitFor(() => {
+      getByText('1407 Haultain St, Victoria, BC');
+    });
+    expect(queryByText('1407 Haultain St, Victoria, BC')).toBeInTheDocument();
+  });
+});
