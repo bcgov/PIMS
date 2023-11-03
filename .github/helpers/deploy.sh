51b14719dafef9c297943efefca24a732baebda4
Manish Sihag <manish.sihag@yahoo.com>
2023-11-03 11:44:02 -0700

    minor update

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -3,7 +3,7 @@
 # Use DeploymentConfigs to deploy the application to OpenShift.
 
 
-    oc process -f /home/runner/work/PIMS/PIMS/openshift/templates/"$DC_TEMPLATE" --namespace=$NAMESPACE \
+    oc process -f /home/runner/work/PIMS/PIMS/openshift/templates/"$DC_TEMPLATE" --namespace="ec1236-dev" \
         -p APPLICATION_NAME=$APPLICATION_NAME \
         -p NAMESPACE=$NAMESPACE \
         -p IMAGE_TAG=$IMAGE_TAG \
