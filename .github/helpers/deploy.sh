f4457a18c5ac3a0224907605b3d34aa2431023a8
Manish Sihag <manish.sihag@yahoo.com>
2023-11-06 16:32:57 -0800

    Config Updates

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -5,7 +5,7 @@
 
     oc process -f /home/runner/work/PIMS/PIMS/openshift/templates/"$DC_TEMPLATE" --namespace=$NAMESPACE \
         -p APPLICATION_NAME=$APPLICATION_NAME \
-        -p NAMESPACE=$NAMESPACE \
+        -p LICENSE_PLATE=$LICENSE_PLATE \
         -p IMAGE_TAG=$IMAGE_TAG \
         -p ENVIRONMENT=$ENVIRONMENT | \
         oc apply -f -
