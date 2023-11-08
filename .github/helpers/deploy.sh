76a7e872c3b29908134c8c816bb56a00b429d23a
Manish Sihag <manish.sihag@yahoo.com>
2023-11-08 14:55:34 -0800

    minor update

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -4,7 +4,6 @@
 
 
     oc process -f /home/runner/work/PIMS/PIMS/openshift/templates/"$DC_TEMPLATE" --namespace=$NAMESPACE \
-        -p APPLICATION_NAME=$APPLICATION_NAME \
         -p LICENSE_PLATE=$LICENSE_PLATE \
         -p IMAGE_TAG=$IMAGE_TAG \
         -p ENVIRONMENT=$ENVIRONMENT | \
