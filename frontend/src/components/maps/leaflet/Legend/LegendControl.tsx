9f67254a58e46b0a0de91668a11768165078eda4
dbarkowsky <dylanbarkowsky@gmail.com>
2023-11-17 07:55:10 -0800

    Move LegendButton style to SCSS

--- frontend/src/components/maps/leaflet/Legend/LegendControl.tsx
+++ frontend/src/components/maps/leaflet/Legend/LegendControl.tsx
@@ -1,25 +1,13 @@
 import './LegendControl.scss';
 
-import variables from '_variables.module.scss';
 import TooltipWrapper from 'components/common/TooltipWrapper';
 import ControlPane from 'components/leaflet';
 import * as React from 'react';
 import { Button, Overlay, Tooltip } from 'react-bootstrap';
 import ClickAwayListener from 'react-click-away-listener';
 import { FiMapPin } from 'react-icons/fi';
-import styled from 'styled-components';
 
 import { Legend } from './Legend';
-
-const LegendButton = styled(Button)`
-  background-color: #ffffff !important;
-  color: ${variables.darkVariantColor} !important;
-  width: 40px;
-  height: 40px;
-  font-size: 25px;
-  display: flex;
-  align-items: center;
-`;
 
 export const LegendControl: React.FC = () => {
   const [visible, setVisible] = React.useState<boolean>(false);
@@ -33,9 +21,9 @@ export const LegendControl: React.FC = () => {
             setVisible(false);
           }}
         >
-          <LegendButton ref={target} onClick={() => setVisible(!visible)}>
+          <Button className="legend-button" ref={target} onClick={() => setVisible(!visible)}>
             <FiMapPin />
-          </LegendButton>
+          </Button>
         </ClickAwayListener>
       </TooltipWrapper>
       <Overlay target={target.current!} show={visible} placement="right">
