01fdd2c34ea34691b9112b65348f34ebb6a165bb
Manish Sihag <manish.sihag@yahoo.com>
2023-11-17 15:24:31 -0800

    DevOps GitHub actions pipeline (#1908)
    Co-authored-by: Adam Kroon <Adam.Kroon@gov.bc.ca>
    Co-authored-by: Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>

---
+++ .github/helpers/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Use DeploymentConfigs to deploy the application to OpenShift.
+
+
+    oc process -f /home/runner/work/PIMS/PIMS/openshift/templates/"$DC_TEMPLATE" --namespace=$NAMESPACE \
+        -p LICENSE_PLATE=$LICENSE_PLATE \
+        -p IMAGE_TAG=$IMAGE_TAG \
+        -p ENVIRONMENT=$ENVIRONMENT | \
+        oc apply -f -
