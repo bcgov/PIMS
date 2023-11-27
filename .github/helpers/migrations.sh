b8b1f4e33c9cc04ae43d691b5fbf3adab1b9c458
Manish Sihag <manish.sihag@yahoo.com>
2023-11-27 15:42:35 -0800

    Quotes issue with while running migration.

--- .github/helpers/migrations.sh
+++ .github/helpers/migrations.sh
@@ -22,6 +22,6 @@ oc run $MIGRATION_IMAGE \
     --labels='role=migration,solution=pims,app=database' \
     --restart=Never \
     --env=ASPNETCORE_ENVIRONMENT=Production \
-    --env=ConnectionStrings__PIMS=${DB_CS} \
-    --env=DB_PASSWORD=${DB_PASSWORD} \
+    --env=ConnectionStrings__PIMS="$DB_CS" \
+    --env=DB_PASSWORD=$DB_PASSWORD \
     --timeout=10m
