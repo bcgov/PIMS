72cd3dc3d5404a9ecb5485632138f5d7fa07cb1d
dbarkowsky <dylanbarkowsky@gmail.com>
2023-12-04 12:13:12 -0800

    Add jest config, add package.json commands

---
+++ express-api/jest.config.ts
@@ -0,0 +1,32 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.ts?$': [
+      'ts-jest',
+      {
+        // ts-jest configuration goes here
+      },
+    ],
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'controllers/**/*.ts',
+    'middleware/**/*.ts',
+  ],
+  coveragePathIgnorePatterns: ['index.ts'],
+  coverageReporters: ['lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  randomize: true, // Randomizes order of tests
+}
+
+export default jestConfig;
