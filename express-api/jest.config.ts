80e50404d25f088cc666ad3d1b32e0d4c26f8f37
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2023-12-05 08:09:22 -0800

    PIMS-386 Express API Testing Framework (#1960)

---
+++ express-api/jest.config.ts
@@ -0,0 +1,34 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.ts?$': [
+      'ts-jest',
+      {
+        // ts-jest configuration goes here
+      },
+    ],
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'controllers/**/*.ts',
+    'middleware/**/*.ts',
+    'routes/**/*.ts',
+    'express.ts',
+  ],
+  coveragePathIgnorePatterns: ['index.ts'],
+  coverageReporters: ['lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  randomize: true, // Randomizes order of tests
+}
+
+export default jestConfig;
---
+++ express-api/tests/integration/health/getHealth.test.ts
@@ -0,0 +1,13 @@
+import app from '../../../express';
+import supertest from 'supertest';
+
+const request = supertest(app);
+
+describe('INTEGRATION - /health', () => {
+  it('should return a 200 status code and relevant text message', async () => {
+    const response = await request
+      .get('/api/v2/health');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('/health endpoint reached. API running.');
+  })
+})
