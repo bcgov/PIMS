c73cf98a531380757442a241cb84b7a4a9262afe
taylorfries <taylor.friesen@gov.bc.ca>
2023-12-11 13:19:22 -0800

    updating to use env variables

--- express-api/app-data-source.ts
+++ express-api/app-data-source.ts
@@ -1,16 +1,20 @@
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+config({ path: resolve(__dirname, '../.env') });
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
+  type: "postgres",
+  host: "localhost",
+  port: Number(parseInt(process.env.DATABASEPORT)),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   synchronize: true,
   migrationsRun: false,
   logging: true,
-  entities: ['./typeorm/entity/*.ts'],
-  migrations: ['./typeorm/migrations/seed/*.ts', './typeorm/migrations/*.ts'],
+  entities: ["./typeorm/entity/*.ts"],
+  migrations: ["./typeorm/migrations/seed/*.ts", "./typeorm/migrations/*.ts"],
   subscribers: [],
 });
