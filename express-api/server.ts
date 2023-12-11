e01db816b6c451342b2d6890ca2b70319a0ac319
taylorfries <taylor.friesen@gov.bc.ca>
2023-12-11 09:40:12 -0800

    adding datasource

--- express-api/server.ts
+++ express-api/server.ts
@@ -2,6 +2,7 @@ import logger from './utilities/winstonLogger';
 import constants from './constants';
 import app from './express';
 import 'reflect-metadata';
+import { myDataSource } from './app-data-source';
 
 const { API_PORT } = constants;
 
@@ -9,3 +10,13 @@ app.listen(API_PORT, (err?: Error) => {
   if (err) logger.error(err);
   logger.info(`Server started on port ${API_PORT}.`);
 });
+
+// creating connection to database
+myDataSource
+  .initialize()
+  .then(() => {
+    logger.info("Database connection has been initialized")
+  })
+  .catch((err) => {
+    logger.info("Error during data source initialization")
+  })
