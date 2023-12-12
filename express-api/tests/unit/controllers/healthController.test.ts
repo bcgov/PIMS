82d33ba835849774ccd8e94278a4d41a36ce0fe5
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2023-12-12 08:07:14 -0800

    PIMS-388 Express Linting Workflow (#1966)

--- express-api/tests/unit/controllers/healthController.test.ts
+++ express-api/tests/unit/controllers/healthController.test.ts
@@ -1,15 +1,15 @@
-import controllers from "../../../controllers";
-import { Request } from "express";
+import controllers from '../../../controllers';
+import { Request, Response } from 'express';
 
 describe('UNIT - Testing controller for /health routes', () => {
-  const mockRequest = {} as Request
+  const mockRequest = {} as Request;
   const mockResponse = {
     send: jest.fn().mockReturnThis(),
     status: jest.fn().mockReturnThis(),
-  } as any
+  } as unknown as Response;
   it('should set status 200 and text message', async () => {
     await controllers.healthCheck(mockRequest, mockResponse);
     expect(mockResponse.status).toHaveBeenLastCalledWith(200);
     expect(mockResponse.send).toHaveBeenLastCalledWith('/health endpoint reached. API running.');
-  })
-})
+  });
+});
