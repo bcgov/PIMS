61b17f2ae56c496ee2758ba01a21829338fd663b
Manish Sihag <manish.sihag@yahoo.com>
2023-12-12 15:01:48 -0800

    added asp.net variable

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -6,5 +6,6 @@
     oc process -f /home/runner/work/PIMS/PIMS/openshift/templates/"$DC_TEMPLATE" --namespace=$NAMESPACE \
         -p LICENSE_PLATE="$LICENSE_PLATE" \
         -p IMAGE_TAG=$IMAGE_TAG \
+        -p ASPNETCORE_ENVIRONMENT=$ASPNETCORE_ENVIRONMENT \
         -p ENVIRONMENT=$ENVIRONMENT | \
         oc apply -f -
