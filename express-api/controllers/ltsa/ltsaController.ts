cd89a5bb04cd6dcfef7c901096413e1e223a55b9
dbarkowsky <dylanbarkowsky@gmail.com>
2023-12-19 15:49:54 -0800

    Update swagger documentation

--- express-api/controllers/ltsa/ltsaController.ts
+++ express-api/controllers/ltsa/ltsaController.ts
@@ -3,11 +3,19 @@ import { stubResponse } from '../../utilities/stubResponse';
 
 /**
  * @description Used to retrieve property information from LTSA.
- * @param {Request}     req Incoming request
- * @param {Response}    res Outgoing response
- * @returns {Response}      A 200 status with LTSA order information.
+ * @param   {Request}     req Incoming request
+ * @param   {Response}    res Outgoing response
+ * @returns {Response}        A 200 status with LTSA order information.
  */
 export const getLTSA = async (req: Request, res: Response) => {
+  /**
+   * #swagger.tags = ['LTSA']
+   * #swagger.description = 'Returns property information from LTSA.'
+   * #swagger.security = [{
+            "bearerAuth": []
+      }]
+   */
+
   // TODO: Replace stub response with controller logic
   return stubResponse(res);
 };
