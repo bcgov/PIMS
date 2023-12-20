fc8b614b24c7e273d0e23b4bb7aed9024e526b6f
Graham Stewart <graham.stewart@quartech.com>
2023-12-20 15:36:30 -0800

    Added routes and controller stubs for parcels

--- express-api/routes/index.ts
+++ express-api/routes/index.ts
@@ -1,9 +1,11 @@
 import ltsaRouter from './ltsaRouter';
 import healthRouter from './healthRouter';
+import parcelsRouter from './parcelsRouter';
 
 const router = {
   healthRouter,
   ltsaRouter,
+  parcelsRouter,
 };
 
 export default router;
