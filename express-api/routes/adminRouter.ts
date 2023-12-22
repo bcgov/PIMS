b524d52ecf1f9c0107208d130e89d75948a14415
dbarkowsky <dylanbarkowsky@gmail.com>
2023-12-21 16:02:34 -0800

    more routes for admin administrative areas

--- express-api/routes/adminRouter.ts
+++ express-api/routes/adminRouter.ts
@@ -1,5 +1,13 @@
 import controllers from '../controllers';
 import express from 'express';
+import {
+  addAdministrativeArea,
+  deleteAdministrativeAreaById,
+  getAdministrativeAreaById,
+  getAdministrativeAreas,
+  getAdministrativeAreasFiltered,
+  updateAdministrativeAreaById,
+} from '../controllers/admin/administrativeAreas/administrativeAreasController';
 
 const router = express.Router();
 
@@ -9,5 +17,27 @@ const ADMIN_ROUTE = '/admin';
 
 // Endpoints for Admin Access Requests
 router.route(`${ADMIN_ROUTE}/accessRequests`).get(getAccessRequests).delete(deleteAccessRequest);
+
+// Endpoints for Admin Administrative Areas
+router
+  .route(`${ADMIN_ROUTE}/administrativeAreas/areas`)
+  .get(getAdministrativeAreas)
+  .post(addAdministrativeArea);
+
+router
+  .route(`${ADMIN_ROUTE}/administrativeAreas/areas/filter`)
+  .post(getAdministrativeAreasFiltered); // TODO: Could be a get with query strings
+
+router
+  .route(`${ADMIN_ROUTE}/administrativeAreas/areas/:id`)
+  .get(getAdministrativeAreaById)
+  .put(updateAdministrativeAreaById) // TODO: Should this be a patch?
+  .delete(deleteAdministrativeAreaById);
+
+// Endpoints for Admin Agencies
+
+// Endpoints for Admin Claims
+
+// Endpoints for Admin Users
 
 export default router;
