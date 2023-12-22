3a2adf4ebd3849660fc6824d5632e5c250d79721
dbarkowsky <dylanbarkowsky@gmail.com>
2023-12-22 12:36:50 -0800

    update access request tests

--- express-api/tests/unit/controllers/admin/accessRequests/accessRequestController.test.ts
+++ express-api/tests/unit/controllers/admin/accessRequests/accessRequestController.test.ts
@@ -1,61 +1,55 @@
 import { Request, Response } from 'express';
 import controllers from '../../../../../controllers';
+import { MockReq, MockRes, getRequestHandlerMocks } from '../../../../testUtils/factories';
+import { Roles } from '../../../../../constants/roles';
 
-const mockRequest = {
-  user: {
-    idir_user_guid: 'W7802F34D2390EFA9E7JK15923770279',
-    identity_provider: 'idir',
-    idir_username: 'JOHNDOE',
-    name: 'Doe, John CITZ:EX',
-    preferred_username: 'a7254c34i2755fea9e7ed15918356158@idir',
-    given_name: 'John',
-    display_name: 'Doe, John CITZ:EX',
-    family_name: 'Doe',
-    email: 'john.doe@gov.bc.ca',
-    client_roles: ['Admin'],
-  },
-} as Request;
+let mockRequest: Request & MockReq, mockResponse: Response & MockRes;
 
-const mockResponse = {
-  send: jest.fn().mockReturnThis(),
-  status: jest.fn().mockReturnThis(),
-  json: jest.fn().mockReturnThis(),
-} as unknown as Response;
+const { getAccessRequests, deleteAccessRequest } = controllers.admin;
 
-describe('UNIT - Testing controllers for /admin/accessRequest GET route', () => {
-  // TODO: remove stub test when controller is complete
-  it('should return the stub response of 501', async () => {
-    await controllers.getAccessRequests(mockRequest, mockResponse);
-    expect(mockResponse.status).toHaveBeenLastCalledWith(501);
+describe('UNIT - Access Requests Admin', () => {
+  beforeEach(() => {
+    const { mockReq, mockRes } = getRequestHandlerMocks();
+    mockRequest = mockReq;
+    mockRequest.setUser({ client_roles: [Roles.ADMIN] });
+    mockResponse = mockRes;
+  });
+  describe('GET /admin/accessRequest', () => {
+    // TODO: remove stub test when controller is complete
+    it('should return the stub response of 501', async () => {
+      await getAccessRequests(mockRequest, mockResponse);
+      expect(mockResponse.statusValue).toBe(501);
+    });
+
+    // TODO: enable other tests when controller is complete
+    xit('should return status 200 and a list of access requests', async () => {
+      await getAccessRequests(mockRequest, mockResponse);
+      expect(mockResponse.statusValue).toBe(200);
+    });
+
+    xit('should return status 400 when a bad request is received', async () => {
+      await getAccessRequests(mockRequest, mockResponse);
+      expect(mockResponse.statusValue).toBe(400);
+    });
   });
 
-  // TODO: enable other tests when controller is complete
-  xit('should return status 200 and a list of access requests', async () => {
-    await controllers.getAccessRequests(mockRequest, mockResponse);
-    expect(mockResponse.status).toHaveBeenLastCalledWith(200);
-  });
+  describe('DELETE /admin/accessRequest', () => {
+    // TODO: remove stub test when controller is complete
+    it('should return the stub response of 501', async () => {
+      await deleteAccessRequest(mockRequest, mockResponse);
+      expect(mockResponse.statusValue).toBe(501);
+    });
 
-  xit('should return status 400 when a bad request is received', async () => {
-    await controllers.getAccessRequests(mockRequest, mockResponse);
-    expect(mockResponse.status).toHaveBeenLastCalledWith(400);
-  });
-});
+    // TODO: enable other tests when controller is complete
+    xit('should return status 204 upon successful deletion', async () => {
+      await deleteAccessRequest(mockRequest, mockResponse);
+      expect(mockResponse.statusValue).toBe(204);
+    });
 
-describe('UNIT - Testing controllers for /admin/accessRequest DELETE route', () => {
-  // TODO: remove stub test when controller is complete
-  it('should return the stub response of 501', async () => {
-    await controllers.deleteAccessRequest(mockRequest, mockResponse);
-    expect(mockResponse.status).toHaveBeenLastCalledWith(501);
+    xit('should return status 400 when a bad request is received', async () => {
+      await deleteAccessRequest(mockRequest, mockResponse);
+      expect(mockResponse.statusValue).toBe(400);
+    });
   });
+})
 
-  // TODO: enable other tests when controller is complete
-  xit('should return status 204 upon successful deletion', async () => {
-    await controllers.deleteAccessRequest(mockRequest, mockResponse);
-    expect(mockResponse.status).toHaveBeenLastCalledWith(204);
-  });
-
-  xit('should return status 400 when a bad request is received', async () => {
-    await controllers.deleteAccessRequest(mockRequest, mockResponse);
-    expect(mockResponse.status).toHaveBeenLastCalledWith(400);
-  });
-});
