90eb0f9422dc044050e82f85840007ac044e562c
dbarkowsky <dylanbarkowsky@gmail.com>
2023-12-22 12:56:36 -0800

    adding interfaces

--- express-api/controllers/admin/accessRequests/IAccessRequest.ts
+++ express-api/controllers/admin/accessRequests/IAccessRequest.ts
@@ -1,4 +1,6 @@
-import { UUID } from 'crypto';
+import { IAgency } from '../agencies/IAgency';
+import { IRole } from '../roles/IRole';
+import { IUser } from '../users/IUser';
 
 export interface IAccessRequest {
   createdOn: string;
@@ -9,49 +11,7 @@ export interface IAccessRequest {
   id: number;
   status: string;
   note: string;
-  user: {
-    createdOn: string;
-    updatedOn: string;
-    updatedByName: string;
-    updatedByEmail: string;
-    rowVersion: string;
-    id: UUID;
-    displayName: string;
-    firstName: string;
-    middleName: string;
-    lastName: string;
-    email: string;
-    username: string;
-    position: string;
-  };
-  agencies: {
-    createdOn: string;
-    updatedOn: string;
-    updatedByName: string;
-    updatedByEmail: string;
-    rowVersion: string;
-    id: number;
-    name: string;
-    isDisabled: true;
-    isVisible: true;
-    sortOrder: number;
-    type: string;
-    code: string;
-    parentId: number;
-    description: string;
-  }[];
-  roles: {
-    createdOn: string;
-    updatedOn: string;
-    updatedByName: string;
-    updatedByEmail: string;
-    rowVersion: string;
-    id: UUID;
-    name: string;
-    isDisabled: true;
-    isVisible: true;
-    sortOrder: number;
-    type: string;
-    description: string;
-  }[];
+  user: IUser;
+  agencies: IAgency[];
+  roles: IRole[];
 }
---
+++ express-api/controllers/admin/agencies/IAgency.ts
@@ -0,0 +1,15 @@
+export interface IAgency {
+  createdOn: string;
+  updatedOn: string;
+  updatedByName: string;
+  updatedByEmail: string;
+  id: number;
+  name: string;
+  isDisabled: true;
+  isVisible: true;
+  sortOrder: number;
+  type: string;
+  code: string;
+  parentId: number;
+  description: string;
+}
