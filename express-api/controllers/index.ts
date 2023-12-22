48112ce9fdef4fc5170619698af259ac900165e5
GrahamS-Quartech <112989452+GrahamS-Quartech@users.noreply.github.com>
2023-12-22 11:17:10 -0800

    PIMS-419: Parcel stubs (#1992)
    Co-authored-by: dbarkowsky <dylanbarkowsky@gmail.com>
    Co-authored-by: Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>

--- express-api/controllers/index.ts
+++ express-api/controllers/index.ts
@@ -1,7 +1,9 @@
 import { healthCheck } from './healthController';
 import * as ltsa from './ltsa/ltsaController';
+import * as parcels from './parcels/parcelsController';
 
 export default {
   healthCheck,
   ...ltsa,
+  ...parcels,
 };
