60059590fdc61dd502af2f80efc7d16b1ced5de5
Graham Stewart <graham.stewart@quartech.com>
2023-12-22 11:53:18 -0800

    Added routes, controller, and unit tests for users endpoints.

--- express-api/controllers/index.ts
+++ express-api/controllers/index.ts
@@ -1,7 +1,9 @@
 import { healthCheck } from './healthController';
 import * as ltsa from './ltsa/ltsaController';
+import * as users from './users/usersController';
 
 export default {
   healthCheck,
   ...ltsa,
+  ...users,
 };
