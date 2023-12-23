e8ce93233225245feb8ac3328ebc2edbf7fb4eb7
Graham Stewart <graham.stewart@quartech.com>
2023-12-22 16:30:40 -0800

    Added zod, some schema definitions for the users endpoints, implemented those into the integration tests.

--- express-api/tests/integration/users/users.test.ts
+++ express-api/tests/integration/users/users.test.ts
@@ -1,5 +1,12 @@
 import supertest from 'supertest';
 import app from '../../../express';
+import {
+  UserAccessRequest,
+  UserAccessRequestSchema,
+  UserKeycloakInfoSchema,
+  UserReportCsvSchema,
+  UserReportFilter,
+} from '../../../controllers/users/usersSchema';
 
 const request = supertest(app);
 describe('INTEGRATION - Users', () => {
@@ -29,6 +36,8 @@ describe('INTEGRATION - Users', () => {
       const id = '1';
       const response = await request.get(`${API_ROUTE}/access/requests/${id}`);
       expect(response.status).toBe(200);
+      const parsed = UserKeycloakInfoSchema.parse(response);
+      expect(parsed.id).toBe(1);
     });
     xit('should return 404 if no request found', async () => {
       const id = '-1';
@@ -40,8 +49,14 @@ describe('INTEGRATION - Users', () => {
   describe('PUT /users/access/requests/:requestId', () => {
     xit('should return 200 and the desired access request', async () => {
       const id = '1';
-      const response = await request.put(`${API_ROUTE}/access/requests/${id}`);
+      const accessRequest: UserAccessRequest = {
+        id: 1,
+        note: 'updated',
+      };
+      const response = await request.put(`${API_ROUTE}/access/requests/${id}`).send(accessRequest);
       expect(response.status).toBe(200);
+      const parsedResponse = UserAccessRequestSchema.parse(response);
+      expect(parsedResponse.note).toBe('updated');
     });
     xit('should return 400 if malformed', async () => {
       const id = '-1';
@@ -68,6 +83,7 @@ describe('INTEGRATION - Users', () => {
       const username = 'john';
       const response = await request.get(`${API_ROUTE}/agencies/${username}`);
       expect(response.status).toBe(200);
+      expect(Array.isArray(response.body)).toBe(true);
     });
     xit('should return 404 if no username', async () => {
       const username = 'john';
@@ -80,6 +96,7 @@ describe('INTEGRATION - Users', () => {
     xit('should return 200 and csv data', async () => {
       const response = await request.get(`/reports/users`);
       expect(response.status).toBe(200);
+      UserReportCsvSchema.parse(response.body);
     });
     xit('should return 400 if malformed', async () => {
       const response = await request.get(`/reports/users`);
@@ -89,11 +106,16 @@ describe('INTEGRATION - Users', () => {
 
   describe('POST /reports/users', () => {
     xit('should return 200 and csv data', async () => {
-      const response = await request.post(`/reports/users`);
+      const filter: UserReportFilter = {
+        page: 1,
+      };
+      const response = await request.post(`/reports/users`).send(filter);
       expect(response.status).toBe(200);
+      UserReportCsvSchema.parse(response.body);
     });
     xit('should return 400 if malformed', async () => {
-      const response = await request.post(`/reports/users`);
+      const badFilter = { a: 1 };
+      const response = await request.post(`/reports/users`).send(badFilter);
       expect(response.status).toBe(400);
     });
   });
