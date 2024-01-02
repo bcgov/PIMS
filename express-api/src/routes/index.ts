b7a529105c8a04679698349a01bccacb6ad78d1b
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-01-02 10:23:23 -0800

    PIMS-423 Routes - Lookup (#1997)

--- express-api/src/routes/index.ts
+++ express-api/src/routes/index.ts
@@ -1,6 +1,7 @@
 import ltsaRouter from '@/routes/ltsaRouter';
 import healthRouter from '@/routes/healthRouter';
 import parcelsRouter from '@/routes/parcelsRouter';
+import lookupRouter from '@/routes/lookupRouter';
 import adminRouter from '@/routes/adminRouter';
 import usersRouter from '@/routes/usersRouter';
 
@@ -8,6 +9,7 @@ const router = {
   healthRouter,
   ltsaRouter,
   parcelsRouter,
+  lookupRouter,
   adminRouter,
   usersRouter,
 };
