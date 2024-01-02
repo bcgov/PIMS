deaa9d3eeb71899cd3366f6c568ede915ebbc879
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-01-02 10:24:14 -0800

    PIMS-616 Init Frontend Rebuild (#1999)

---
+++ react-app/src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
---
+++ react-app/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+// This configuration is only relevant when running dev locally using Vite.
+// Production build is handled by NGINX, which has its own settings.
+export default () => {
+  const frontendPort: number = 3000;
+  const target = `http://localhost:5000`;
+
+  return defineConfig({
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+    },
+    server: {
+      host: true,
+      port: frontendPort,
+      proxy: {
+        '/api': {
+          target: target,
+          changeOrigin: true,
+        },
+      },
+    },
+  });
+};
