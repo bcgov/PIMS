1fd2f73c6f663cba71b4e88f286ca4536046c201
GrahamS-Quartech <112989452+GrahamS-Quartech@users.noreply.github.com>
2024-01-02 09:31:26 -0800

    PIMS-417 Users stubs (#1995)
    Co-authored-by: dbarkowsky <dylanbarkowsky@gmail.com>

--- express-api/src/controllers/index.ts
+++ express-api/src/controllers/index.ts
@@ -1,9 +1,11 @@
 import { healthCheck } from '@/controllers/healthController';
 import * as ltsa from '@/controllers/ltsa/ltsaController';
 import * as parcels from '@/controllers/parcels/parcelsController';
+import * as users from '@/controllers/users/usersController';
 
 export default {
   healthCheck,
   ...ltsa,
   ...parcels,
+  ...users,
 };
--- express-api/src/routes/index.ts
+++ express-api/src/routes/index.ts
@@ -1,11 +1,13 @@
 import ltsaRouter from '@/routes/ltsaRouter';
 import healthRouter from '@/routes/healthRouter';
 import parcelsRouter from '@/routes/parcelsRouter';
+import usersRouter from '@/routes/usersRouter';
 
 const router = {
   healthRouter,
   ltsaRouter,
   parcelsRouter,
+  usersRouter,
 };
 
 export default router;
