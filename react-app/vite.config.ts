b9476579a398d74d6d1501581e1a51879a6bc795
dbarkowsky <dylanbarkowsky@gmail.com>
2024-01-02 08:43:13 -0800

    Added readme, remove ENV complexity from vite config

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
-  const frontendPort: number = +(process.env.APP_HTTP_PORT ?? 3000);
-  const target = process.env.CONTAINERIZED
-    ? `http://${process.env.API_SERVICE_NAME ?? 'backend'}:${process.env.API_PROXY_PORT ?? 8080}/`
-    : `http://localhost:${process.env.API_HTTP_PORT ?? 5000}/`;
+// This configuration is only relevant when running dev locally using Vite.
+// Production build is handled by NGINX, which has its own settings.
+export default () => {
+  const frontendPort: number = 3000;
+  const target = `http://localhost:5000`;
 
   return defineConfig({
     plugins: [react()],
