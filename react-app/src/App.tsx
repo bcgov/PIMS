1fdc972d5ebb97676d78fe3d86835c663a9c552f
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-01-03 11:46:59 -0800

    PIMS-624 Frontend Keycloak (#2006)

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,20 @@
+import { useKeycloak } from '@bcgov/citz-imb-kc-react';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
 import React from 'react';
 import './App.css';
+import { Login } from './pages/Login';
 
 const App = () => {
-  return <></>;
+  const { isAuthenticated } = useKeycloak();
+
+  return isAuthenticated ? (
+    <Routes>
+      <Route index element={<Home />} />
+    </Routes>
+  ) : (
+    <Login />
+  );
 };
 
 export default App;
--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -20,6 +20,7 @@ export default () => {
         '/api': {
           target: target,
           changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
     },
