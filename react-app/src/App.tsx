37d64a802f6f8d0dbbcabef3d3642e0029fc1ede
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-01-04 08:29:18 -0800

    PIMS-623 Frontend Absolute Paths (#2007)

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,9 @@
 import { useKeycloak } from '@bcgov/citz-imb-kc-react';
 import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
+import { Home } from '@/pages/Home';
 import React from 'react';
-import './App.css';
-import { Login } from './pages/Login';
+import '@/App.css';
+import { Login } from '@/pages/Login';
 
 const App = () => {
   const { isAuthenticated } = useKeycloak();
