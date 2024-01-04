556f6f62ce651105f4ea4b2fd623f76e88eefba8
dbarkowsky <dylanbarkowsky@gmail.com>
2024-01-04 13:47:01 -0800

    Add indexes to Updated and Created by

--- express-api/src/typeorm/Entities/BaseEntity.ts
+++ express-api/src/typeorm/Entities/BaseEntity.ts
@@ -1,9 +1,10 @@
 import { Users } from '@/typeorm/Entities/Users';
-import { Column, CreateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Column, CreateDateColumn, ManyToOne, JoinColumn, Index } from 'typeorm';
 
 export abstract class BaseEntity {
   @ManyToOne(() => Users, (user) => user.Id)
   @JoinColumn({ name: 'CreatedById' })
+  @Index()
   CreatedById: Users;
 
   @CreateDateColumn()
@@ -11,6 +12,7 @@ export abstract class BaseEntity {
 
   @ManyToOne(() => Users, (user) => user.Id, { nullable: true })
   @JoinColumn({ name: 'UpdatedById' })
+  @Index()
   UpdatedById: Users;
 
   @Column({ type: 'timestamp', nullable: true })
