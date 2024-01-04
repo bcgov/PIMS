8c314a95291e3b9067377e1ffdc8b1495095638b
Sharala-Perumal <Sharala.Perumal@gov.bc.ca>
2024-01-04 10:35:11 -0800

    Created ProjectTypes

---
+++ express-api/src/typeorm/Entities/ProjectTypes.ts
@@ -0,0 +1,33 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, Index } from 'typeorm';
+
+@Entity()
+@Index(['IsDisabled', 'Name', 'SortOrder'])
+export class Tasks {
+  @PrimaryGeneratedColumn()
+  Id: number;
+
+  @Column('uuid')
+  CreatedById: string;
+
+  @CreateDateColumn()
+  CreatedOn: Date;
+
+  @Column('uuid')
+  UpdatedById: string;
+
+  @CreateDateColumn()
+  UpdatedOn: Date;
+
+  @Column({ type: 'character varying', length: 20 })
+  @Index({ unique: true })
+  Name: string;
+
+  @Column('text', { nullable: true })
+  Description: string;
+
+  @Column('bit')
+  IsDisabled: boolean;
+
+  @Column('int')
+  SortOrder: number;
+}
