671bff36f1a5c395a871ca398e30c162d494e1a3
Sharala-Perumal <Sharala.Perumal@gov.bc.ca>
2024-01-05 13:33:36 -0800

    Created ProjectReports

---
+++ express-api/src/typeorm/Entities/ProjectReports.ts
@@ -0,0 +1,24 @@
+import { BaseEntity } from '@/typeorm/Entities/BaseEntity';
+import { Entity, PrimaryGeneratedColumn, Column, Index } from 'typeorm';
+
+@Entity()
+@Index(['Id', 'To', 'From', 'IsFinal'])
+export class ProjectReports extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  Id: number;
+
+  @Column('bit')
+  IsFinal: boolean;
+
+  @Column({ type: 'character varying', length: 250, nullable: true })
+  Name: string;
+
+  @Column('timestamp')
+  From: Date;
+
+  @Column('timestamp')
+  To: Date;
+
+  @Column('int')
+  ReportType: number;
+}
