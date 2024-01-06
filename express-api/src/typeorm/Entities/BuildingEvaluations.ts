4acb2c5d479e55179cba7064ccfff736e634e18e
dbarkowsky <dylanbarkowsky@gmail.com>
2024-01-05 16:03:01 -0800

    add ParcelEvaluations and abstract Evaluation entity

--- express-api/src/typeorm/Entities/BuildingEvaluations.ts
+++ express-api/src/typeorm/Entities/BuildingEvaluations.ts
@@ -1,24 +1,12 @@
 import { Buildings } from '@/typeorm/Entities/Buildings';
-import { BaseEntity } from '@/typeorm/Entities/abstractEntities/BaseEntity';
-import { Entity, Column, Index, ManyToOne, JoinColumn, PrimaryColumn } from 'typeorm';
+import { Evaluation } from '@/typeorm/Entities/abstractEntities/Evaluation';
+import { Entity, Index, ManyToOne, JoinColumn, PrimaryColumn } from 'typeorm';
 
 @Entity()
 @Index(['BuildingId', 'EvaluationKey'])
-export class BuildingEvaluations extends BaseEntity {
+export class BuildingEvaluations extends Evaluation {
   @ManyToOne(() => Buildings, (building) => building.Id)
   @JoinColumn({ name: 'BuildingId' })
   @PrimaryColumn()
   BuildingId: Buildings;
-
-  @PrimaryColumn('timestamp')
-  Date: Date;
-
-  @PrimaryColumn('int')
-  EvaluationKey: number;
-
-  @Column({ type: 'money' })
-  Value: number;
-
-  @Column({ type: 'character varying', length: 500, nullable: true })
-  Note: string;
 }
