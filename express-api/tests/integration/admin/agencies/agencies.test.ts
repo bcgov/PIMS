8c6153381badf6fe4c562b3915e1a08f0c8102d4
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-01-05 16:19:08 -0800

    PIMS-421 Routes - Properties (#2001)

--- express-api/tests/integration/admin/agencies/agencies.test.ts
+++ express-api/tests/integration/admin/agencies/agencies.test.ts
@@ -6,7 +6,7 @@ import { IAgency } from '@/controllers/admin/agencies/IAgency';
 
 const request = supertest(app);
 
-const AGENCIES_PATH = '/api/v2//admin/agencies';
+const AGENCIES_PATH = '/api/v2/admin/agencies';
 const mockAgency: IAgency = {
   createdOn: faker.date.anytime().toLocaleString(),
   updatedOn: faker.date.anytime().toLocaleString(),
