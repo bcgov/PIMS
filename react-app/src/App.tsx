f8c82e0f2e8a5c02c77b4040135abfa0bbc248aa
GrahamS-Quartech <112989452+GrahamS-Quartech@users.noreply.github.com>
2024-01-08 12:00:58 -0800

    PIMS-653/645 MUI Setup and Landing Page (#2014)

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,23 @@
-import { useKeycloak } from '@bcgov/citz-imb-kc-react';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '@/pages/Home';
 import React from 'react';
 import '@/App.css';
-import { Login } from '@/pages/Login';
+import { ThemeProvider } from '@emotion/react';
+import appTheme from './themes/appTheme';
 
-const App = () => {
-  const { isAuthenticated } = useKeycloak();
-
-  return isAuthenticated ? (
+const Router = () => {
+  return (
     <Routes>
       <Route index element={<Home />} />
     </Routes>
-  ) : (
-    <Login />
+  );
+};
+
+const App = () => {
+  return (
+    <ThemeProvider theme={appTheme}>
+      <Router />
+    </ThemeProvider>
   );
 };
 
---
+++ react-app/src/constants/strings.ts
@@ -0,0 +1,3 @@
+export const footerCopyright = `© ${new Date().getFullYear()} Government of British Columbia.`;
+export const landingPageTopText = `PIMS enables you to search properties owned by the Government of British Columbia`;
+export const landingPageBottomText = `The data provided can assist your agency in making informed, timely, and strategic decisions on the optimal use of real property assets on behalf of the people and priorities of British Columbia.`;
