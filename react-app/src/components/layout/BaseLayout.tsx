31ed98be0356d4192c7fe0cd2275eead1298cdc6
GrahamS-Quartech <112989452+GrahamS-Quartech@users.noreply.github.com>
2024-01-08 12:00:58 -0800

    PIMS-653/645 MUI Setup and Landing Page (#2014)

---
+++ react-app/src/components/layout/BaseLayout.tsx
@@ -0,0 +1,28 @@
+import React, { PropsWithChildren } from 'react';
+import { Box } from '@mui/material';
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+
+interface IBaseLayoutProps extends PropsWithChildren {
+  displayFooter?: boolean;
+}
+
+const BaseLayout = (props: IBaseLayoutProps) => {
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Header />
+      <Box component="main" flex="1 1 auto">
+        {props.children}
+      </Box>
+      {props.displayFooter && <Footer />}
+    </Box>
+  );
+};
+
+export default BaseLayout;
---
+++ react-app/src/components/layout/Header.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import headerImageLarge from '@/assets/images/BCID_H_rgb_pos.png';
+import headerImageSmall from '@/assets/images/BCID_V_rgb_pos.png';
+import { AppBar, Link, Box, Button, Divider, Toolbar, Typography, useTheme } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import { useKeycloak } from '@bcgov/citz-imb-kc-react';
+
+const AppBrand = () => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        '& a': {
+          display: 'flex',
+          alignItems: 'center',
+          overflow: 'hidden',
+          textDecoration: 'none',
+        },
+        '& img': {
+          mr: '12px',
+        },
+      }}
+    >
+      <RouterLink to="/" aria-label="Go to PIMS Home">
+        <picture>
+          <source srcSet={headerImageLarge} media="(min-width: 960px)"></source>
+          <source srcSet={headerImageSmall} media="(min-width: 600px)"></source>
+          <img
+            src={headerImageSmall}
+            alt={'Government of British Columbia'}
+            style={{ height: '48px' }}
+          />
+        </picture>
+        <Divider
+          style={{ borderBottomWidth: '26px', borderRightWidth: '2px' }}
+          orientation="vertical"
+        />
+        <Typography marginLeft={'16px'} color={theme.palette.black.main} variant="h5">
+          Property Inventory Management System
+        </Typography>
+      </RouterLink>
+    </Box>
+  );
+};
+
+const Header: React.FC = () => {
+  const { logout, isAuthenticated, login } = useKeycloak();
+  const theme = useTheme();
+  const handleLoginButton = () => {
+    if (isAuthenticated) {
+      logout();
+    } else {
+      login({ idpHint: 'idir' });
+    }
+  };
+  return (
+    <AppBar
+      elevation={0}
+      style={{
+        backgroundColor: theme.palette.white.main,
+        height: '74px',
+        display: 'flex',
+        position: 'relative',
+        justifyContent: 'center',
+        borderBottom: '1px solid',
+        borderBottomColor: theme.palette.gray.main,
+      }}
+    >
+      <Toolbar>
+        <AppBrand />
+        <Box flexGrow={1}></Box>
+        <Box textAlign={'center'} alignItems={'center'} gap={'32px'} display={'flex'}>
+          {isAuthenticated && (
+            <>
+              <Link underline="none" href="#" variant="h5">
+                Active Inventory
+              </Link>
+              <Link underline="none" href="#" variant="h5">
+                Disposal Inventory
+              </Link>
+              <Link underline="none" href="#" variant="h5">
+                Users
+              </Link>
+            </>
+          )}
+          <Button onClick={() => handleLoginButton()} variant="contained">
+            {isAuthenticated ? 'Logout' : 'Login'}
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
