410562dfaaca2a6580d18a99b94704c9192e7448
Graham Stewart <graham.stewart@quartech.com>
2024-01-08 11:42:26 -0800

    Replaced placeholder text and replaced generic vector image with PIMS vector logo

--- react-app/src/constants/strings.ts
+++ react-app/src/constants/strings.ts
@@ -1,3 +1,3 @@
 export const footerCopyright = `© ${new Date().getFullYear()} Government of British Columbia.`;
-export const landingPageTopText = `Search and Visualize Government Property`;
-export const landingPageBottomText = `Placeholder text about visualizing the government property will fill some space here.`;
+export const landingPageTopText = `PIMS enables you to search properties owned by the Government of British Columbia`;
+export const landingPageBottomText = `The data provided can assist your agency in making informed, timely, and strategic decisions on the optimal use of real property assets on behalf of the people and priorities of British Columbia.`;
--- react-app/src/pages/Home.tsx
+++ react-app/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import BaseLayout from '@/components/layout/BaseLayout';
-import propertyVector from '@/assets/images/hero.svg';
+import propertyVector from '@/assets/images/PIMS_logo.svg';
 import { landingPageBottomText, landingPageTopText } from '@/constants/strings';
 
 export const Home = () => {
@@ -13,19 +13,21 @@ export const Home = () => {
         display={'flex'}
         justifyContent={'center'}
         height={'100%'}
-        bgcolor={'#F8F8F8'}
+        bgcolor={'#F8F8F8'} //In the ticket this was #D2D8D8, but I think it was meant to be #F8F8F8 based on the example.
       >
-        <Box gap={'32px'} maxWidth={'1090px'} marginX={'auto'} display={'flex'}>
-          <Box>
-            <Typography marginBottom={'16px'} variant="h1" fontSize={'2.5rem'}>
-              {landingPageTopText}
-            </Typography>
-            <Typography variant="h4" fontWeight={500}>
-              {landingPageBottomText}
-            </Typography>
-          </Box>
-          <Box>
-            <img src={propertyVector} alt={'Property vector image.'} />
+        <Box maxWidth={'1090px'} marginX={'auto'} display={'flex'}>
+          <Box display={'flex'} alignContent={'center'}>
+            <Box marginRight={'32px'}>
+              <Typography marginBottom={'16px'} variant="h1" fontSize={'2.5rem'}>
+                {landingPageTopText}
+              </Typography>
+              <Typography variant="h4" fontWeight={500}>
+                {landingPageBottomText}
+              </Typography>
+            </Box>
+            <Box>
+              <img width={'200px'} src={propertyVector} alt={'Property vector image.'} />
+            </Box>
           </Box>
         </Box>
       </Box>
