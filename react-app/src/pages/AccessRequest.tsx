1a7f1ddb8a61e7e47a3fe36c562d10fd61a29cc4
Graham Stewart <graham.stewart@quartech.com>
2024-01-12 12:13:02 -0800

    Added pending vector graphic asset, jsxSnippets for constants that require <a> tags for example, created Access Pending component to display when user submits form

--- react-app/src/pages/AccessRequest.tsx
+++ react-app/src/pages/AccessRequest.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import pendingImage from '@/assets/images/pending.svg';
 import { Box, Button, Grid, Paper, Typography } from '@mui/material';
 import TextInput from '@/components/form/TextFormField';
 import AutocompleteFormField from '@/components/form/AutocompleteFormField';
 import { useKeycloak } from '@bcgov/citz-imb-kc-react';
 import { FormProvider, useForm } from 'react-hook-form';
+import { accessPendingBlurb, signupTermsAndConditionsClaim } from '@/constants/jsxSnippets';
 
-export const AccessRequest = () => {
+const AccessPending = () => {
+  return (
+    <Box
+      display={'flex'}
+      flexDirection={'column'}
+      alignItems={'center'}
+      textAlign={'center'}
+      gap={'2rem'}
+    >
+      <img width={'300px'} src={pendingImage} />
+      <Typography>{accessPendingBlurb}</Typography>
+    </Box>
+  );
+};
+
+const RequestForm = ({ submitHandler }: { submitHandler: (d: any) => void }) => {
   const keycloak = useKeycloak();
+
   const formMethods = useForm({
     defaultValues: {
       UserName: keycloak.state.userInfo.idir_username,
@@ -25,89 +43,100 @@ export const AccessRequest = () => {
     { label: 'BC Electric & Hydro', value: 'key3' },
   ];
 
+  return (
+    <>
+      <FormProvider {...formMethods}>
+        <Grid spacing={2} container>
+          <Grid item xs={6}>
+            <TextInput
+              fullWidth
+              name={'UserName'}
+              label={'IDIR/BCeID'}
+              defaultValue={keycloak.state.userInfo.idir_username}
+              disabled
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextInput
+              fullWidth
+              name={'Email'}
+              label={'Email'}
+              defaultValue={keycloak.state.userInfo.email}
+              disabled
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextInput
+              fullWidth
+              name={'FirstName'}
+              label={'First name'}
+              defaultValue={keycloak.state.userInfo.given_name}
+              disabled
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextInput
+              name={'LastName'}
+              fullWidth
+              label={'Last name'}
+              defaultValue={keycloak.state.userInfo.family_name}
+              disabled
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <AutocompleteFormField
+              name={'Agency'}
+              label={'Your agency'}
+              options={placeholderData}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextInput name={'Position'} fullWidth label={'Your position'} />
+          </Grid>
+          <Grid item xs={12}>
+            <TextInput
+              name={'Notes'}
+              multiline
+              fullWidth
+              label={'Notes (e.g. Reason for access)'}
+            />
+          </Grid>
+        </Grid>
+      </FormProvider>
+      <Box mt={'2rem'} display="flex">
+        <Button
+          onClick={formMethods.handleSubmit(submitHandler)}
+          variant="contained"
+          color="primary"
+          sx={{ width: '6rem', marginX: 'auto' }}
+        >
+          Submit
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export const AccessRequest = () => {
+  const [requestSent, setRequestSent] = useState(false);
+
   const onSubmit = (data) => {
     // eslint-disable-next-line no-console
     console.log(JSON.stringify(data));
+    setRequestSent(true);
   };
 
   return (
     <Box display="flex" flexDirection={'column'} width="600px" marginX="auto">
       <Paper sx={{ padding: '2rem', borderRadius: '32px' }}>
         <Typography mb={'2rem'} variant="h2">
-          Access Request
+          {requestSent ? 'Access Request' : 'Access Pending'}
         </Typography>
-        <FormProvider {...formMethods}>
-          <Grid spacing={2} container>
-            <Grid item xs={6}>
-              <TextInput
-                fullWidth
-                name={'UserName'}
-                label={'IDIR/BCeID'}
-                defaultValue={keycloak.state.userInfo.idir_username}
-                disabled
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextInput
-                fullWidth
-                name={'Email'}
-                label={'Email'}
-                defaultValue={keycloak.state.userInfo.email}
-                disabled
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextInput
-                fullWidth
-                name={'FirstName'}
-                label={'First name'}
-                defaultValue={keycloak.state.userInfo.given_name}
-                disabled
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextInput
-                name={'LastName'}
-                fullWidth
-                label={'Last name'}
-                defaultValue={keycloak.state.userInfo.family_name}
-                disabled
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <AutocompleteFormField
-                name={'Agency'}
-                label={'Your agency'}
-                options={placeholderData}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextInput name={'Position'} fullWidth label={'Your position'} />
-            </Grid>
-            <Grid item xs={12}>
-              <TextInput
-                name={'Notes'}
-                multiline
-                fullWidth
-                label={'Notes (e.g. Reason for access)'}
-              />
-            </Grid>
-          </Grid>
-        </FormProvider>
-        <Box mt={'2rem'} display="flex">
-          <Button
-            onClick={formMethods.handleSubmit(onSubmit)}
-            variant="contained"
-            color="primary"
-            sx={{ width: '6rem', marginX: 'auto' }}
-          >
-            Submit
-          </Button>
-        </Box>
+        {requestSent ? <AccessPending /> : <RequestForm submitHandler={onSubmit} />}
       </Paper>
+
       <Typography mt={'1rem'} textAlign={'center'}>
-        By signing up, you agree to <a href="#">Terms and Conditions</a> and that you have read{' '}
-        <a href="#">Privacy Policy</a>.
+        {signupTermsAndConditionsClaim}
       </Typography>
     </Box>
   );
