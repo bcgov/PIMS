a67b1a6456ebaa26a074bbe2229309135d69fd51
GrahamS-Quartech <112989452+GrahamS-Quartech@users.noreply.github.com>
2024-01-15 11:05:22 -0800

    PIMS-661: Access Pending (#2030)

---
+++ react-app/src/constants/jsxSnippets.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const accessPendingBlurb = (
+  <>
+    We have received your request. We will notify you via email when your request has been reviewed.
+    If you haven&apos;t heard from us within five business days, please feel free to reach out to us
+    at <a href="mailto: pimshelp@gov.bc.ca">pimshelp@gov.bc.ca</a>.
+  </>
+);
+
+export const signupTermsAndConditionsClaim = (
+  <>
+    By signing up, you agree to the <a href="#">Terms and Conditions</a> and confirm that you have
+    read the <a href="#">Privacy Policy</a>.
+  </>
+);
