50cfb6e21eb1ce78244c84772985bf2549702443
dbarkowsky <dylanbarkowsky@gmail.com>
2024-01-26 10:14:48 -0800

    main ticket points met, starting on subtasks

--- react-app/src/pages/UsersTable.tsx
+++ react-app/src/pages/UsersTable.tsx
@@ -1,42 +1,42 @@
 import BaseLayout from '@/components/layout/BaseLayout';
 import { CustomDataGrid } from '@/components/table/DataTable';
-import { Box, Chip, Paper, Typography } from '@mui/material';
+import { Box, Chip, Paper, Typography, useTheme } from '@mui/material';
 import { GridColDef } from '@mui/x-data-grid';
 import React, { useEffect, useState } from 'react';
 import { UUID } from 'crypto';
 import { useKeycloak } from '@bcgov/citz-imb-kc-react';
 
-interface IAgency {
-  createdOn: string;
-  updatedOn: string;
-  updatedByName: string;
-  updatedByEmail: string;
-  id: number;
-  name: string;
-  isDisabled: true;
-  isVisible: true;
-  sortOrder: number;
-  type: string;
-  code: string;
-  parentId: number;
-  description: string;
-}
+// interface IAgency {
+//   createdOn: string;
+//   updatedOn: string;
+//   updatedByName: string;
+//   updatedByEmail: string;
+//   id: number;
+//   name: string;
+//   isDisabled: true;
+//   isVisible: true;
+//   sortOrder: number;
+//   type: string;
+//   code: string;
+//   parentId: number;
+//   description: string;
+// }
 
-interface IRole {
-  createdOn: string;
-  updatedOn: string;
-  updatedByName: string;
-  updatedByEmail: string;
-  id: UUID;
-  name: string;
-  isDisabled: true;
-  isVisible: true;
-  sortOrder: 0;
-  type: string;
-  description: string;
-  keycloakGroupId: UUID;
-  isPublic: true;
-}
+// interface IRole {
+//   createdOn: string;
+//   updatedOn: string;
+//   updatedByName: string;
+//   updatedByEmail: string;
+//   id: UUID;
+//   name: string;
+//   isDisabled: true;
+//   isVisible: true;
+//   sortOrder: 0;
+//   type: string;
+//   description: string;
+//   keycloakGroupId: UUID;
+//   isPublic: true;
+// }
 
 interface IUser {
   createdOn: string;
@@ -56,13 +56,14 @@ interface IUser {
   emailVerified: true;
   note: string;
   lastLogin: string;
-  agencies: IAgency[];
-  roles: IRole[];
+  agency: string;
+  roles: string;
 }
 
 const UsersTable = () => {
   const [users, setUsers] = useState<IUser[]>([]);
   const { getAuthorizationHeaderValue, state } = useKeycloak();
+  const theme = useTheme();
 
   useEffect(() => {
     fetch('http://localhost:5000/api/v2/admin/users', {
@@ -79,210 +80,79 @@ const UsersTable = () => {
   }, [state]);
 
   const colorMap = {
-    Pending: 'warning',
+    Pending: 'info',
     Active: 'success',
-    Hold: 'error',
+    Hold: 'warning',
   };
-  const rows = [
-    {
-      id: 0,
-      firstName: 'Graham',
-      lastName: 'Stewart',
-      Status: 'Active',
-      created: '2023-04-02',
-      username: 'username@idir',
-      email: 'email@gmail.com',
-      agency: 'CITZ',
-      position: 'Captain',
-      role: 'Admin',
-      lastLogin: '2024-02-06',
-    },
-    {
-      id: 1,
-      firstName: 'John',
-      lastName: 'Smith',
-      Status: 'Pending',
-      created: '2023-04-02',
-      username: 'username@idir',
-      email: 'email@gmail.com',
-      agency: 'CITZ',
-      position: 'Captain',
-      role: 'Admin',
-      lastLogin: '2024-02-06',
-    },
-    {
-      id: 2,
-      firstName: 'Alice',
-      lastName: 'Johnson',
-      Status: 'Hold',
-      created: '2023-02-03',
-      username: 'username@idir',
-      email: 'email@gmail.com',
-      agency: 'CITZ',
-      position: 'Captain',
-      role: 'Admin',
-      lastLogin: '2024-02-06',
-    },
-    {
-      id: 3,
-      firstName: 'Bob',
-      lastName: 'Anderson',
-      Status: 'Active',
-      created: '2023-04-03',
-      username: 'username@idir',
-      email: 'email@gmail.com',
-      agency: 'CITZ',
-      position: 'Captain',
-      role: 'Admin',
-      lastLogin: '2024-02-06',
-    },
-    {
-      id: 4,
-      firstName: 'Emma',
-      lastName: 'White',
-      Status: 'Pending',
-      created: '2023-06-04',
-      username: 'username@idir',
-      email: 'email@gmail.com',
-      agency: 'CITZ',
-      position: 'Captain',
-      role: 'Admin',
-      lastLogin: '2024-02-06',
-    },
-    {
-      id: 5,
-      firstName: 'David',
-      lastName: 'Taylor',
-      Status: 'Hold',
-      created: '2023-04-04',
-      username: 'username@idir',
-      email: 'email@gmail.com',
-      agency: 'CITZ',
-      position: 'Captain',
-      role: 'Admin',
-      lastLogin: '2024-02-06',
-    },
-    {
-      id: 6,
-      firstName: 'Sophie',
-      lastName: 'Brown',
-      Status: 'Active',
-      created: '2023-04-05',
-      username: 'username@idir',
-      email: 'email@gmail.com',
-      agency: 'CITZ',
-      position: 'Captain',
-      role: 'Admin',
-      lastLogin: '2024-02-06',
-    },
-    {
-      id: 7,
-      firstName: 'Michael',
-      lastName: 'Jones',
-      Status: 'Pending',
-      created: '2023-04-05',
-      username: 'username@idir',
-      email: 'email@gmail.com',
-      agency: 'CITZ',
-      position: 'Captain',
-      role: 'Admin',
-      lastLogin: '2024-02-06',
-    },
-    {
-      id: 8,
-      firstName: 'Olivia',
-      lastName: 'Wilson',
-      Status: 'Active',
-      created: '2023-04-06',
-      username: 'username@idir',
-      email: 'email@gmail.com',
-      agency: 'CITZ',
-      position: 'Captain',
-      role: 'Admin',
-      lastLogin: '2024-02-06',
-    },
-    {
-      id: 9,
-      firstName: 'Daniel',
-      lastName: 'Miller',
-      Status: 'Hold',
-      created: '2023-04-06',
-      username: 'username@idir',
-      email: 'email@gmail.com',
-      agency: 'CITZ',
-      position: 'Captain',
-      role: 'Admin',
-      lastLogin: '2024-02-06',
-    },
-    {
-      id: 10,
-      firstName: 'Cotton-Eye',
-      lastName: 'Joe',
-      Status: 'Hold',
-      created: '2023-04-06',
-      username: 'username@idir',
-      email: 'email@gmail.com',
-      agency: 'CITZ',
-      position: 'Captain',
-      role: 'Admin',
-      lastLogin: '2024-02-06',
-    },
-  ];
+
   const columns: GridColDef[] = [
     {
       field: 'firstName',
       headerName: 'First Name',
       flex: 1,
+      minWidth: 150,
     },
     {
       field: 'lastName',
       headerName: 'Last Name',
       flex: 1,
+      minWidth: 150,
     },
     {
-      field: 'Status',
+      field: 'status',
       headerName: 'Status',
-      flex: 1,
       renderCell: (params) => {
-        return <Chip sx={{ width: '6rem' }} label={params.value} color={colorMap[params.value]} />;
+        return (
+          <Chip
+            sx={{
+              width: '6rem',
+              color: theme.palette[colorMap[params.value]]['main'],
+              backgroundColor: theme.palette[colorMap[params.value]]['light'],
+            }}
+            label={params.value}
+          />
+        );
       },
+      maxWidth: 100,
     },
     {
       field: 'email',
       headerName: 'Email Address',
+      minWidth: 150,
       flex: 1,
     },
     {
       field: 'username',
       headerName: 'IDIR/BCeID',
+      minWidth: 150,
       flex: 1,
     },
     {
       field: 'agency',
-      headerName: 'Agencies',
+      headerName: 'Agency',
+      minWidth: 150,
       flex: 1,
     },
     {
       field: 'position',
       headerName: 'Position',
+      minWidth: 150,
       flex: 1,
     },
     {
       field: 'role',
       headerName: 'Role',
+      minWidth: 100,
       flex: 1,
-      maxWidth: 150,
     },
     {
       field: 'created',
-      headerName: 'Created On',
-      flex: 1,
+      headerName: 'Created',
       maxWidth: 120,
     },
     {
       field: 'lastLogin',
       headerName: 'Last Login',
-      flex: 1,
       maxWidth: 120,
     },
   ];
@@ -296,12 +166,12 @@ const UsersTable = () => {
               marginBottom: '1em',
             }}
           >
-            <Typography variant="h4">Users Overview ({rows.length} users)</Typography>
+            <Typography variant="h4">Users Overview ({users.length ?? 0} users)</Typography>
           </Box>
           <CustomDataGrid
             getRowId={(row) => row.id}
             columns={columns}
-            rows={rows}
+            rows={users ?? []}
             initialState={{
               pagination: { paginationModel: { pageSize: 10 } },
               sorting: {
@@ -309,6 +179,21 @@ const UsersTable = () => {
               },
             }}
             pageSizeOptions={[10, 20, 30, 100]} // DataGrid max is 100
+            disableRowSelectionOnClick
+            sx={{
+              overflow: 'scroll',
+              // Neutralize the hover colour (causing a flash)
+              '& .MuiDataGrid-row.Mui-hovered': {
+                backgroundColor: 'transparent',
+              },
+              // Take out the hover colour
+              '& .MuiDataGrid-row:hover': {
+                backgroundColor: 'transparent',
+              },
+              '& .MuiDataGrid-cell:focus-within': {
+                outline: 'none',
+              }
+            }}
           />
         </Paper>
       </Box>
