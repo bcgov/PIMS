3ec931d44226be7da2eb5a827513ff164b3ef15c
Sharala-Perumal <Sharala.Perumal@gov.bc.ca>
2024-02-06 14:52:20 -0800

    Adding Tier levels and Project Status Migration

---
+++ express-api/src/typeorm/Migrations/Seeds/TierLevels_202402011455.sql
@@ -0,0 +1,5 @@
+INSERT INTO "tier_levels" ("Id","CreatedById","CreatedOn","UpdatedById","UpdatedOn","Name","IsDisabled","SortOrder","Description") VALUES
+	 (1,'00000000-0000-0000-0000-000000000000','2023-01-17 17:58:35.2300000',NULL,NULL,N'Tier 1',B'0',1,N'Properties with a net value of less than $1M.'),
+	 (2,'00000000-0000-0000-0000-000000000000','2023-01-17 17:58:35.2300000',NULL,NULL,N'Tier 2',B'0',2,N'Properties with a net value of $1M or more and less than $10M'),
+	 (3,'00000000-0000-0000-0000-000000000000','2023-01-17 17:58:35.2300000',NULL,NULL,N'Tier 3',B'0',3,N'Properties from a single parcels with a net value of $10M or more'),
+	 (4,'00000000-0000-0000-0000-000000000000','2023-01-17 17:58:35.2300000',NULL,NULL,N'Tier 4',B'0',4,N'Properties from multiple parcels with a cumulative net value of $10M or more');
