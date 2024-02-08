91aa02c5e876bb6e8dff938787c56aa466fc5f0b
dbarkowsky <dylanbarkowsky@gmail.com>
2024-02-08 11:20:41 -0800

    Add report types seed

---
+++ express-api/src/typeorm/Migrations/Seeds/ReportTypes_2024020081047.sql
@@ -0,0 +1,3 @@
+INSERT INTO "report_type" ("Id","CreatedById","CreatedOn","Name","IsDisabled","SortOrder","Description") 
+VALUES
+	 (0, "00000000-0000-0000-0000-000000000000", "2023-01-17 17:58:34.7500000", "SPL", FALSE, 0, "Surplus Properties");
