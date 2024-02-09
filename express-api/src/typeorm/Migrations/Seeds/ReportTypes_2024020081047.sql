0c1847c0a49fb997819a996d957e3c1b5ed7e782
dbarkowsky <dylanbarkowsky@gmail.com>
2024-02-09 10:42:36 -0800

    Adjust ReportTypes SQL

--- express-api/src/typeorm/Migrations/Seeds/ReportTypes_2024020081047.sql
+++ express-api/src/typeorm/Migrations/Seeds/ReportTypes_2024020081047.sql
@@ -1,3 +1,3 @@
 INSERT INTO "report_type" ("Id","CreatedById","CreatedOn","Name","IsDisabled","SortOrder","Description") 
 VALUES
-	 (0, "00000000-0000-0000-0000-000000000000", "2023-01-17 17:58:34.7500000", "SPL", FALSE, 0, "Surplus Properties");
+	 (0, (SELECT "Id" FROM "user" WHERE "Username" = 'system'), '2023-01-17 17:58:34.7500000', 'SPL', FALSE, 0, 'Surplus Properties');
