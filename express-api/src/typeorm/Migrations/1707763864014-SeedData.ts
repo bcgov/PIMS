6a1a910f9afa29747045721f3033b2e045acfc73
dbarkowsky <dylanbarkowsky@gmail.com>
2024-02-12 11:17:00 -0800

    Adjusted names of sql files

--- express-api/src/typeorm/Migrations/1707763864014-SeedData.ts
+++ express-api/src/typeorm/Migrations/1707763864014-SeedData.ts
@@ -2,114 +2,107 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 import { SqlReader } from 'node-sql-reader';
 import * as path from 'path';
 
-export class FullMigration1707268607199 implements MigrationInterface {
+export class SeedData1707763864014 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const sqlFilePath = path.resolve(__dirname, 'Seeds');
 
-    const Users = SqlReader.readSqlFile(path.join(sqlFilePath, 'Users_202401241615.sql'));
+    const Users = SqlReader.readSqlFile(path.join(sqlFilePath, 'Users.sql'));
     await queryRunner.query(Users.toString());
 
-    const Agencies = SqlReader.readSqlFile(path.join(sqlFilePath, 'Agencies_202401251646.sql'));
+    const Agencies = SqlReader.readSqlFile(path.join(sqlFilePath, 'Agencies.sql'));
     await queryRunner.query(Agencies.toString());
 
-    const Provinces = SqlReader.readSqlFile(path.join(sqlFilePath, 'Provinces_202401241102.sql'));
+    const Provinces = SqlReader.readSqlFile(path.join(sqlFilePath, 'Provinces.sql'));
     await queryRunner.query(Provinces.toString());
 
     const BuildingOccupantTypes = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'BuildingOccupantTypes_202401301141.sql'),
+      path.join(sqlFilePath, 'BuildingOccupantTypes.sql'),
     );
     await queryRunner.query(BuildingOccupantTypes.toString());
 
     const BuildingPredominateUses = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'BuildingPredominateUses_202401301156.sql'),
+      path.join(sqlFilePath, 'BuildingPredominateUses.sql'),
     );
     await queryRunner.query(BuildingPredominateUses.toString());
 
     const NotificationTemplates = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'NotificationTemplates_202401301346.sql'),
+      path.join(sqlFilePath, 'NotificationTemplates.sql'),
     );
     await queryRunner.query(NotificationTemplates.toString());
 
-    const ProjectRisks = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'ProjectRisks_202401301443.sql'),
-    );
+    const ProjectRisks = SqlReader.readSqlFile(path.join(sqlFilePath, 'ProjectRisks.sql'));
     await queryRunner.query(ProjectRisks.toString());
 
-    const ProjectStatus = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'ProjectStatus_202401301456.sql'),
-    );
+    const ProjectStatus = SqlReader.readSqlFile(path.join(sqlFilePath, 'ProjectStatus.sql'));
     await queryRunner.query(ProjectStatus.toString());
 
-    const TierLevels = SqlReader.readSqlFile(path.join(sqlFilePath, 'TierLevels_202402011455.sql'));
+    const TierLevels = SqlReader.readSqlFile(path.join(sqlFilePath, 'TierLevels.sql'));
     await queryRunner.query(TierLevels.toString());
 
-    const Workflows = SqlReader.readSqlFile(path.join(sqlFilePath, 'Workflows_202401311420.sql'));
+    const Workflows = SqlReader.readSqlFile(path.join(sqlFilePath, 'Workflows.sql'));
     await queryRunner.query(Workflows.toString());
 
     const WorkflowProjectStatus = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'WorkflowProjectStatus_202401311454.sql'),
+      path.join(sqlFilePath, 'WorkflowProjectStatus.sql'),
     );
     await queryRunner.query(WorkflowProjectStatus.toString());
 
     const PropertyClassifications = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'PropertyClassifications_202402010859.sql'),
+      path.join(sqlFilePath, 'PropertyClassifications.sql'),
     );
     await queryRunner.query(PropertyClassifications.toString());
 
-    const PropertyTypes = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'PropertyTypes_202402011400.sql'),
-    );
+    const PropertyTypes = SqlReader.readSqlFile(path.join(sqlFilePath, 'PropertyTypes.sql'));
     await queryRunner.query(PropertyTypes.toString());
 
     const BuildingConstructionTypes = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'BuildingConstructionTypes_202401251351.sql'),
+      path.join(sqlFilePath, 'BuildingConstructionTypes.sql'),
     );
     await queryRunner.query(BuildingConstructionTypes.toString());
 
-    const Tasks = SqlReader.readSqlFile(path.join(sqlFilePath, 'Tasks_202402011411.sql'));
+    const Tasks = SqlReader.readSqlFile(path.join(sqlFilePath, 'Tasks.sql'));
     await queryRunner.query(Tasks.toString());
 
     const ProjectStatusNotifications = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'ProjectStatusNotifications_202401301558.sql'),
+      path.join(sqlFilePath, 'ProjectStatusNotifications.sql'),
     );
     await queryRunner.query(ProjectStatusNotifications.toString());
 
     const ProjectStatusTransitions = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'ProjectStatusTransitions_202401311020.sql'),
+      path.join(sqlFilePath, 'ProjectStatusTransitions.sql'),
     );
     await queryRunner.query(ProjectStatusTransitions.toString());
 
     const Roles = SqlReader.readSqlFile(path.join(sqlFilePath, 'Roles.sql'));
     await queryRunner.query(Roles.toString());
 
-    const ReportTypes = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'ReportTypes_2024020081047.sql'),
-    );
+    const ReportTypes = SqlReader.readSqlFile(path.join(sqlFilePath, 'ReportTypes.sql'));
     await queryRunner.query(ReportTypes.toString());
 
     const RegionalDistricts = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'RegionalDistricts_20240208.sql'),
+      path.join(sqlFilePath, 'RegionalDistricts.sql'),
     );
     await queryRunner.query(RegionalDistricts.toString());
 
-    // const AdministrativeAreas = SqlReader.readSqlFile(
-    //   path.join(sqlFilePath, 'AdministrativeAreas_202402091218.sql'),
-    // );
-    // await queryRunner.query(AdministrativeAreas.toString());
-    const FiscalKeys = SqlReader.readSqlFile(path.join(sqlFilePath, 'FiscalKeys_20240206.sql'));
-    await queryRunner.query(FiscalKeys.toString());
-    const EvaluationKeys = SqlReader.readSqlFile(
-      path.join(sqlFilePath, 'EvaluationKeys_20240208.sql'),
+    const AdministrativeAreas = SqlReader.readSqlFile(
+      path.join(sqlFilePath, 'AdministrativeAreas.sql'),
     );
+    await queryRunner.query(AdministrativeAreas.toString());
+
+    const FiscalKeys = SqlReader.readSqlFile(path.join(sqlFilePath, 'FiscalKeys.sql'));
+    await queryRunner.query(FiscalKeys.toString());
+
+    const EvaluationKeys = SqlReader.readSqlFile(path.join(sqlFilePath, 'EvaluationKeys.sql'));
     await queryRunner.query(EvaluationKeys.toString());
-    const ProjectTypes = SqlReader.readSqlFile(path.join(sqlFilePath, 'ProjectTypes_20240208.sql'));
+
+    const ProjectTypes = SqlReader.readSqlFile(path.join(sqlFilePath, 'ProjectTypes.sql'));
     await queryRunner.query(ProjectTypes.toString());
   }
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('TRUNCATE TABLE project_type cascade');
     await queryRunner.query('TRUNCATE TABLE evaluation_key cascade');
     await queryRunner.query('TRUNCATE TABLE fiscal_key cascade');
-    // await queryRunner.query('TRUNCATE TABLE administrative_area cascade');
+    await queryRunner.query('TRUNCATE TABLE administrative_area cascade');
     await queryRunner.query('TRUNCATE TABLE report_types cascade');
     await queryRunner.query('TRUNCATE TABLE project_status_transition cascade');
     await queryRunner.query('TRUNCATE TABLE project_status_notification cascade');
