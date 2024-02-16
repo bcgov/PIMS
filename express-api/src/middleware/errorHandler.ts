70f5244da21308a43c11c8395f08d74e3b5f8966
dbarkowsky <dylanbarkowsky@gmail.com>
2024-02-16 15:04:36 -0800

    Additional logging and type checking

--- express-api/src/middleware/errorHandler.ts
+++ express-api/src/middleware/errorHandler.ts
@@ -1,5 +1,6 @@
-import { ErrorWithCode } from "@/utilities/customErrors/ErrorWithCode";
-import { NextFunction, Request, Response } from "express";
+import { ErrorWithCode } from '@/utilities/customErrors/ErrorWithCode';
+import logger from '@/utilities/winstonLogger';
+import { NextFunction, Request, Response } from 'express';
 
 const errorHandler = (
   err: string | Error | ErrorWithCode,
@@ -7,8 +8,22 @@ const errorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
+  // Is this one of the valid input options?
+  if (!(typeof err === 'string' || err instanceof Error)) {
+    const message = `Unknown server error.`;
+    logger.error(message);
+    return res.status(500).send(message);
+  }
+  // Determine what message and status should be
   const message = err instanceof Error ? err.message : err;
-  const code = err instanceof ErrorWithCode ? err.code : 400;
+  const code = err instanceof ErrorWithCode ? err.code : 500;
+  // Report through logger
+  if (code === 500) {
+    logger.error(message);
+  } else {
+    logger.warn(message);
+  }
+  // Return status and message
   res.status(code).send(`it's broken: ${message}`);
   next();
 };
