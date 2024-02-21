9dccd98e303467330ac325cc2535b30ba8d33a22
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-02-21 15:51:42 -0800

    PIMS-1328 Express Error Handling (#2197)

--- express-api/src/utilities/customErrors/ErrorWithCode.ts
+++ express-api/src/utilities/customErrors/ErrorWithCode.ts
@@ -1,7 +1,25 @@
+/**
+ * Represents an error with an associated error code.
+ *
+ * @class ErrorWithCode
+ * @extends Error
+ *
+ * @param {string} message - The error message.
+ * @param {number} code - The error code. Defaults to 400.
+ *
+ * @example
+ * const err = new ErrorWithCode('test');
+ * console.log(err.code); // 400
+ *
+ * @example
+ * const err = new ErrorWithCode('test', 401);
+ * console.log(err.code); // 401
+ * console.log(err.message); // 'test'
+ */
 export class ErrorWithCode extends Error {
   public code: number;
 
-  constructor(message: string, code?: number) {
+  constructor(message: string, code: number = 400) {
     super(message);
     this.code = code;
   }
---
+++ express-api/tests/unit/utilities/ErrorWithCode.test.ts
@@ -0,0 +1,13 @@
+import { ErrorWithCode } from '@/utilities/customErrors/ErrorWithCode';
+describe('UNIT - ErrorWithCode', () => {
+  it('should have a default 400 error code', () => {
+    const err = new ErrorWithCode('test');
+    expect(err.code).toEqual(400);
+  });
+
+  it('should use the error code provided', () => {
+    const err = new ErrorWithCode('test', 401);
+    expect(err.code).toEqual(401);
+    expect(err.message).toBe('test');
+  });
+});
