469e88e40662d4087708d31422bd7e0c1368ee8b
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-02-26 15:37:08 -0800

    PIMS-1336 Parcels Service: getParcels (#2213)

--- express-api/tests/unit/services/parcels/parcelsService.test.ts
+++ express-api/tests/unit/services/parcels/parcelsService.test.ts
@@ -3,6 +3,7 @@ import { Parcel } from '@/typeorm/Entities/Parcel';
 import { produceParcel } from 'tests/testUtils/factories';
 import { DeepPartial } from 'typeorm';
 import * as parcelService from '@/services/parcels/parcelServices';
+import { ParcelFilterSchema } from '@/services/parcels/parcelSchema';
 
 //jest.setTimeout(30000);
 
@@ -15,6 +16,8 @@ const _parcelSave = jest
 const _parcelFindOne = jest
   .spyOn(parcelRepo, 'findOne')
   .mockImplementation(async () => produceParcel());
+
+jest.spyOn(parcelRepo, 'find').mockImplementation(async () => [produceParcel(), produceParcel()]);
 
 describe('UNIT - Parcel Services', () => {
   describe('addParcel', () => {
@@ -29,6 +32,40 @@ describe('UNIT - Parcel Services', () => {
       const parcel = produceParcel();
       _parcelFindOne.mockResolvedValueOnce(parcel);
       expect(async () => await parcelService.addParcel(parcel)).rejects.toThrow();
+    });
+  });
+
+  describe('UNIT - getParcels', () => {
+    it('should return a list of parcels', async () => {
+      const parcels = await parcelService.getParcels({});
+      expect(parcels).toHaveLength(2);
+    });
+  });
+
+  describe('UNIT - ParcelFilterSchema', () => {
+    it('should validate partial or complete filters', () => {
+      // Empty filter
+      expect(() => ParcelFilterSchema.parse({})).not.toThrow();
+      // Partial filter
+      expect(() =>
+        ParcelFilterSchema.parse({
+          agencyId: 3,
+        }),
+      ).not.toThrow();
+      // Filter with only page
+      expect(() =>
+        ParcelFilterSchema.parse({
+          page: 3,
+          quantity: 50,
+        }),
+      ).not.toThrow();
+    });
+    it('should throw an error when invalid filters are given', () => {
+      expect(() =>
+        ParcelFilterSchema.parse({
+          agencyId: 'hi',
+        }),
+      ).toThrow();
     });
   });
 });
