b0e30bd9f06d6296758ea1bea0b040a32abc1f58
taylorfries <taylor.friesen@gov.bc.ca>
2024-02-26 08:55:52 -0800

    create deleteParcelById service

--- express-api/src/services/parcels/parcelServices.ts
+++ express-api/src/services/parcels/parcelServices.ts
@@ -10,16 +10,37 @@ const parcelRepo = AppDataSource.getRepository(Parcel);
  * @returns {Response}   A 201 status and the data of the role added.
  * @throws ErrorWithCode If the parcel already exists or is unable to be added.
  */
-export const addParcel = async (parcel: Partial<Parcel>) => {
+export const postParcel = async (parcel: Parcel) => {
   const existingParcel = await getParcelByPid(parcel.PID);
   if (existingParcel) {
     throw new ErrorWithCode('Parcel already exists.', 409);
   }
+  const newParcel = parcelRepo.save(parcel);
+  return newParcel;
+};
+
+export const deleteParcelById = async (parcelId: number) => {
+  const existingParcel = await getParcelById(parcelId);
+  if (!existingParcel) {
+    throw new ErrorWithCode('Parcel Id was not found.', 404);
+  }
+  const removeParcel = parcelRepo.delete(parcelId);
+  return removeParcel;
+};
+
+/**
+ * @description Finds and returns a parcel with matching Id
+ * @param       parcelId Number representing parcel we want to find.
+ * @returns     findParcel Parcel data matching Id passed in.
+ */
+export const getParcelById = async (parcelId: number) => {
   try {
-    const newParcel = parcelRepo.save(parcel);
-    return newParcel;
+    const findParcel = await parcelRepo.findOne({
+      where: { Id: parcelId },
+    });
+    return findParcel;
   } catch (e) {
-    throw new Error(e);
+    throw new ErrorWithCode(e.message, e.status);
   }
 };
 
