3cd87d763d2cc7e588533fe927fac5b91197511d
dbarkowsky <dylanbarkowsky@gmail.com>
2024-02-26 09:00:27 -0800

    better dev example

--- react-app/src/pages/DevZone.tsx
+++ react-app/src/pages/DevZone.tsx
@@ -1,11 +1,24 @@
 /* eslint-disable no-console */
 //Simple component testing area.
-import React from 'react';
+import React, { useState } from 'react';
 import { Agency } from '@/hooks/api/useAgencyApi';
+import { Button } from '@mui/material';
 
 const Dev = () => {
   const agencies: Agency[] = [];
-  return <div>{agencies.at(0).Id}</div>;
+  const [error, setError] = useState(false);
+  return error ? (
+    <div>{agencies.at(0).Id}</div>
+  ) : (
+    <Button
+      variant="contained"
+      onClick={() => {
+        setError(true);
+      }}
+    >
+      Cause Rendering Error
+    </Button>
+  );
 };
 
 export default Dev;
