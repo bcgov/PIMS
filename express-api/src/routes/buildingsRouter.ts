2263f32b9900d0e8f655e85bdaac153dca3b1b5d
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-03-21 15:48:42 -0700

    PIMS-1472 Controller Error Wrapper (#2266)

--- express-api/src/routes/buildingsRouter.ts
+++ express-api/src/routes/buildingsRouter.ts
@@ -1,23 +1,32 @@
 import controllers from '@/controllers';
+import catchErrors from '@/utilities/controllerErrorWrapper';
 import express from 'express';
 
 export const BUILDINGS_ROUTE = '/buildings';
 
 const router = express.Router();
 
+const {
+  getBuilding,
+  updateBuilding,
+  deleteBuilding,
+  filterBuildingsQueryString,
+  addBuilding,
+  filterBuildingsRequestBody,
+  updateBuildingFinancial,
+} = controllers;
+
 // Endpoints for buildings data manipulation
 router
   .route(`${BUILDINGS_ROUTE}/:buildingId`)
-  .get(controllers.getBuilding)
-  .put(controllers.updateBuilding)
-  .delete(controllers.deleteBuilding);
+  .get(catchErrors(getBuilding))
+  .put(catchErrors(updateBuilding))
+  .delete(catchErrors(deleteBuilding));
 router
   .route(BUILDINGS_ROUTE)
-  .get(controllers.filterBuildingsQueryString)
-  .post(controllers.addBuilding);
-router.route(`${BUILDINGS_ROUTE}/filter`).post(controllers.filterBuildingsRequestBody);
-router.route(`${BUILDINGS_ROUTE}/check/pid-available`).get(controllers.checkPidAvailable);
-router.route(`${BUILDINGS_ROUTE}/check/pin-available`).get(controllers.checkPinAvailable);
-router.route(`${BUILDINGS_ROUTE}/:buildingId/financials`).put(controllers.updateBuildingFinancial);
+  .get(catchErrors(filterBuildingsQueryString))
+  .post(catchErrors(addBuilding));
+router.route(`${BUILDINGS_ROUTE}/filter`).post(catchErrors(filterBuildingsRequestBody));
+router.route(`${BUILDINGS_ROUTE}/:buildingId/financials`).put(catchErrors(updateBuildingFinancial));
 
 export default router;
