bc845885dd093d4762509ddb251bbd862d5c927f
Sharala-Perumal <Sharala.Perumal@gov.bc.ca>
2024-03-22 13:12:37 -0700

    updated BuildingsRouter

--- express-api/src/routes/buildingsRouter.ts
+++ express-api/src/routes/buildingsRouter.ts
@@ -17,12 +17,12 @@ const {
 
 // Endpoints for buildings data manipulation
 router
-  .route(`${BUILDINGS_ROUTE}/:buildingId`)
+  .route(`/:buildingId`)
   .get(catchErrors(getBuilding))
   .put(catchErrors(updateBuilding))
   .delete(catchErrors(deleteBuilding));
-router.route(BUILDINGS_ROUTE).get(catchErrors(getBuildings)).post(catchErrors(addBuilding));
-router.route(`${BUILDINGS_ROUTE}/filter`).post(catchErrors(getBuildings));
-router.route(`${BUILDINGS_ROUTE}/:buildingId/financials`).put(catchErrors(updateBuildingFinancial));
+router.route(`/`).get(catchErrors(getBuildings)).post(catchErrors(addBuilding));
+router.route(`/filter`).post(catchErrors(getBuildings));
+router.route(`/:buildingId/financials`).put(catchErrors(updateBuildingFinancial));
 
 export default router;
