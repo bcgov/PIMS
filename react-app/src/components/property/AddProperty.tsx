6d6f5a9bd5801043c7d382cbb60d34ae5f5c0660
GrahamS-Quartech <112989452+GrahamS-Quartech@users.noreply.github.com>
2024-04-10 14:49:48 -0700

    PIMS-1544: Parcel Layer Integration for Property Management (#2315)
    Co-authored-by: dbarkowsky <dylanbarkowsky@gmail.com>

--- react-app/src/components/property/AddProperty.tsx
+++ react-app/src/components/property/AddProperty.tsx
@@ -68,6 +68,7 @@ const AddProperty = () => {
       TotalArea: '',
       RentableArea: '',
       BuildingTenancy: '',
+      Location: null,
       BuildingTenancyUpdatedOn: dayjs(),
       Fiscals: years.map((yr) => ({
         FiscalYear: yr,
@@ -155,7 +156,7 @@ const AddProperty = () => {
       <Button
         onClick={async () => {
           const isValid = await formMethods.trigger();
-          if (isValid) {
+          if (isValid && formMethods.getValues()['Location'] != null) {
             setShowErrorTest(false);
             if (propertyType === 'Parcel') {
               const formValues = formMethods.getValues();
@@ -167,7 +168,6 @@ const AddProperty = () => {
                 PropertyTypeId: 0,
                 AgencyId: userContext.pimsUser.data.AgencyId,
                 IsVisibleToOtherAgencies: false,
-                Location: { x: 0, y: 0 },
                 Fiscals: formValues.Fiscals.map((a) => ({
                   ...a,
                   EffectiveDate: a?.EffectiveDate?.toDate(),
@@ -190,10 +190,6 @@ const AddProperty = () => {
                 PropertyTypeId: 0,
                 AgencyId: userContext.pimsUser.data.AgencyId,
                 IsVisibleToOtherAgencies: false,
-                Location: {
-                  x: 0,
-                  y: 0,
-                },
                 Fiscals: formValues.Fiscals.map((a) => ({
                   ...a,
                   EffectiveDate: a?.EffectiveDate?.toDate(),
