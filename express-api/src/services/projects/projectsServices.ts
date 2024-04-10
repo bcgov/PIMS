4a5fb85963e8dd4ffa946f78bafee3c12e0e56f5
dbarkowsky <dylanbarkowsky@gmail.com>
2024-04-10 14:52:41 -0700

    incorporate transaction

--- express-api/src/services/projects/projectsServices.ts
+++ express-api/src/services/projects/projectsServices.ts
@@ -5,6 +5,7 @@ import { Parcel } from '@/typeorm/Entities/Parcel';
 import { Project } from '@/typeorm/Entities/Project';
 import { ProjectProperty } from '@/typeorm/Entities/ProjectProperty';
 import { ErrorWithCode } from '@/utilities/customErrors/ErrorWithCode';
+import { DeepPartial } from 'typeorm';
 
 const projectRepo = AppDataSource.getRepository(Project);
 const projectPropertiesRepo = AppDataSource.getRepository(ProjectProperty);
@@ -15,7 +16,7 @@ export interface ProjectPropertyIds {
   buildings?: number[];
 }
 
-const addProject = async (project: Project, propertyIds?: ProjectPropertyIds) => {
+const addProject = async (project: DeepPartial<Project>, propertyIds?: ProjectPropertyIds) => {
   // Does the project have a name?
   if (!project.Name) throw new ErrorWithCode('Projects must have a name.', 400);
 
@@ -31,21 +32,28 @@ const addProject = async (project: Project, propertyIds?: ProjectPropertyIds) =>
   project.ProjectType = 1;
 
   // What type of submission is this? Regular (7) or Exemption (8)?
-  project.StatusId = project.Metadata.exemptionRequested ? 8 : 7;
+  project.StatusId = project.Metadata?.exemptionRequested ? 8 : 7;
 
   // Get a project number from the sequence
   const [{ nextval }] = await AppDataSource.query("SELECT NEXTVAL('project_num_seq')");
 
   // TODO: If drafts become possible, this can't always be SPP.
   project.ProjectNumber = `SPP-${nextval}`;
-  const newProject = await projectRepo.save(project);
-
-  // After project is saved, add parcel/building relations
-  const { parcels, buildings } = propertyIds;
-  await addProjectParcelRelations(newProject, parcels);
-  await addProjectBuildingRelations(newProject, buildings);
-
-  return newProject;
+  const queryRunner = AppDataSource.createQueryRunner();
+  await queryRunner.startTransaction();
+  try {
+    const newProject = await projectRepo.save(project);
+    // After project is saved, add parcel/building relations
+    const { parcels, buildings } = propertyIds;
+    await addProjectParcelRelations(newProject, parcels);
+    await addProjectBuildingRelations(newProject, buildings);
+    await queryRunner.commitTransaction();
+    return newProject;
+  } catch (e) {
+    await queryRunner.rollbackTransaction();
+    if (e instanceof ErrorWithCode) throw e;
+    throw new ErrorWithCode('Error creating project.', 500);
+  }
 };
 
 const addProjectParcelRelations = async (project: Project, parcelIds: number[]) => {
@@ -54,6 +62,20 @@ const addProjectParcelRelations = async (project: Project, parcelIds: number[]) 
       const existingParcel = await parcelRepo.findOne({ where: { Id: parcelId } });
       if (!existingParcel) {
         throw new ErrorWithCode(`Parcel with ID ${parcelId} does not exist.`, 404);
+      }
+      if (
+        await projectPropertiesRepo.exists({
+          where: {
+            ProjectId: project.Id,
+            PropertyTypeId: 1,
+            ParcelId: parcelId,
+          },
+        })
+      ) {
+        throw new ErrorWithCode(
+          `Parcel with ID ${parcelId} already belongs to another project.`,
+          400,
+        );
       }
       // Is this a land (0) or subdivision (2)
       const propertyType = existingParcel.ParentParcelId ? 2 : 0;
@@ -74,6 +96,20 @@ const addProjectBuildingRelations = async (project: Project, buildingIds: number
       const existingBuilding = await buildingRepo.findOne({ where: { Id: buildingId } });
       if (!existingBuilding) {
         throw new ErrorWithCode(`Building with ID ${buildingId} does not exist.`, 404);
+      }
+      if (
+        await projectPropertiesRepo.exists({
+          where: {
+            ProjectId: project.Id,
+            PropertyTypeId: 1,
+            BuildingId: buildingId,
+          },
+        })
+      ) {
+        throw new ErrorWithCode(
+          `Building with ID ${buildingId} already belongs to another project.`,
+          400,
+        );
       }
       // Property type building (1)
       const entry: Partial<ProjectProperty> = {
