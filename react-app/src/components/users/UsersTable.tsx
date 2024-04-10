fa980968611468939d3c45866f2316e71e7ecf25
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-04-10 10:22:31 -0700

    PIMS-1486 Combine utils into utilities folder (react-app) (#2310)

--- react-app/src/components/users/UsersTable.tsx
+++ react-app/src/components/users/UsersTable.tsx
@@ -5,7 +5,7 @@ import { GridColDef, GridEventListener } from '@mui/x-data-grid';
 import { MutableRefObject, PropsWithChildren, useEffect, useState } from 'react';
 import { useKeycloak } from '@bcgov/citz-imb-kc-react';
 import { IUser } from '@/interfaces/IUser';
-import { dateFormatter, statusChipFormatter } from '@/utils/formatters';
+import { dateFormatter, statusChipFormatter } from '@/utilities/formatters';
 import { GridApiCommunity } from '@mui/x-data-grid/internals';
 
 const CustomMenuItem = (props: PropsWithChildren & { value: string }) => {
