4491c2030b55dc1a6808f8a40f459393bafcb574
dbarkowsky <dylanbarkowsky@gmail.com>
2024-04-16 16:08:49 -0700

    expand Project interface

--- react-app/src/hooks/api/useProjectsApi.ts
+++ react-app/src/hooks/api/useProjectsApi.ts
@@ -1,6 +1,7 @@
 import { BaseEntityInterface } from '@/interfaces/IBaseEntity';
 import { IFetch } from '../useFetch';
 import { Agency } from './useAgencyApi';
+import { User } from '@/hooks/api/useUsersApi';
 
 export interface TierLevel extends BaseEntityInterface {
   Id: number;
@@ -41,22 +42,22 @@ export interface Project {
   Id: number;
   ProjectNumber: string;
   Name: string;
-  Manager: string;
+  Manager?: string;
   ReportedFiscalYear: string;
   ActualFiscalYear: number;
-  Description: string;
-  SubmittedOn: Date;
-  ApprovedOn: Date;
-  DeniedOn: Date;
-  CancelledOn: Date;
-  CompletedOn: Date;
-  NetBook: number;
-  Market: number;
-  Assessed: number;
-  Appraised: number;
+  Description?: string;
+  SubmittedOn?: Date;
+  ApprovedOn?: Date;
+  DeniedOn?: Date;
+  CancelledOn?: Date;
+  CompletedOn?: Date;
+  NetBook?: number;
+  Market?: number;
+  Assessed?: number;
+  Appraised?: number;
   ProjectType: number;
   AgencyId: number;
-  Agency: Agency;
+  Agency?: Agency;
   WorkflowId: number;
   Workflow?: Workflow;
   TierLevelId: number;
@@ -66,6 +67,61 @@ export interface Project {
   RiskId: number;
   Risk?: ProjectRisk;
   CreatedOn: string;
+  CreatedBy?: User;
+  UpdatedOn?: string;
+  UpdatedBy?: User;
+  Metadata?: ProjectMetadata;
+}
+
+export interface ProjectMetadata {
+  // Exemption Fields
+  exemptionRequested?: boolean;
+  exemptionApprovedOn?: Date;
+  // ERP Fields
+  initialNotificationSentOn?: Date;
+  thirtyDayNotificationSentOn?: Date;
+  sixtyDayNotificationSentOn?: Date;
+  ninetyDayNotificationSentOn?: Date;
+  onHoldNotificationSentOn?: Date;
+  interestReceivedOn?: Date;
+  transferredWithinGreOn?: Date;
+  clearanceNotificationSentOn?: Date;
+  // SPL Fields
+  requestForSplReceivedOn?: Date;
+  approvedForSplOn?: Date;
+  marketedOn?: Date;
+  purchaser?: string;
+  offerAcceptedOn?: Date;
+  adjustedOn?: Date;
+  preliminaryFormSignedOn?: Date;
+  finalFormSignedOn?: Date;
+  priorYearAdjustmentOn?: Date;
+  disposedOn?: Date;
+  // Removing from SPL
+  removalFromSplRequestOn?: Date;
+  removalFromSplApprovedOn?: Date;
+  // Financials
+  assessedOn?: Date;
+  appraisedBy?: string;
+  appraisedOn?: Date;
+  salesCost?: number;
+  netProceeds?: number;
+  programCost?: number;
+  gainLost?: number;
+  sppCapitalization?: number;
+  gainBeforeSpl?: number;
+  ocgFinancialStatement?: number;
+  interestComponent?: number;
+  plannedFutureUse?: string;
+  offerAmount?: number;
+  saleWithLeaseInPlace?: boolean;
+  priorYearAdjustment?: boolean;
+  priorYearAdjustmentAmount?: number;
+  realtor?: string;
+  realtorRate?: string;
+  realtorCommission?: number;
+  preliminaryFormSignedBy?: string;
+  finalFormSignedBy?: string;
 }
 
 const useProjectsApi = (absoluteFetch: IFetch) => {
