05fca95f428b9cb6e84eb518cf44b22e63ef9a3e
Lawrence Lau <lawrence.lau@gov.bc.ca>
2024-04-23 13:25:39 -0700

    reverting change to factories.ts

--- express-api/tests/testUtils/factories.ts
+++ express-api/tests/testUtils/factories.ts
@@ -427,7 +427,7 @@ export const produceTierLevels = (): TierLevel => {
 export const produceProject = (
   props?: Partial<Project>,
   projectProperties?: ProjectProperty[],
-): { project: Project } => {
+): Project => {
   const projectId = faker.number.int();
   const project: Project = {
     Id: projectId,
@@ -471,7 +471,7 @@ export const produceProject = (
     ],
     ...props,
   };
-  return { project };
+  return project;
 };
 
 export const produceProjectProperty = (props?: Partial<ProjectProperty>): ProjectProperty => {
