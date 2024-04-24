0435f316af0aa2499a3b809409abb0ba0c1a5ed7
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-04-24 07:49:36 -0700

    PIMS-1524/1566 Frontend Auth Checks (#2345)

--- react-app/src/guards/AuthRouteGuard.tsx
+++ react-app/src/guards/AuthRouteGuard.tsx
@@ -1,15 +1,41 @@
+import { Roles } from '@/constants/roles';
 import { AuthContext } from '@/contexts/authContext';
 import { CircularProgress } from '@mui/material';
-import { PropsWithChildren, useContext } from 'react';
+import { PropsWithChildren, useContext, useEffect } from 'react';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export interface AuthGuardProps extends PropsWithChildren {
+  permittedRoles?: Roles[];
+  redirectRoute?: string;
+  ignoreStatus?: boolean;
+}
 
 /**
  * AuthRouteGuard - Use this to wrap any component you don't want being rendered until the keycloak authentication state has resolved.
  * @param props
  * @returns
  */
-const AuthRouteGuard = (props: PropsWithChildren) => {
+const AuthRouteGuard = (props: AuthGuardProps) => {
+  const { permittedRoles, redirectRoute, ignoreStatus } = props;
   const authStateContext = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (authStateContext.pimsUser?.data && authStateContext.keycloak?.isAuthenticated) {
+      // Redirect from page if lacking roles
+      if (
+        permittedRoles &&
+        !authStateContext.keycloak.hasRole(permittedRoles, { requireAllRoles: false })
+      ) {
+        navigate(redirectRoute ?? '/');
+      }
+      // Redirect from page if user does not have Active status
+      if (!ignoreStatus && authStateContext.pimsUser?.data?.Status !== 'Active') {
+        navigate(redirectRoute ?? '/');
+      }
+    }
+  }, [authStateContext.pimsUser?.isLoading, authStateContext.keycloak.isLoggingIn]);
 
   if (!authStateContext.keycloak.isAuthenticated || authStateContext.pimsUser.isLoading) {
     return <CircularProgress sx={{ position: 'fixed', top: '50%', left: '50%' }} />;
