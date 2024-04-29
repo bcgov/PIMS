1c77d7c4312b8b2bf849ae97537ed3b23c3d6bf0
dbarkowsky <dylanbarkowsky@gmail.com>
2024-04-29 13:58:06 -0700

    adding await fixes test issue

--- express-api/tests/unit/middleware/activeUserCheck.test.ts
+++ express-api/tests/unit/middleware/activeUserCheck.test.ts
@@ -20,29 +20,29 @@ describe('UNIT - activeUserCheck middleware', () => {
   });
   const nextFunction = jest.fn();
 
-  it('should give return a 401 response if the Keycloak User is missing', () => {
+  it('should give return a 401 response if the Keycloak User is missing', async () => {
     mockRequest.user = undefined;
-    activeUserCheck(mockRequest, mockResponse, nextFunction);
+    await activeUserCheck(mockRequest, mockResponse, nextFunction);
     expect(mockResponse.status).toHaveBeenCalledWith(401);
     expect(mockResponse.send).toHaveBeenCalledWith('Unauthorized request.');
     expect(_findUserMock).toHaveBeenCalledTimes(0);
   });
 
-  it('should give return a 404 response if the user is not found', () => {
+  it('should give return a 404 response if the user is not found', async () => {
     _findUserMock.mockImplementationOnce(async () => null);
-    activeUserCheck(mockRequest, mockResponse, nextFunction);
+    await activeUserCheck(mockRequest, mockResponse, nextFunction);
     expect(mockResponse.status).toHaveBeenCalledWith(404);
     expect(mockResponse.send).toHaveBeenCalledWith('Requesting user not found.');
     expect(_findUserMock).toHaveBeenCalledTimes(1);
   });
 
-  it('should give return a 403 response if the user does not have Active status', () => {
+  it('should give return a 403 response if the user does not have Active status', async () => {
     _findUserMock.mockImplementationOnce(async () =>
       produceUser({
         Status: UserStatus.OnHold,
       }),
     );
-    activeUserCheck(mockRequest, mockResponse, nextFunction);
+    await activeUserCheck(mockRequest, mockResponse, nextFunction);
     expect(mockResponse.status).toHaveBeenCalledWith(403);
     expect(mockResponse.send).toHaveBeenCalledWith('Request forbidden. User lacks Active status.');
     expect(_findUserMock).toHaveBeenCalledTimes(1);
