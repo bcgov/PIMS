6c5ff291561650eac39f022cf918e02920002f81
Graham Stewart <graham.stewart@quartech.com>
2024-04-29 14:20:43 -0700

    Updated tests

--- express-api/src/services/projects/projectsServices.ts
+++ express-api/src/services/projects/projectsServices.ts
@@ -434,6 +434,7 @@ const updateProject = async (
   } catch (e) {
     await queryRunner.rollbackTransaction();
     logger.warn(e.message);
+    console.log(e.message);
     if (e instanceof ErrorWithCode) throw e;
     throw new ErrorWithCode('Error updating project.', 500);
   }
--- express-api/tests/testUtils/factories.ts
+++ express-api/tests/testUtils/factories.ts
@@ -20,6 +20,8 @@ import { Project } from '@/typeorm/Entities/Project';
 import { ProjectProperty } from '@/typeorm/Entities/ProjectProperty';
 import { ProjectStatusHistory } from '@/typeorm/Entities/ProjectStatusHistory';
 import { Task } from '@/typeorm/Entities/Task';
+import { ProjectTask } from '@/typeorm/Entities/ProjectTask';
+import { ProjectStatusNotification } from '@/typeorm/Entities/ProjectStatusNotification';
 
 export class MockRes {
   statusValue: any;
@@ -539,4 +541,44 @@ export const productProjectStatusHistory = (props?: Partial<ProjectStatusHistory
     ...props,
   };
   return history;
+};
+
+export const produceProjectTask = () => {
+  const task: ProjectTask = {
+    ProjectId: faker.number.int(),
+    Project: undefined,
+    TaskId: faker.number.int(),
+    Task: undefined,
+    IsCompleted: faker.datatype.boolean(),
+    CompletedOn: new Date(),
+    CreatedById: randomUUID(),
+    CreatedBy: undefined,
+    CreatedOn: new Date(),
+    UpdatedById: randomUUID(),
+    UpdatedBy: undefined,
+    UpdatedOn: new Date(),
+  };
+  return task;
+};
+
+export const produceProjectNotification = () => {
+  const notif: ProjectStatusNotification = {
+    Id: faker.number.int(),
+    TemplateId: faker.number.int(),
+    Template: undefined,
+    FromStatusId: faker.number.int(),
+    FromStatus: undefined,
+    ToStatusId: faker.number.int(),
+    ToStatus: undefined,
+    Priority: faker.number.int(),
+    Delay: faker.number.int(),
+    DelayDays: faker.number.int(),
+    CreatedById: randomUUID(),
+    CreatedBy: undefined,
+    CreatedOn: new Date(),
+    UpdatedById: randomUUID(),
+    UpdatedBy: undefined,
+    UpdatedOn: new Date(),
+  };
+  return notif;
 };
