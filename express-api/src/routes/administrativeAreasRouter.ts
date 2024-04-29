89b4869c5814d54434b522f621a0fd2c0d1ed406
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-04-29 14:36:14 -0700

    PIMS-1525 Active status check backend (#2354)

--- express-api/src/routes/administrativeAreasRouter.ts
+++ express-api/src/routes/administrativeAreasRouter.ts
@@ -6,6 +6,7 @@ import {
   updateAdministrativeAreaById,
   deleteAdministrativeAreaById,
 } from '@/controllers/administrativeAreas/administrativeAreasController';
+import activeUserCheck from '@/middleware/activeUserCheck';
 import catchErrors from '@/utilities/controllerErrorWrapper';
 import { protectedRoute } from '@bcgov/citz-imb-sso-express';
 import express from 'express';
@@ -15,13 +16,17 @@ const router = express.Router();
 // Endpoints for Admin Administrative Areas
 router
   .route(`/`)
-  .get(getAdministrativeAreas)
-  .post(protectedRoute([Roles.ADMIN]), catchErrors(addAdministrativeArea));
+  .get(catchErrors(getAdministrativeAreas))
+  .post(protectedRoute([Roles.ADMIN]), activeUserCheck, catchErrors(addAdministrativeArea));
 
 router
   .route(`/:id`)
-  .get(getAdministrativeAreaById)
-  .put(protectedRoute([Roles.ADMIN]), catchErrors(updateAdministrativeAreaById)) // TODO: Should this be a patch?
-  .delete(protectedRoute([Roles.ADMIN]), catchErrors(deleteAdministrativeAreaById));
+  .get(activeUserCheck, catchErrors(getAdministrativeAreaById))
+  .put(protectedRoute([Roles.ADMIN]), activeUserCheck, catchErrors(updateAdministrativeAreaById)) // TODO: Should this be a patch?
+  .delete(
+    protectedRoute([Roles.ADMIN]),
+    activeUserCheck,
+    catchErrors(deleteAdministrativeAreaById),
+  );
 
 export default router;
--- express-api/src/routes/propertiesRouter.ts
+++ express-api/src/routes/propertiesRouter.ts
@@ -1,5 +1,6 @@
 import controllers from '@/controllers';
 import { getPropertiesFuzzySearch } from '@/controllers/properties/propertiesController';
+import activeUserCheck from '@/middleware/activeUserCheck';
 import catchErrors from '@/utilities/controllerErrorWrapper';
 import express from 'express';
 
@@ -16,15 +17,15 @@ const {
 
 // TODO: Could these just be GET requests with query params? Then no need for /filter routes. Would cut controllers in half too.
 
-router.route('/search/fuzzy').get(catchErrors(getPropertiesFuzzySearch));
+router.route('/search/fuzzy').get(activeUserCheck, catchErrors(getPropertiesFuzzySearch));
 
-router.route('/search').get(catchErrors(getProperties));
-router.route('/search/filter').post(catchErrors(getPropertiesFilter));
+router.route('/search').get(activeUserCheck, catchErrors(getProperties));
+router.route('/search/filter').post(activeUserCheck, catchErrors(getPropertiesFilter));
 
-router.route('/search/geo').get(catchErrors(getPropertiesForMap)); // Formerly wfs route
-router.route('/search/geo/filter').post(catchErrors(getPropertiesForMapFilter));
+router.route('/search/geo').get(activeUserCheck, catchErrors(getPropertiesForMap)); // Formerly wfs route
+router.route('/search/geo/filter').post(activeUserCheck, catchErrors(getPropertiesForMapFilter));
 
-router.route('/search/page').get(catchErrors(getPropertiesPaged));
-router.route('/search/page/filter').post(catchErrors(getPropertiesPagedFilter));
+router.route('/search/page').get(activeUserCheck, catchErrors(getPropertiesPaged));
+router.route('/search/page/filter').post(activeUserCheck, catchErrors(getPropertiesPagedFilter));
 
 export default router;
