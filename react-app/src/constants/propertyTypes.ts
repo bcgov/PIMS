39ada1e7b102156be9223a4516c914abb37bb23a
dbarkowsky <dylanbarkowsky@gmail.com>
2024-04-30 11:24:31 -0700

    property type mapper

--- react-app/src/constants/propertyTypes.ts
+++ react-app/src/constants/propertyTypes.ts
@@ -3,3 +3,16 @@ export enum PropertyTypes {
   BUILDING = 1,
   SUBDIVISION = 2,
 }
+
+export const propertyTypeMapper = (id: number) => {
+  switch (id){
+    case 0:
+      return 'Land';
+    case 1:
+      return 'Building';
+    case 2:
+      return 'Subdivision';
+    default:
+      return '';
+  }
+}
