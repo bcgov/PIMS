d15396fd68cd1814550699f5dc11c8798df4454d
Sharala-Perumal <Sharala.Perumal@gov.bc.ca>
2024-05-01 12:10:06 -0700

    Added Get Token endpoint

--- express-api/src/controllers/ltsa/ltsaController.ts
+++ express-api/src/controllers/ltsa/ltsaController.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
 import { stubResponse } from '@/utilities/stubResponse';
+import * as ltsaService from '@/services/ltsa/ltsaservice';
 
+export const getToken = async (req: Request, res: Response) => {
+  const token = await ltsaService.getTokenAsync();
+  return res.status(200).send(token);
+};
 /**
  * @description Used to retrieve property information from LTSA.
  * @param   {Request}     req Incoming request
--- express-api/src/routes/ltsaRouter.ts
+++ express-api/src/routes/ltsaRouter.ts
@@ -6,6 +6,6 @@ import express from 'express';
 const router = express.Router();
 
 // Endpoints for LTSA title information
-router.route('/land/title').get(activeUserCheck, catchErrors(controllers.getLTSA));
+router.route('/land/title').get(activeUserCheck, catchErrors(controllers.getToken));
 
 export default router;
