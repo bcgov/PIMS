8d6694a42e7ac8479eea7f081351f47b7eeaf4b9
dbarkowsky <dylanbarkowsky@gmail.com>
2024-05-06 10:07:24 -0700

    icons for buildings/etc

--- react-app/src/components/map/InventoryLayer.tsx
+++ react-app/src/components/map/InventoryLayer.tsx
@@ -1,19 +1,15 @@
 import usePimsApi from '@/hooks/usePimsApi';
-import React, { useCallback, useEffect, useState } from 'react';
-import { Marker, Polygon, Popup, useMap } from 'react-leaflet';
-import L, { LatLngExpression } from 'leaflet';
+import React, { useEffect, useState } from 'react';
+import { Marker } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
 import useDataLoader from '@/hooks/useDataLoader';
 import { PropertyGeo } from '@/hooks/api/usePropertiesApi';
-import { CircularProgress } from '@mui/material';
-import { parcelIcon } from '@/components/map/markers/propertyPins';
+import { getMatchingPropertyPin } from '@/components/map/markers/propertyPins';
 
 export const InventoryLayer = () => {
   const api = usePimsApi();
-  const map = useMap();
   const { data, refreshData } = useDataLoader(api.properties.propertiesGeoSearch);
   const [properties, setProperties] = useState<PropertyGeo[]>([]);
-  const [filter, setFilter] = useState({});
 
   useEffect(() => {
     if (data && data.length > 0) {
@@ -23,23 +19,13 @@ export const InventoryLayer = () => {
     }
   }, [data]);
 
-  useEffect(() => {
-    refreshData();
-  }, [filter]);
-
   return (
-    <MarkerClusterGroup
-      chunkedLoading
-      removeOutsideVisibleBounds
-      chunkInterval={100}
-      chunkDelay={1000}
-      showCoverageOnHover={false}
-    >
+    <MarkerClusterGroup chunkedLoading removeOutsideVisibleBounds showCoverageOnHover>
       {properties.map((property: PropertyGeo) => (
         <Marker
           key={`${property.Id} + ${property.PropertyTypeId}`}
           position={[property.Location.y, property.Location.x]}
-          icon={parcelIcon}
+          icon={getMatchingPropertyPin(property.PropertyTypeId)}
         >
           {/* TODO: Marker popup goes here */}
         </Marker>
--- react-app/src/components/map/markers/propertyPins.ts
+++ react-app/src/components/map/markers/propertyPins.ts
@@ -1,4 +1,4 @@
-import L from "leaflet";
+import L, { IconOptions } from 'leaflet';
 import buildingErpPng from '@/assets/markers/building-erp.png';
 import buildingErpHighlightPng from '@/assets/markers/building-erp-highlight.png';
 import buildingIconPng from '@/assets/markers/building-reg.png';
@@ -19,193 +19,153 @@ import subdivRegPng from '@/assets/markers/subdiv-reg.png';
 import subdivRegHighlightPng from '@/assets/markers/subdiv-reg-highlight.png';
 import subdivSplPng from '@/assets/markers/subdiv-spl.png';
 import subdivSplHighlightPng from '@/assets/markers/subdiv-spl-highlight.png';
+import { PropertyTypes } from '@/constants/propertyTypes';
 
-// parcel icon (green)
-export const parcelIcon = L.icon({
-  iconUrl: parcelIconPng,
+// TODO: Expand to handle more Property Pin types
+export const getMatchingPropertyPin = (propertyType: PropertyTypes) => {
+  switch (true) {
+    case propertyType === PropertyTypes.LAND:
+      return parcelIcon;
+    case propertyType === PropertyTypes.BUILDING:
+      return buildingIcon;
+    case propertyType === PropertyTypes.SUBDIVISION:
+      return subdivisionIcon;
+    default:
+      return geocoderIcon;
+  }
+}
+
+// default icon values
+const defaults: Partial<IconOptions> = {
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
+};
+
+// parcel icon (green)
+export const parcelIcon = L.icon({
+  ...defaults,
+  iconUrl: parcelIconPng,
 });
 
 // parcel icon (green) highlighted
 export const parcelIconSelect = L.icon({
+  ...defaults,
   iconUrl: parcelSelectedIconPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // building icon (blue)
 export const buildingIcon = L.icon({
+  ...defaults,
   iconUrl: buildingIconPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // building icon (blue) highlighted
 export const buildingIconSelect = L.icon({
+  ...defaults,
   iconUrl: buildingSelectedIconPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // subdivision icon (green)
 export const subdivisionIcon = L.icon({
+  ...defaults,
   iconUrl: subdivRegPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // subdivision icon (green) highlighted
 export const subdivisionIconSelect = L.icon({
+  ...defaults,
   iconUrl: subdivRegHighlightPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 export const geocoderIcon = L.icon({
+  ...defaults,
   iconUrl: markerGreenPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // draft parcel icon (green)
 export const draftParcelIcon = L.icon({
+  ...defaults,
   iconUrl: markerGreenPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
   className: 'draft',
 });
 
 // draft building icon (blue)
 export const draftBuildingIcon = L.icon({
+  ...defaults,
   iconUrl: markerBluePng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
   className: 'draft',
 });
 
 // spp icon (purple)
 export const landSppIcon = L.icon({
+  ...defaults,
   iconUrl: landSplPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // spp icon (purple) highlighted
 export const landSppIconSelect = L.icon({
+  ...defaults,
   iconUrl: landSplHighlightPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // erp icon (red)
 export const landErpIcon = L.icon({
+  ...defaults,
   iconUrl: landErpPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // erp icon (red) highlight
 export const landErpIconSelect = L.icon({
+  ...defaults,
   iconUrl: landErpHighlightPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // spp icon (purple)
 export const buildingSppIcon = L.icon({
+  ...defaults,
   iconUrl: buildingSplPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // spp icon (purple) highlight
 export const buildingSppIconSelect = L.icon({
+  ...defaults,
   iconUrl: buildingSplHighlightPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // erp icon (red)
 export const buildingErpIcon = L.icon({
+  ...defaults,
   iconUrl: buildingErpPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // erp icon (red) highlighted
 export const buildingErpIconSelect = L.icon({
+  ...defaults,
   iconUrl: buildingErpHighlightPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // spp icon (purple)
 export const subdivisionSppIcon = L.icon({
+  ...defaults,
   iconUrl: subdivSplPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // spp icon (purple) highlighted
 export const subdivisionSppIconSelect = L.icon({
+  ...defaults,
   iconUrl: subdivSplHighlightPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // erp icon (red)
 export const subdivisionErpIcon = L.icon({
+  ...defaults,
   iconUrl: subdivErpPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 // erp icon (red) highlight
 export const subdivisionErpIconSelect = L.icon({
+  ...defaults,
   iconUrl: subdivErpHighlightPng,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
