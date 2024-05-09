1d6b98de8154c7dcb715604f29a731684fb5e216
Graham Stewart <graham.stewart@quartech.com>
2024-05-09 14:35:04 -0700

    Added collapisble sidebar to project page

--- react-app/src/components/projects/ProjectDetail.tsx
+++ react-app/src/components/projects/ProjectDetail.tsx
@@ -19,6 +19,7 @@ import {
   ProjectPropertiesDialog,
 } from './ProjectDialog';
 import { AgencySimpleTable } from './AgencyResponseSearchTable';
+import CollapsibleSidebar from '../layout/CollapsibleSidebar';
 
 interface IProjectDetail {
   onClose: () => void;
@@ -116,161 +117,181 @@ const ProjectDetail = (props: IProjectDetail) => {
     refreshData();
   }, [id]);
 
+  const projectInformation = 'Project Information';
+  const disposalProperties = 'Disposal Properties';
+  const financialInformation = 'Financial Information';
+  const agencyInterest = 'Agency Interest';
+  const documentationHistory = 'Documentation History';
+
   return (
-    <Box
-      display={'flex'}
-      gap={'1rem'}
-      mt={'2rem'}
-      flexDirection={'column'}
-      width={'46rem'}
-      marginX={'auto'}
+    <CollapsibleSidebar
+      items={[
+        { title: projectInformation },
+        { title: disposalProperties },
+        { title: financialInformation },
+        { title: agencyInterest },
+        { title: documentationHistory },
+      ]}
     >
-      <DetailViewNavigation
-        navigateBackTitle={'Back to disposal Overview'}
-        deleteTitle={'Delete project'}
-        onDeleteClick={() => setOpenDeleteDialog(true)}
-        onBackClick={() => props.onClose()}
-      />
-      <DataCard
-        loading={isLoading}
-        customFormatter={customFormatter}
-        values={ProjectInfoData}
-        title={'Project Information'}
-        onEdit={() => setOpenProjectInfoDialog(true)}
-      />
-      <DataCard
-        id={`$Disposal Properties`}
-        values={undefined}
-        title={'Disposal Properties'}
-        onEdit={() => setOpenDisposalPropDialog(true)}
+      <Box
+        display={'flex'}
+        gap={'1rem'}
+        mt={'2rem'}
+        flexDirection={'column'}
+        width={'46rem'}
+        marginX={'auto'}
       >
-        {isLoading ? (
-          <Skeleton variant="rectangular" height={'150px'} />
-        ) : (
-          <DisposalPropertiesTable
-            rows={[
-              ...(data?.Parcels?.map((p) => ({ ...p, PropertyType: 'Parcel' })) ?? []),
-              ...(data?.Buildings?.map((b) => ({ ...b, PropertyType: 'Building' })) ?? []),
-            ]}
-          />
-        )}
-      </DataCard>
-      <DataCard
-        loading={isLoading}
-        customFormatter={customFormatter}
-        values={FinancialInformationData}
-        title={'Financial Information'}
-        onEdit={() => setOpenFinancialInfoDialog(true)}
-      />
-      <DataCard
-        title={'Agency Interest'}
-        values={undefined}
-        onEdit={() => setOpenAgencyInterestDialog(true)}
-      >
-        {!data ? ( //TODO: Logic will depend on precense of agency responses
-          <Box display={'flex'} justifyContent={'center'}>
-            <Typography>No agencies registered.</Typography>
-          </Box>
-        ) : (
-          <AgencySimpleTable
-            sx={{
-              borderStyle: 'none',
-              '& .MuiDataGrid-columnHeaders': {
-                borderBottom: 'none',
-              },
-              '& div div div div >.MuiDataGrid-cell': {
-                borderBottom: 'none',
-                borderTop: '1px solid rgba(224, 224, 224, 1)',
-              },
-            }}
-            rows={[]}
-          />
-        )}
-      </DataCard>
-      <DataCard
-        customFormatter={customFormatter}
-        values={undefined}
-        disableEdit={true}
-        title={'Documentation History'}
-        onEdit={() => setOpenDocumentationDialog(true)}
-      >
-        {Object.entries(collectedTasksByStatus)?.map(
-          (
-            [key, value], //Each key here is a status name. Each value a list of tasks.
-          ) => (
-            <Box key={`${key}-group`}>
-              <Typography variant="h5" mt={'1rem'}>
-                {key}
-              </Typography>
-              {value.map((task) => (
-                <FormGroup key={`${task.TaskId}-task-formgroup`}>
-                  <FormControlLabel
-                    control={<Checkbox checked={task.IsCompleted} />}
-                    style={{ pointerEvents: 'none' }}
-                    value={task.IsCompleted}
-                    label={task.Name}
-                    disabled={false}
-                  />
-                </FormGroup>
-              ))}
+        <DetailViewNavigation
+          navigateBackTitle={'Back to disposal Overview'}
+          deleteTitle={'Delete project'}
+          onDeleteClick={() => setOpenDeleteDialog(true)}
+          onBackClick={() => props.onClose()}
+        />
+        <DataCard
+          loading={isLoading}
+          customFormatter={customFormatter}
+          values={ProjectInfoData}
+          id={projectInformation}
+          title={projectInformation}
+          onEdit={() => setOpenProjectInfoDialog(true)}
+        />
+        <DataCard
+          values={undefined}
+          id={disposalProperties}
+          title={disposalProperties}
+          onEdit={() => setOpenDisposalPropDialog(true)}
+        >
+          {isLoading ? (
+            <Skeleton variant="rectangular" height={'150px'} />
+          ) : (
+            <DisposalPropertiesTable
+              rows={[
+                ...(data?.Parcels?.map((p) => ({ ...p, PropertyType: 'Parcel' })) ?? []),
+                ...(data?.Buildings?.map((b) => ({ ...b, PropertyType: 'Building' })) ?? []),
+              ]}
+            />
+          )}
+        </DataCard>
+        <DataCard
+          loading={isLoading}
+          customFormatter={customFormatter}
+          values={FinancialInformationData}
+          title={financialInformation}
+          id={financialInformation}
+          onEdit={() => setOpenFinancialInfoDialog(true)}
+        />
+        <DataCard
+          title={agencyInterest}
+          values={undefined}
+          id={agencyInterest}
+          onEdit={() => setOpenAgencyInterestDialog(true)}
+        >
+          {!data ? ( //TODO: Logic will depend on precense of agency responses
+            <Box display={'flex'} justifyContent={'center'}>
+              <Typography>No agencies registered.</Typography>
             </Box>
-          ),
-        )}
-      </DataCard>
-      <DeleteDialog
-        open={openDeleteDialog}
-        title={'Delete property'}
-        message={'Are you sure you want to delete this project?'}
-        onDelete={async () => {}} //Purposefully omitted for now.
-        onClose={async () => setOpenDeleteDialog(false)}
-      />
-      <ProjectGeneralInfoDialog
-        initialValues={data}
-        open={openProjectInfoDialog}
-        postSubmit={() => {
-          setOpenProjectInfoDialog(false);
-          refreshData();
-        }}
-        onCancel={() => setOpenProjectInfoDialog(false)}
-      />
-      <ProjectFinancialDialog
-        initialValues={data}
-        open={openFinancialInfoDialog}
-        postSubmit={() => {
-          setOpenFinancialInfoDialog(false);
-          refreshData();
-        }}
-        onCancel={() => setOpenFinancialInfoDialog(false)}
-      />
-      <ProjectDocumentationDialog
-        initialValues={data}
-        open={openDocumentationDialog}
-        postSubmit={() => {
-          setOpenDocumentationDialog(false);
-          refreshData();
-        }}
-        onCancel={() => setOpenDocumentationDialog(false)}
-      />
-      <ProjectPropertiesDialog
-        initialValues={data}
-        open={openDisposalPropDialog}
-        postSubmit={() => {
-          setOpenDisposalPropDialog(false);
-          refreshData();
-        }}
-        onCancel={() => setOpenDisposalPropDialog(false)}
-      />
-      <ProjectAgencyResponseDialog
-        initialValues={undefined}
-        open={openAgencyInterestDialog}
-        postSubmit={() => {
-          setOpenAgencyInterestDialog(false);
-        }}
-        onCancel={() => {
-          setOpenAgencyInterestDialog(false);
-        }}
-      />
-    </Box>
+          ) : (
+            <AgencySimpleTable
+              sx={{
+                borderStyle: 'none',
+                '& .MuiDataGrid-columnHeaders': {
+                  borderBottom: 'none',
+                },
+                '& div div div div >.MuiDataGrid-cell': {
+                  borderBottom: 'none',
+                  borderTop: '1px solid rgba(224, 224, 224, 1)',
+                },
+              }}
+              rows={[]}
+            />
+          )}
+        </DataCard>
+        <DataCard
+          customFormatter={customFormatter}
+          values={undefined}
+          disableEdit={true}
+          title={documentationHistory}
+          id={documentationHistory}
+          onEdit={() => setOpenDocumentationDialog(true)}
+        >
+          {Object.entries(collectedTasksByStatus)?.map(
+            (
+              [key, value], //Each key here is a status name. Each value a list of tasks.
+            ) => (
+              <Box key={`${key}-group`}>
+                <Typography variant="h5" mt={'1rem'}>
+                  {key}
+                </Typography>
+                {value.map((task) => (
+                  <FormGroup key={`${task.TaskId}-task-formgroup`}>
+                    <FormControlLabel
+                      control={<Checkbox checked={task.IsCompleted} />}
+                      style={{ pointerEvents: 'none' }}
+                      value={task.IsCompleted}
+                      label={task.Name}
+                      disabled={false}
+                    />
+                  </FormGroup>
+                ))}
+              </Box>
+            ),
+          )}
+        </DataCard>
+        <DeleteDialog
+          open={openDeleteDialog}
+          title={'Delete property'}
+          message={'Are you sure you want to delete this project?'}
+          onDelete={async () => {}} //Purposefully omitted for now.
+          onClose={async () => setOpenDeleteDialog(false)}
+        />
+        <ProjectGeneralInfoDialog
+          initialValues={data}
+          open={openProjectInfoDialog}
+          postSubmit={() => {
+            setOpenProjectInfoDialog(false);
+            refreshData();
+          }}
+          onCancel={() => setOpenProjectInfoDialog(false)}
+        />
+        <ProjectFinancialDialog
+          initialValues={data}
+          open={openFinancialInfoDialog}
+          postSubmit={() => {
+            setOpenFinancialInfoDialog(false);
+            refreshData();
+          }}
+          onCancel={() => setOpenFinancialInfoDialog(false)}
+        />
+        <ProjectDocumentationDialog
+          initialValues={data}
+          open={openDocumentationDialog}
+          postSubmit={() => {
+            setOpenDocumentationDialog(false);
+            refreshData();
+          }}
+          onCancel={() => setOpenDocumentationDialog(false)}
+        />
+        <ProjectPropertiesDialog
+          initialValues={data}
+          open={openDisposalPropDialog}
+          postSubmit={() => {
+            setOpenDisposalPropDialog(false);
+            refreshData();
+          }}
+          onCancel={() => setOpenDisposalPropDialog(false)}
+        />
+        <ProjectAgencyResponseDialog
+          initialValues={undefined}
+          open={openAgencyInterestDialog}
+          postSubmit={() => {
+            setOpenAgencyInterestDialog(false);
+          }}
+          onCancel={() => {
+            setOpenAgencyInterestDialog(false);
+          }}
+        />
+      </Box>
+    </CollapsibleSidebar>
   );
 };
 
