9d320b4a6c9d86b265d782ac5b2b46e6a3e8d073
Sharala-Perumal <Sharala.Perumal@gov.bc.ca>
2024-05-13 12:39:43 -0700

    updated title ownership

--- react-app/src/components/ltsa/TitleOwnership.tsx
+++ react-app/src/components/ltsa/TitleOwnership.tsx
@@ -1,33 +1,102 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import { ILtsaOrderInfo } from './ILtsaOrderInfo';
 import usePimsApi from '@/hooks/usePimsApi';
-import { useContext } from 'react';
-import { AuthContext } from '@/contexts/authContext';
-import { Roles } from '@/constants/roles';
+import DataCard from '../display/DataCard';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import useDataLoader from '@/hooks/useDataLoader';
+import { Box } from '@mui/material';
+import LtsaOwnershipTable from './LtsaOwnershipTable';
+import LtsaChargeTable from './LtsaChargeTable';
 
-interface ILTSADetails {
-  ltsa: ILtsaOrderInfo;
-  pid?: string;
-}
-
-const TitleOwnership = (props: ILTSADetails) => {
-  const navigate = useNavigate();
+const TitleOwnership = () => {
   const { pid } = useParams();
   const api = usePimsApi();
-  const userContext = useContext(AuthContext);
-  if (!userContext.keycloak.hasRoles([Roles.ADMIN, Roles.AUDITOR], { requireAllRoles: false })) {
-    navigate('/');
-  }
-  const { data, refreshData } = useDataLoader(() => api.ltsa.getLtsabyPid(Number(pid)));
+  const { data, refreshData, isLoading } = useDataLoader(() => api.ltsa.getLtsabyPid(Number(pid)));
+
+  useEffect(() => {
+    refreshData();
+  }, []);
 
   const TitleDetails = {
     TitleNumber: data?.order.productOrderParameters.titleNumber,
     LegalDescription:
-      data?.order.orderedProduct.fieldedData.descriptionsOfLand[0].fullLegalDescription,
+      data?.order.orderedProduct.fieldedData.descriptionsOfLand[0]?.fullLegalDescription,
     TitleStatus: data?.order.status,
-    SalesHistory: data?.order.orderedProduct.fieldedData.tombstone.marketValueAmount,
-    ApplicationReceived: data?.order.orderedProduct.fieldedData.tombstone.applicationReceivedDate,
-    EnteredOn: data?.order.orderedProduct.fieldedData.tombstone.enteredDate,
+    SalesHistory: data?.order.orderedProduct.fieldedData.tombstone?.marketValueAmount,
+    ApplicationReceived: data?.order.orderedProduct.fieldedData.tombstone?.applicationReceivedDate,
+    EnteredOn: data?.order.orderedProduct.fieldedData.tombstone?.enteredDate,
   };
+
+  const ownershipDetails = {
+    rows: [],
+    length: data?.order.orderedProduct.fieldedData.ownershipGroups.length || 0,
+    jointTenancyIndication:
+      data?.order.orderedProduct.fieldedData.ownershipGroups[0]?.jointTenancyIndication || false,
+    interestFractionNumerator:
+      data?.order.orderedProduct.fieldedData.ownershipGroups[0]?.interestFractionNumerator || '',
+    interestFractionDenominator:
+      data?.order.orderedProduct.fieldedData.ownershipGroups[0]?.interestFractionDenominator || '',
+    titleOwners:
+      data?.order.orderedProduct.fieldedData.ownershipGroups.map((group) => ({
+        lastNameOrCorpName1: group.titleOwners[0].lastNameOrCorpName1,
+        givenName: group.titleOwners[0].givenName,
+        incorporationNumber: group.titleOwners[0].incorporationNumber,
+      })) || [],
+  };
+
+  const chargeDetails = {
+    rows: [],
+    length: data?.order.orderedProduct.fieldedData.chargesOnTitle?.length || 0,
+    chargeNumber: data?.order.orderedProduct.fieldedData.chargesOnTitle[0]?.chargeNumber,
+    status: data?.order.orderedProduct.fieldedData.chargesOnTitle[0]?.status,
+    enteredDate: data?.order.orderedProduct.fieldedData.chargesOnTitle[0]?.enteredDate,
+    chargeRemarks: data?.order.orderedProduct.fieldedData.chargesOnTitle[0]?.chargeRemarks,
+    ineteralia: data?.order.orderedProduct.fieldedData.chargesOnTitle[0]?.interAlia,
+    charge: data?.order.orderedProduct.fieldedData.chargesOnTitle?.map((group) => ({
+      receivedDate: group.charge.applicationReceivedDate,
+      chargeNumber: group.charge.chargeNumber,
+      transactionType: group.charge.transactionType,
+      chargeOwnershipGroups: group.charge.chargeOwnershipGroups,
+    })),
+    chargeRelease: {},
+  };
+
+  return (
+    <>
+      <Box
+        display={'flex'}
+        gap={'1rem'}
+        mt={'2rem'}
+        flexDirection={'column'}
+        width={'46rem'}
+        marginX={'auto'}
+      ></Box>
+      <DataCard
+        loading={isLoading}
+        values={TitleDetails}
+        title={'Title Details'}
+        disableEdit={true}
+        onEdit={undefined}
+      />
+      <DataCard
+        loading={isLoading}
+        values={undefined}
+        title={'Ownership Information by Interest Fraction'}
+        disableEdit={true}
+        onEdit={undefined}
+      >
+        <LtsaOwnershipTable row={ownershipDetails} />
+      </DataCard>
+      <DataCard
+        loading={isLoading}
+        values={undefined}
+        title={'Charge Details'}
+        disableEdit={true}
+        onEdit={undefined}
+      >
+        <LtsaChargeTable row={chargeDetails} />
+      </DataCard>
+    </>
+  );
 };
+
+export default TitleOwnership;
