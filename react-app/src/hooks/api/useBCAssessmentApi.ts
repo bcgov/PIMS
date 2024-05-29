d065be2604c0bf1e42a77da3d6231444c7f0ecf0
dbarkowsky <dylanbarkowsky@gmail.com>
2024-05-29 15:44:15 -0700

    edit bca url

--- react-app/src/hooks/api/useBCAssessmentApi.ts
+++ react-app/src/hooks/api/useBCAssessmentApi.ts
@@ -8,7 +8,7 @@ const useBCAssessmentApi = (absoluteFetch: IFetch) => {
     'https://test.apps.gov.bc.ca/ext/sgw/geo.bca?REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&typeName=geo.bca:WHSE_HUMAN_CULTURAL_ECONOMIC.BCA_FOLIO_GNRL_PROP_VALUES_SV&outputFormat=application/json';
 
   const getBCAssessmentByLocation = async (lat: string, lng: string) => {
-    const finalUrl = `${testURL}&CQL_FILTER=CONTAINS(SHAPE,SRID=4326;POINT(${lng} ${lat}))`;
+    const finalUrl = `${testURL}srsName=EPSG:4326&CQL_FILTER=CONTAINS(SHAPE,SRID=4326;POINT(${lng} ${lat}))`;
     const { parsedBody } = await absoluteFetch.get(
       finalUrl,
       {},
