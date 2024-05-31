2e42f42448329b4e24ea559a1cb3dd1a22f937a1
GrahamS-Quartech <112989452+GrahamS-Quartech@users.noreply.github.com>
2024-05-31 13:11:37 -0700

    PIMS-1713: Project Note Enumeration (#2423)

---
+++ express-api/src/typeorm/Entities/NoteType.ts
@@ -0,0 +1,32 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from './abstractEntities/BaseEntity';
+import { ProjectStatus } from './ProjectStatus';
+
+@Entity()
+export class NoteType extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  Id: number;
+
+  @Column({ type: 'character varying', length: 150 })
+  Name: string;
+
+  @Column('boolean', { default: false })
+  IsDisabled: boolean;
+
+  @Column('int', { default: 0 })
+  SortOrder: number;
+
+  @Column('text', { nullable: true })
+  Description: string;
+
+  @Column('boolean')
+  IsOptional: boolean;
+
+  // Status Relation
+  @Column({ name: 'status_id', type: 'int', nullable: true })
+  StatusId: number;
+
+  @ManyToOne(() => ProjectStatus, (ProjectStatus) => ProjectStatus.Id, { nullable: true })
+  @JoinColumn({ name: 'status_id' })
+  Status: ProjectStatus;
+}
