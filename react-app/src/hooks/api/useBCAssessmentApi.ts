f589cdea8fd64d6af186c5b63c2ab4a99ef20767
Graham Stewart <graham.stewart@quartech.com>
2024-05-31 15:43:37 -0700

    Trying without headers manually set

--- react-app/src/hooks/api/useBCAssessmentApi.ts
+++ react-app/src/hooks/api/useBCAssessmentApi.ts
@@ -6,7 +6,7 @@ const useBCAssessmentApi = (absoluteFetch: IFetch) => {
     'https://apps.gov.bc.ca/ext/sgw/geo.bca?REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&typeName=geo.bca:WHSE_HUMAN_CULTURAL_ECONOMIC.BCA_FOLIO_GNRL_PROP_VALUES_SV&outputFormat=application/json';
   const testURL =
     'https://test.apps.gov.bc.ca/ext/sgw/geo.bca?REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&typeName=geo.bca:WHSE_HUMAN_CULTURAL_ECONOMIC.BCA_FOLIO_GNRL_PROP_VALUES_SV&outputFormat=application/json';
-  
+
   //  https://test.apps.gov.bc.ca/ext/sgw/geo.bca?REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&typeName=geo.bca:WHSE_HUMAN_CULTURAL_ECONOMIC.BCA_FOLIO_GNRL_PROP_VALUES_SV&outputFormat=application/json&srsName=EPSG:4326&CQL_FILTER=CONTAINS(SHAPE,SRID=4326;POINT(-123.36905121803285 48.41397415311252))
   const getBCAssessmentByLocation = async (lat: string, lng: string) => {
     const finalUrl = `${testURL}&srsName=EPSG:4326&CQL_FILTER=CONTAINS(SHAPE,SRID=4326;POINT(${lng} ${lat}))`;
@@ -14,10 +14,10 @@ const useBCAssessmentApi = (absoluteFetch: IFetch) => {
       finalUrl,
       {},
       {
-        headers: {
-          'Access-Control-Allow-Headers': 'Origin',
-          'Access-Control-Allow-Origin': '*',
-        },
+        // headers: {
+        //   'Access-Control-Allow-Headers': 'Origin',
+        //   'Access-Control-Allow-Origin': '*',
+        // },
         credentials: 'include',
       },
     );
