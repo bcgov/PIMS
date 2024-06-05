1a23e2dba14eb8c37d9d4e5cfd237ab239bd33c8
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-06-05 08:52:49 -0700

    PIMS-1652 Clusters Rework (#2435)

--- react-app/src/components/map/InventoryLayer.tsx
+++ react-app/src/components/map/InventoryLayer.tsx
@@ -2,13 +2,11 @@ import usePimsApi from '@/hooks/usePimsApi';
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 import useDataLoader from '@/hooks/useDataLoader';
 import { MapFilter, PropertyGeo } from '@/hooks/api/usePropertiesApi';
-import PropertyMarker from '@/components/map/markers/PropertyMarker';
-import { Marker, Polyline, useMap, useMapEvents } from 'react-leaflet';
+import { Marker, useMap, useMapEvents } from 'react-leaflet';
 import useSupercluster from 'use-supercluster';
 import './clusterHelpers/clusters.css';
 import L, { LatLngExpression } from 'leaflet';
 import { BBox } from 'geojson';
-import { Spiderfier } from '@/components/map/clusterHelpers/Spiderfier';
 import { SnackBarContext } from '@/contexts/snackbarContext';
 
 export interface InventoryLayerProps {
@@ -110,11 +108,6 @@ export const InventoryLayer = (props: InventoryLayerProps) => {
     ]);
     const zoom = map.getZoom();
     setClusterZoom(zoom);
-    // If zooming out too far, close the spidered cluster
-    if (zoom < 16) {
-      setSpider({});
-      setSelectedCluster(undefined);
-    }
   };
 
   // Update clusters once upon load
@@ -133,41 +126,24 @@ export const InventoryLayer = (props: InventoryLayerProps) => {
 
   // Create icons for clusters
   const icons = {};
-  const makeClusterIcon = (count, size) => {
-    // Gets colour based on count of cluster
-    const getColour = () => {
-      const maxHue = 120; // 120 is max for a nice green
-      const consideredBig = data?.length ? data?.length / 5 : 1000; // Change this to affect how the colour scales
-      const colourScore = (1 - count / consideredBig) * maxHue;
-      const hue = Math.max(0, Math.min(maxHue, colourScore)).toString(10);
-      return ['hsl(', hue, ',60%,70%)'].join('');
-    };
-
+  const makeClusterIcon = (count: number) => {
     // Only make one icon per identical count
+    const size = 10 + (count / properties.length) * 40;
     if (!icons[count]) {
-      icons[count] = L.divIcon({
-        html: `<div class="cluster-marker" style="width: ${size}px; height: ${size}px; background-color: ${getColour()}; border: solid 1px white">
-        ${count}
+      const displayCount = count < 1000 ? count : `${(count / 1000).toFixed(1)}K`;
+      return (icons[count] = L.divIcon({
+        html: `<div class="cluster-marker" style="width: ${size}px; height: ${size}px; background-color: rgb(120,120,120); color: white;">
+        ${displayCount}
       </div>`,
-      });
+      }));
     }
     return icons[count];
   };
 
-  // Spiderfy clustered markers
-  const spiderfier = new Spiderfier(map, {
-    getClusterPoints: (clusterId) => supercluster?.getLeaves(clusterId, Infinity) ?? [],
-    spiderfyDistanceMultiplier: map.getZoom() * 0.1,
-  });
-  // Track the spider object
-  const [spider, setSpider] = useState<any>({});
-  // Track the currently selected cluster
-  const [selectedCluster, setSelectedCluster] = useState(undefined);
-
-  // Decide if clusters should zoom or expand into spider
-  const zoomOrSpiderfy = useCallback(
+  // Zoom towards cluster enough to break into small clusters
+  const zoomOnCluster = useCallback(
     (cluster: PropertyGeo & ClusterGeo) => {
-      if (!supercluster || !spiderfier || !cluster) {
+      if (!supercluster || !cluster) {
         return;
       }
       const expansionZoom = Math.min(
@@ -175,25 +151,16 @@ export const InventoryLayer = (props: InventoryLayerProps) => {
         maxZoom,
       );
 
-      const showCluster = () => {
-        const res = spiderfier.spiderfy(cluster);
-        setSpider(res);
-      };
-      // already at maxZoom, need to spiderfy child markers
-      if (expansionZoom === map.getZoom()) {
-        showCluster();
-      } else {
-        // Zoom towards cluster
-        map.setView(
-          [cluster.geometry.coordinates[1], cluster.geometry.coordinates[0]],
-          expansionZoom,
-          {
-            animate: true,
-          },
-        );
-      }
+      // Zoom towards cluster
+      map.setView(
+        [cluster.geometry.coordinates[1], cluster.geometry.coordinates[0]],
+        expansionZoom,
+        {
+          animate: true,
+        },
+      );
     },
-    [map, supercluster, selectedCluster],
+    [map, supercluster],
   );
 
   // Update map after these actions
@@ -212,67 +179,35 @@ export const InventoryLayer = (props: InventoryLayerProps) => {
             <Marker
               key={`cluster-${property.properties.cluster_id}`}
               position={[property.geometry.coordinates[1], property.geometry.coordinates[0]]} // Flip this back to leaflet-expected positions
-              icon={makeClusterIcon(
-                property.properties.point_count,
-                10 + (property.properties.point_count / properties.length) * 40,
-              )}
+              icon={makeClusterIcon(property.properties.point_count)}
+              eventHandlers={{
+                click: () => zoomOnCluster(property),
+              }}
+            />
+          );
+        } else {
+          // Cluster icons of 1
+          return (
+            <Marker
+              key={`cluster-${property.properties.Id}`}
+              position={[property.geometry.coordinates[1], property.geometry.coordinates[0]]} // Flip this back to leaflet-expected positions
+              icon={makeClusterIcon(1)}
               eventHandlers={{
                 click: () => {
-                  if (spider.markers) {
-                    spiderfier.unspiderfy();
-                    setSpider({});
-                    setSelectedCluster(undefined);
-                  } else {
-                    setSelectedCluster(property);
-                    zoomOrSpiderfy(property);
-                  }
+                  // Zoom fully towards cluster of 1
+                  map.setView(
+                    [property.geometry.coordinates[1], property.geometry.coordinates[0]],
+                    maxZoom,
+                    {
+                      animate: true,
+                    },
+                  );
                 },
               }}
             />
           );
         }
-        // Not a cluster, return a regular marker
-        return (
-          <PropertyMarker
-            key={`${property.properties.Id} + ${property.properties.PropertyTypeId}`}
-            property={property}
-          />
-        );
       })}
-      {/* For all markers on spidered clusters, return a marker */}
-      {spider.markers?.map(
-        (
-          spiderMarker: PropertyGeo &
-            ClusterGeo & {
-              position: {
-                lat: number;
-                lng: number;
-              };
-            },
-        ) => {
-          return (
-            <PropertyMarker
-              key={`spider-marker-${spiderMarker.properties.Id} + ${spiderMarker.properties.PropertyTypeId}`}
-              property={spiderMarker}
-              position={[spiderMarker.position.lat, spiderMarker.position.lng]}
-            />
-          );
-        },
-      )}
-      {/**
-       * Render lines/legs from a spiderfied cluster click
-       */}
-      {spider.lines?.map(
-        (
-          line: {
-            coords: { lat: number; lng: number }[];
-            options: { weight: number; color: string; opacity: number };
-          },
-          index: number,
-        ) => {
-          return <Polyline key={index} positions={line.coords} {...line.options} />;
-        },
-      )}
     </>
   );
 };
