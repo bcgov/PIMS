d4cdd04ff0d0fc8c172209566f2f90f0845a5c9f
dbarkowsky <dylanbarkowsky@gmail.com>
2024-06-07 15:46:55 -0700

    move filter into sidebar

--- react-app/src/components/map/controls/FilterControl.tsx
+++ react-app/src/components/map/controls/FilterControl.tsx
@@ -6,9 +6,8 @@ import useGroupedAgenciesApi from '@/hooks/api/useGroupedAgenciesApi';
 import { MapFilter } from '@/hooks/api/usePropertiesApi';
 import useDataLoader from '@/hooks/useDataLoader';
 import usePimsApi from '@/hooks/usePimsApi';
-import { Close, FilterAlt } from '@mui/icons-material';
-import { Box, Paper, SxProps, Typography, useTheme, Grid, IconButton, Button } from '@mui/material';
-import React, { Dispatch, SetStateAction, useContext, useState } from 'react';
+import { Box, Typography, Grid, Button } from '@mui/material';
+import React, { Dispatch, SetStateAction, useContext } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
 interface FilterControlProps {
@@ -18,7 +17,7 @@ interface FilterControlProps {
 /**
  * FilterControl component renders a filter control interface for filtering inventory items.
  * It includes dropdowns for selecting agencies, administrative areas, classifications, and property types.
- * Users can input PID, PIN, Address, and Name for filtering purposes.
+ * Users can input PID, PIN, Address, etc. for filtering purposes.
  * The component allows users to clear the filter or apply the selected filter criteria.
  *
  * @param {FilterControlProps} props - Props for the FilterControl component.
@@ -26,9 +25,6 @@ interface FilterControlProps {
  */
 const FilterControl = (props: FilterControlProps) => {
   const { setFilter } = props;
-  const [open, setOpen] = useState<boolean>(false);
-  const [fading, setFading] = useState<boolean>(false); // Controls the fade in/out
-  const theme = useTheme();
   const api = usePimsApi();
   const user = useContext(AuthContext);
 
@@ -51,21 +47,6 @@ const FilterControl = (props: FilterControlProps) => {
   loadPropertyTypes();
   loadUsersAgencies();
 
-  // Define styles for control in different states
-  const closedStyle: SxProps = {
-    height: '25px',
-    width: '25px',
-    cursor: 'pointer',
-    '&:hover': {
-      backgroundColor: theme.palette.gray.main,
-    },
-  };
-  const openStyle: SxProps = {
-    height: 'auto',
-    width: '300px',
-  };
-  const style = open ? openStyle : closedStyle;
-
   // Initial form values
   const formMethods = useForm({
     defaultValues: {
@@ -82,141 +63,118 @@ const FilterControl = (props: FilterControlProps) => {
 
   return (
     <Box
-      component={Paper}
       sx={{
         padding: '1em',
-        transition: 'all 0.1s ease-in-out',
-        ...style,
-      }}
-      onClick={() => {
-        // Handles a delayed fade in if opening the filter
-        if (!open) {
-          setFading(true);
-          setOpen(true);
-          setTimeout(() => {
-            setFading(false);
-          }, 100);
-        }
       }}
     >
-      {open ? (
-        <Box
-          sx={{
-            opacity: fading ? 0 : 1,
-            transition: 'all 0.3s ease-in',
-          }}
-        >
-          <FormProvider {...formMethods}>
-            <Grid container gap={1}>
-              <Grid
-                item
-                xs={12}
-                display={'inline-flex'}
-                alignItems={'center'}
-                justifyContent={'space-between'}
-                mb={'1em'}
-              >
-                <Typography variant="h4">Inventory Filter</Typography>
-                <IconButton
-                  onClick={() => {
-                    setOpen(false);
-                  }}
-                >
-                  <Close />
-                </IconButton>
-              </Grid>
-              <TextFormField fullWidth name={'PID'} label={`PID`}></TextFormField>
-              <TextFormField fullWidth numeric name={'PIN'} label={`PIN`}></TextFormField>
-              <TextFormField fullWidth name={'Address'} label={`Address`}></TextFormField>
-              <TextFormField fullWidth name={'Name'} label={`Name`}></TextFormField>
-              <MultiselectFormField
-                name={'Agencies'}
-                label={'Agencies'}
-                // Only return options that should be visible based on user's agency/role
-                options={agencyOptions.filter(
-                  (option) =>
-                    user.keycloak.hasRoles([Roles.ADMIN, Roles.AUDITOR], {
-                      requireAllRoles: false,
-                    }) || usersAgencies.includes(option.value),
-                )}
-                allowNestedIndent
-              />
-              <MultiselectFormField
-                name={'AdministrativeAreas'}
-                label="Administrative Areas"
-                options={adminAreasData
-                  .filter((aa) => !aa.IsDisabled)
-                  .map((aa) => ({
-                    label: aa.Name,
-                    value: aa.Id,
-                  }))}
-              />
-              <MultiselectFormField
-                name={'Classifications'}
-                label="Classifications"
-                options={classificationsData.map((c) => ({
-                  label: c.Name,
-                  value: c.Id,
-                }))}
-              />
-              <MultiselectFormField
-                name={'PropertyTypes'}
-                label="Property Types"
-                options={propertyTypesData
-                  .filter((pt) => !pt.IsDisabled)
-                  .map((pt) => ({
-                    label: pt.Name,
-                    value: pt.Id,
-                  }))}
-              />
-              <Grid item xs={12} justifyContent={'space-between'} display={'inline-flex'} gap={1}>
-                <Button
-                  variant="outlined"
-                  fullWidth
-                  onClick={() => {
-                    setFilter({});
-                    formMethods.reset();
-                  }}
-                >
-                  Clear
-                </Button>
-                <Button
-                  variant="contained"
-                  fullWidth
-                  onClick={() => {
-                    const formValues = formMethods.getValues();
-                    const newFilter: MapFilter = {
-                      PID: !isNaN(parseInt(formValues.PID.replace(/-/g, '').trim()))
-                        ? parseInt(formValues.PID.replace(/-/g, '').trim())
-                        : undefined,
-                      PIN: !isNaN(parseInt(formValues.PIN)) ? parseInt(formValues.PIN) : undefined,
-                      Name:
-                        formValues.Name && formValues.Name.trim().length
-                          ? formValues.Name.trim()
-                          : undefined,
-                      Address:
-                        formValues.Address && formValues.Address.trim().length
-                          ? formValues.Address.trim()
-                          : undefined,
-                      AgencyIds: formValues.Agencies.map((option) => option.value),
-                      ClassificationIds: formValues.Classifications.map((option) => option.value),
-                      PropertyTypeIds: formValues.PropertyTypes.map((option) => option.value),
-                      AdministrativeAreaIds: formValues.AdministrativeAreas.map(
-                        (option) => option.value,
-                      ),
-                    };
-                    setFilter(newFilter);
-                  }}
-                >
-                  Filter
-                </Button>
-              </Grid>
-            </Grid>
-          </FormProvider>
-        </Box>
-      ) : (
-        <FilterAlt />
-      )}
+      <FormProvider {...formMethods}>
+        <Grid container gap={1}>
+          <Grid
+            item
+            xs={12}
+            display={'inline-flex'}
+            alignItems={'center'}
+            justifyContent={'space-between'}
+            mb={'1em'}
+          >
+            <Typography variant="h4">Inventory Filter</Typography>
+          </Grid>
+          <TextFormField fullWidth name={'PID'} label={`PID`}></TextFormField>
+          <TextFormField fullWidth numeric name={'PIN'} label={`PIN`}></TextFormField>
+          <TextFormField fullWidth name={'Address'} label={`Address`}></TextFormField>
+          <TextFormField fullWidth name={'Name'} label={`Name`}></TextFormField>
+          <MultiselectFormField
+            name={'Agencies'}
+            label={'Agencies'}
+            // Only return options that should be visible based on user's agency/role
+            options={
+              agencyOptions?.filter(
+                (option) =>
+                  user.keycloak.hasRoles([Roles.ADMIN, Roles.AUDITOR], {
+                    requireAllRoles: false,
+                  }) || usersAgencies.includes(option.value),
+              ) ?? []
+            }
+            allowNestedIndent
+          />
+          <MultiselectFormField
+            name={'AdministrativeAreas'}
+            label="Administrative Areas"
+            options={
+              adminAreasData
+                ?.filter((aa) => !aa.IsDisabled)
+                .map((aa) => ({
+                  label: aa.Name,
+                  value: aa.Id,
+                })) ?? []
+            }
+          />
+          <MultiselectFormField
+            name={'Classifications'}
+            label="Classifications"
+            options={
+              classificationsData?.map((c) => ({
+                label: c.Name,
+                value: c.Id,
+              })) ?? []
+            }
+          />
+          <MultiselectFormField
+            name={'PropertyTypes'}
+            label="Property Types"
+            options={
+              propertyTypesData
+                ?.filter((pt) => !pt.IsDisabled)
+                .map((pt) => ({
+                  label: pt.Name,
+                  value: pt.Id,
+                })) ?? []
+            }
+          />
+          <Grid item xs={12} justifyContent={'space-between'} display={'inline-flex'} gap={1}>
+            <Button
+              variant="outlined"
+              fullWidth
+              onClick={() => {
+                setFilter({});
+                formMethods.reset();
+              }}
+            >
+              Clear
+            </Button>
+            <Button
+              variant="contained"
+              fullWidth
+              onClick={() => {
+                const formValues = formMethods.getValues();
+                const newFilter: MapFilter = {
+                  PID: !isNaN(parseInt(formValues.PID.replace(/-/g, '').trim()))
+                    ? parseInt(formValues.PID.replace(/-/g, '').trim())
+                    : undefined,
+                  PIN: !isNaN(parseInt(formValues.PIN)) ? parseInt(formValues.PIN) : undefined,
+                  Name:
+                    formValues.Name && formValues.Name.trim().length
+                      ? formValues.Name.trim()
+                      : undefined,
+                  Address:
+                    formValues.Address && formValues.Address.trim().length
+                      ? formValues.Address.trim()
+                      : undefined,
+                  AgencyIds: formValues.Agencies.map((option) => option.value),
+                  ClassificationIds: formValues.Classifications.map((option) => option.value),
+                  PropertyTypeIds: formValues.PropertyTypes.map((option) => option.value),
+                  AdministrativeAreaIds: formValues.AdministrativeAreas.map(
+                    (option) => option.value,
+                  ),
+                };
+                setFilter(newFilter);
+              }}
+            >
+              Filter
+            </Button>
+          </Grid>
+        </Grid>
+      </FormProvider>
     </Box>
   );
 };
