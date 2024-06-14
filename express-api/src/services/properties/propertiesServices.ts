cb3ee36d04ae5c8ce1589506349ebb467709a46a
Graham Stewart <graham.stewart@quartech.com>
2024-06-14 16:15:52 -0700

    bad import

--- express-api/src/services/properties/propertiesServices.ts
+++ express-api/src/services/properties/propertiesServices.ts
@@ -2,7 +2,6 @@ import { AppDataSource } from '@/appDataSource';
 import { Building } from '@/typeorm/Entities/Building';
 import { Parcel } from '@/typeorm/Entities/Parcel';
 import { MapProperties } from '@/typeorm/Entities/views/MapPropertiesView';
-import { PropertyUnion } from '@/typeorm/Entities/views/PropertyUnionView';
 import { ILike, In } from 'typeorm';
 
 const propertiesFuzzySearch = async (keyword: string, limit?: number) => {
