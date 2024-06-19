bbc79414bd7a2b54c9e39174f03d642b18666b80
dbarkowsky <dylanbarkowsky@gmail.com>
2024-06-19 08:49:54 -0700

    update DevZone

--- react-app/src/pages/DevZone.tsx
+++ react-app/src/pages/DevZone.tsx
@@ -1,14 +1,11 @@
 /* eslint-disable no-console */
 //Simple component testing area.
-import React from 'react';
-import usePimsApi from '@/hooks/usePimsApi';
-import useDataLoader from '@/hooks/useDataLoader';
+import { LookupContext } from '@/contexts/lookupContext';
+import React, { useContext } from 'react';
 
 const Dev = () => {
-  const api = usePimsApi();
-  const { data, loadOnce } = useDataLoader(api.lookup.getAll);
-  loadOnce();
-  console.log(data);
+  const lookup = useContext(LookupContext);
+  console.log(lookup);
   return <></>;
 };
 
