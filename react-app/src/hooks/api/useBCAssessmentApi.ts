2c468819cf66a8b981583802a52332f238a69082
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-06-25 10:28:25 -0700

    Force BCA Test URL (#2477)
    Co-authored-by: Graham Stewart <graham.stewart@quartech.com>

--- react-app/src/hooks/api/useBCAssessmentApi.ts
+++ react-app/src/hooks/api/useBCAssessmentApi.ts
@@ -1,18 +1,15 @@
-import { ConfigContext } from '@/contexts/configContext';
+// import { ConfigContext } from '@/contexts/configContext';
 import { FeatureCollection } from 'geojson';
-import { useContext } from 'react';
+// import { useContext } from 'react';
 
 const useBCAssessmentApi = () => {
-  const config = useContext(ConfigContext);
-  const isProd = config?.NODE_ENV === 'production';
-  const prodURL =
-    'https://apps.gov.bc.ca/ext/sgw/geo.bca?REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&typeName=geo.bca:WHSE_HUMAN_CULTURAL_ECONOMIC.BCA_FOLIO_GNRL_PROP_VALUES_SV&outputFormat=application/json';
+  // const prodURL =
+  //   'https://apps.gov.bc.ca/ext/sgw/geo.bca?REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&typeName=geo.bca:WHSE_HUMAN_CULTURAL_ECONOMIC.BCA_FOLIO_GNRL_PROP_VALUES_SV&outputFormat=application/json';
   const testURL =
     'https://test.apps.gov.bc.ca/ext/sgw/geo.bca?REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&typeName=geo.bca:WHSE_HUMAN_CULTURAL_ECONOMIC.BCA_FOLIO_GNRL_PROP_VALUES_SV&outputFormat=application/json';
-
   //  https://test.apps.gov.bc.ca/ext/sgw/geo.bca?REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&typeName=geo.bca:WHSE_HUMAN_CULTURAL_ECONOMIC.BCA_FOLIO_GNRL_PROP_VALUES_SV&outputFormat=application/json&srsName=EPSG:4326&CQL_FILTER=CONTAINS(SHAPE,SRID=4326;POINT(-123.36905121803285 48.41397415311252))
   const getBCAssessmentByLocation = async (lng: string, lat: string) => {
-    const finalUrl = `${isProd ? prodURL : testURL}&srsName=EPSG:4326&CQL_FILTER=CONTAINS(SHAPE,SRID=4326;POINT(${lng} ${lat}))`;
+    const finalUrl = `${testURL}&srsName=EPSG:4326&CQL_FILTER=CONTAINS(SHAPE,SRID=4326;POINT(${lng} ${lat}))`;
     // const { parsedBody } = await absoluteFetch.get(
     //   finalUrl,
     //   {},
