316d7a220544c6a7e0db16bc145880cdc2079a87
TaylorFries <78506153+TaylorFries@users.noreply.github.com>
2024-07-02 14:58:18 -0700

    Update github-api-requests.js octokit import method (#2491)

---
+++ .github/helpers/npm-deps/create-report-issues.cjs
@@ -0,0 +1,25 @@
+const path = require('path');
+const outputText = require(path.resolve(__dirname, `../../../outputText.json`));
+
+/**
+ * THIS FILE DOES NOT REQUIRE ANY EDITING.
+ * Place within .github/helpers/npm-deps/
+ */
+
+// Get package.json paths from env.
+const packageJsonPaths = JSON.parse(process.env.packageJsonPaths);
+
+(async () => {
+  const module = await import('../github-api/create-and-close-existing-issue.mjs');
+  const createAndCloseExistingIssue = module.default;
+  // Create an array of promises for each packageJsonPath.
+  const promises = packageJsonPaths.map(async (packagePath) => {
+    const issueTitle =
+      packagePath !== '.' ? `${packagePath} NPM Dependency Report` : 'NPM Dependency Report';
+    // Await the completion of create and close existing issue.
+    await createAndCloseExistingIssue(issueTitle, outputText[packagePath]);
+  });
+
+  // Wait for all issues to be created.
+  await Promise.all(promises);
+})();
