c432d7033b132fc8d3bacbecc7adc3db7beb0b13
taylorfries <taylor.friesen@gov.bc.ca>
2024-07-19 11:51:18 -0700

    project cleanup

--- express-api/src/routes/projectsRouter.ts
+++ express-api/src/routes/projectsRouter.ts
@@ -13,19 +13,6 @@ const {
   updateDisposalProject,
   deleteDisposalProject,
   addDisposalProject,
-  requestProjectStatusChange,
-  getAllProjectReports,
-  getProjectReport,
-  updateProjectReport,
-  deleteProjectReport,
-  addProjectReport,
-  getProjectReportSnapshots,
-  generateProjectReportSnapshots,
-  refreshProjectSnapshots,
-  getAllProjectStatus,
-  getProjectStatusTasks,
-  getProjectWorkflowStatuses,
-  getProjectWorkflowTasks,
   getProjects,
 } = controllers;
 
@@ -38,38 +25,18 @@ router
 router.route(`${PROJECT_DISPOSAL}`).post(activeUserCheck, catchErrors(addDisposalProject));
 
 //This originally had like 3 different routes that all seem to do the same thing so let's just consolidate that to one.
-router
-  .route(`${PROJECT_DISPOSAL}/workflows`)
-  .put(activeUserCheck, catchErrors(requestProjectStatusChange));
+router.route(`${PROJECT_DISPOSAL}/workflows`)
 
-router.route(`${PROJECT_REPORTS}`).get(activeUserCheck, catchErrors(getAllProjectReports));
-router
-  .route(`${PROJECT_REPORTS}/:reportId`)
-  .get(activeUserCheck, catchErrors(getProjectReport))
-  .put(activeUserCheck, catchErrors(updateProjectReport))
-  .delete(activeUserCheck, catchErrors(deleteProjectReport));
-router.route(`${PROJECT_REPORTS}`).post(activeUserCheck, catchErrors(addProjectReport));
+router.route(`${PROJECT_REPORTS}/:reportId`);
 
-router
-  .route(`${PROJECT_REPORTS}/snapshots/:reportId`)
-  .get(activeUserCheck, catchErrors(getProjectReportSnapshots))
-  .post(activeUserCheck, catchErrors(generateProjectReportSnapshots));
+router.route(`${PROJECT_REPORTS}/snapshots/:reportId`);
 
-router
-  .route(`${PROJECT_REPORTS}/refresh/:reportId`)
-  .get(activeUserCheck, catchErrors(refreshProjectSnapshots));
+router.route(`${PROJECT_REPORTS}/refresh/:reportId`);
 
 //Omitting search endpoints.
-router.route('/').get(activeUserCheck, catchErrors(getProjects));
-router.route(`/projects/status`).get(activeUserCheck, catchErrors(getAllProjectStatus));
-router
-  .route(`/projects/status/:statusCode/tasks`)
-  .get(activeUserCheck, catchErrors(getProjectStatusTasks));
-router
-  .route(`/projects/workflows/:workflowCode/status`)
-  .get(activeUserCheck, catchErrors(getProjectWorkflowStatuses));
-router
-  .route(`/projects/workflows/:workflowCode/tasks`)
-  .get(activeUserCheck, catchErrors(getProjectWorkflowTasks));
+router.route('/').get(activeUserCheck, catchErrors(getProjects));;
+router.route(`/projects/status/:statusCode/tasks`);
+router.route(`/projects/workflows/:workflowCode/status`);
+router.route(`/projects/workflows/:workflowCode/tasks`);
 
 export default router;
