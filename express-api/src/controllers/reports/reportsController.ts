a621edc1a69496db3ba7cc2892a80c5185100d94
taylorfries <taylor.friesen@gov.bc.ca>
2024-07-19 13:23:04 -0700

    report cleanup

--- express-api/src/controllers/reports/reportsController.ts
+++ express-api/src/controllers/reports/reportsController.ts
@@ -1,58 +1,6 @@
 import logger from '@/utilities/winstonLogger';
-import { stubResponse } from '../../utilities/stubResponse';
 import { Request, Response } from 'express';
 import { ErrorReport, errorReportSchema } from '@/controllers/reports/errorReportSchema';
-
-/**
- * @description Get all reports as a CSV or Excel file.
- * @param {Request}     req Incoming request.
- * @param {Response}    res Outgoing response.
- * @returns {Response}      A 200 status with the requested reports.
- */
-export const getSpreadsheetProjectsReports = async (req: Request, res: Response) => {
-  /**
-   * #swagger.tags = ['Projects']
-   * #swagger.description = 'Exports projects as CSV or Excel file. Include 'Accept' header to request the appropriate expor - ["text/csv", "application/application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]'
-   * #swagger.security = [{
-   *   "bearerAuth" : []
-   * }]
-   */
-  return stubResponse(res);
-};
-
-/**
- * @description Get all reports as a CSV or Excel file.
- * @param {Request}     req Incoming request.
- * @param {Response}    res Outgoing response.
- * @returns {Response}      A 200 status with the requested reports.
- */
-export const getSpreadsheetPropertiesReports = async (req: Request, res: Response) => {
-  /**
-   * #swagger.tags = ['Projects']
-   * #swagger.description = 'Exports projects as CSV or Excel file. Include 'Accept' header to request the appropriate expor - ["text/csv", "application/application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]'
-   * #swagger.security = [{
-   *   "bearerAuth" : []
-   * }]
-   */
-  return stubResponse(res);
-};
-
-/**
- * @description Get all reports as a CSV or Excel file.
- * @param {Request}     req Incoming request.
- * @param {Response}    res Outgoing response.
- * @returns {Response}      A 200 status with the requested reports.
- */
-export const getSpreadsheetUsersReports = async (req: Request, res: Response) => {
-  /**
-   * #swagger.tags = ['Projects']
-   * #swagger.description = 'Exports projects as CSV or Excel file. Include 'Accept' header to request the appropriate expor - ["text/csv", "application/application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]'
-   * #swagger.security = [{
-   *   "bearerAuth" : []
-   * }]
-   */
-  return stubResponse(res);
-};
 
 export const submitErrorReport = async (req: Request, res: Response) => {
   /**
--- express-api/tests/unit/controllers/reports/reportsController.test.ts
+++ express-api/tests/unit/controllers/reports/reportsController.test.ts
@@ -1,11 +1,6 @@
 import { Request, Response } from 'express';
 import { MockReq, MockRes, getRequestHandlerMocks } from '../../../testUtils/factories';
-import {
-  getSpreadsheetProjectsReports,
-  getSpreadsheetPropertiesReports,
-  getSpreadsheetUsersReports,
-  submitErrorReport,
-} from '@/controllers/reports/reportsController';
+import { submitErrorReport } from '@/controllers/reports/reportsController';
 import { ErrorReport } from '@/controllers/reports/errorReportSchema';
 import { Roles } from '@/constants/roles';
 import logger from '@/utilities/winstonLogger';
@@ -17,78 +12,6 @@ describe('UNIT - Reports', () => {
     const { mockReq, mockRes } = getRequestHandlerMocks();
     mockRequest = mockReq;
     mockResponse = mockRes;
-  });
-
-  describe('GET /reports/projects', () => {
-    it('should return stub response 501', async () => {
-      await getSpreadsheetProjectsReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(501);
-    });
-
-    xit('should return 200 with a csv file', async () => {
-      mockRequest.headers.accept = 'text/csv';
-      await getSpreadsheetProjectsReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(200);
-    });
-
-    xit('should return 200 with an excel file', async () => {
-      mockRequest.headers.accept = 'application/application/vnd.ms-excel';
-      await getSpreadsheetProjectsReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(200);
-
-      mockRequest.headers.accept =
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-      await getSpreadsheetProjectsReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(200);
-    });
-  });
-
-  describe('GET /reports/spreadsheets/properties', () => {
-    it('should return stub response 501', async () => {
-      await getSpreadsheetPropertiesReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(501);
-    });
-
-    xit('should return 200 with a csv file', async () => {
-      mockRequest.headers.accept = 'text/csv';
-      await getSpreadsheetPropertiesReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(200);
-    });
-
-    xit('should return 200 with an excel file', async () => {
-      mockRequest.headers.accept = 'application/vnd.ms-excel';
-      await getSpreadsheetPropertiesReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(200);
-
-      mockRequest.headers.accept =
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-      await getSpreadsheetPropertiesReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(200);
-    });
-  });
-
-  describe('GET /reports/user', () => {
-    it('should return stub response 501', async () => {
-      await getSpreadsheetUsersReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(501);
-    });
-
-    xit('should return 200 with a csv file', async () => {
-      mockRequest.headers.accept = 'text/csv';
-      await getSpreadsheetUsersReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(200);
-    });
-
-    xit('should return 200 with an excel file', async () => {
-      mockRequest.headers.accept = 'application/vnd.ms-excel';
-      await getSpreadsheetUsersReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(200);
-
-      mockRequest.headers.accept =
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-      await getSpreadsheetUsersReports(mockRequest, mockResponse);
-      expect(mockResponse.statusValue).toBe(200);
-    });
   });
 
   // TODO: Add additional test cases when this endpoint starts using CHES
