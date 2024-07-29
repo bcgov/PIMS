b4a04ef6b88bfb539fcfef616b546857620327e7
Graham Stewart <graham.stewart@quartech.com>
2024-07-29 09:18:19 -0700

    Agency made visible on main data card of project detail

--- react-app/src/components/projects/ProjectDetail.tsx
+++ react-app/src/components/projects/ProjectDetail.tsx
@@ -181,6 +181,7 @@ const ProjectDetail = (props: IProjectDetail) => {
     Name: data?.parsedBody.Name,
     AssignTier: getLookupValueById('ProjectTiers', data?.parsedBody.TierLevelId),
     Description: data?.parsedBody.Description,
+    Agency: getLookupValueById('Agencies', data?.parsedBody.AgencyId),
   };
 
   const FinancialInformationData = useMemo(() => {
@@ -206,6 +207,8 @@ const ProjectDetail = (props: IProjectDetail) => {
         return <Typography>{(val as ProjectStatus)?.Name}</Typography>;
       case 'AssignTier':
         return <Typography>{(val as TierLevel)?.Name}</Typography>;
+      case 'Agency':
+        return <Typography>{(val as Agency)?.Name}</Typography>;
       default:
         return <Typography>{val}</Typography>;
     }
