dfca9425e57b72bb00cbcf2863230e0b70bc1644
dbarkowsky <dylanbarkowsky@gmail.com>
2024-08-02 09:26:57 -0700

    add todo for future feature

--- react-app/src/components/property/PropertyForms.tsx
+++ react-app/src/components/property/PropertyForms.tsx
@@ -241,7 +241,7 @@ export const GeneralInformationForm = (props: IGeneralInformationForm) => {
             name={'AdministrativeAreaId'}
             label={'Administrative area'}
             options={adminAreas ?? []}
-            noOptionsText={`No matches. Request an administrative area at ${lookup.data.Config.contactEmail.split('@').join(' @')}`}
+            noOptionsText={`No matches. Request an administrative area at ${lookup.data.Config.contactEmail.split('@').join(' @')}`} // TODO: Replace this with a dialog
           />
         </Grid>
         <Grid item xs={6}>
