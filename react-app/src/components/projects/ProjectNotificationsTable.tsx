17e50ebfa8281696bfeecf156e5dd5a1b52b7412
dbarkowsky <dylanbarkowsky@gmail.com>
2024-08-07 11:00:07 -0700

    add dates to data card

--- react-app/src/components/projects/ProjectNotificationsTable.tsx
+++ react-app/src/components/projects/ProjectNotificationsTable.tsx
@@ -1,6 +1,9 @@
 import { dateFormatter } from '@/utilities/formatters';
 import { Box, Typography } from '@mui/material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { DateField, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
 import React, { useState } from 'react';
 
 export interface INotificationModel {
@@ -50,6 +53,7 @@ const ProjectNotificationsTable = (props: ProjectNotificationsTableProps) => {
       field: 'to',
       headerName: 'To',
       flex: 1,
+      maxWidth: 300,
     },
     {
       field: 'agency',
@@ -77,12 +81,62 @@ const ProjectNotificationsTable = (props: ProjectNotificationsTableProps) => {
 
   if (!props.rows) return <></>;
 
+  // Prepare values for Enhanced Referral Notification fields
+  const initalERN = props.rows.find(
+    (row) => row.subject === 'ACTION REQUIRED - Notification of Surplus Real Property',
+  );
+  const thirtyDayERN = props.rows.find(
+    (row) =>
+      row.subject ===
+      'Notification of Surplus Real Property - 30 Day Reminder Notification of Surplus Real Property',
+  );
+  const sixtyDayERN = props.rows.find(
+    (row) =>
+      row.subject ===
+      'Notification of Surplus Real Property - 60 Day Reminder Notification of Surplus Real Property',
+  );
+  const nintyDayERN = props.rows.find(
+    (row) =>
+      row.subject ===
+      'Notification of Surplus Real Property - Completion of 90 Day Enhanced Referral Period for Notification of Surplus Real Property',
+  );
+
   return !props.rows ? (
     <Box display={'flex'} justifyContent={'center'}>
       <Typography>No notifications were sent.</Typography>
     </Box>
   ) : (
     <>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Typography variant="h6">Enhanced Referral Notification Dates</Typography>
+        <Box gap={1} display={'inline-flex'} mb={3} mt={2}>
+          <DateField
+            disabled={true}
+            value={initalERN ? dayjs(initalERN.sendOn) : undefined}
+            label={'Initial Send Date'}
+            format={'LL'}
+          />
+          <DateField
+            disabled={true}
+            value={thirtyDayERN ? dayjs(thirtyDayERN.sendOn) : undefined}
+            label={'30-day Send Date'}
+            format={'LL'}
+          />
+          <DateField
+            disabled={true}
+            value={sixtyDayERN ? dayjs(sixtyDayERN.sendOn) : undefined}
+            label={'60-day Send Date'}
+            format={'LL'}
+          />
+          <DateField
+            disabled={true}
+            value={nintyDayERN ? dayjs(nintyDayERN.sendOn) : undefined}
+            label={'90-day Send Date'}
+            format={'LL'}
+          />
+        </Box>
+      </LocalizationProvider>
+
       <Typography variant="h6">Total Notifications: {props.rows.length}</Typography>
       <Box marginBottom={2} />
       <DataGrid
