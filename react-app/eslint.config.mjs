24adfad8b21b329bb48e95f291b7925115dd9038
Sharala-Perumal <Sharala.Perumal@gov.bc.ca>
2024-08-28 16:32:11 -0700

    updated eslint and changed the config file

---
+++ react-app/eslint.config.mjs
@@ -0,0 +1,70 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactPlugin from 'eslint-plugin-react';
+
+export default [
+  {
+    languageOptions: {
+      parser: typescriptParser,
+    },
+    /*  globals: {
+    module: 'readonly',
+  },
+  env: {
+    node: true,
+  },
+  extends: [
+    // By extending from a plugin config, we can get recommended rules without having to add them manually.
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
+    // Make sure it's always the last config, so it gets the chance to override other configs.
+    'eslint-config-prettier',
+  ],*/
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier: prettierPlugin,
+      react: reactPlugin,
+    },
+    settings: {
+      react: {
+        // Tells eslint-plugin-react to automatically detect the version of React to use.
+        version: 'detect',
+      },
+      // Tells eslint how to resolve imports.
+      'import/resolver': {
+        node: {
+          paths: ['src'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+    rules: {
+      // Override ones from the extended configs.
+      'prettier/prettier': 'warn',
+      'no-extra-boolean-cast': 'off',
+      'no-unsafe-optional-chaining': 'off',
+      'no-prototype-builtins': 'off',
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+      'no-console': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off', // ie. const val: number = 4;
+      '@typescript-eslint/no-empty-function': 'off', // ie. {}
+      '@typescript-eslint/no-non-null-assertion': 'off', // Allow use of non-null assertion operator (!).
+      '@typescript-eslint/no-explicit-any': 'off', // Warn if 'any' type is used.
+      '@typescript-eslint/no-duplicate-enum-values': 'error',
+      '@typescript-eslint/no-restricted-types': 'error',
+    },
+
+    // Override settings for specific file types
+    files: ['**/*.ts', '**/*.js', '**/*.tsx', '**/*.jsx'],
+  },
+  {
+    // Ignore specific files and directories
+    ignores: ['node_modules/', 'package-lock.json', 'dist/', '/coverage'],
+  },
+];
