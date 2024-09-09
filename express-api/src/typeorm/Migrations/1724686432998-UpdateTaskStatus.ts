7c66e9fde0f9cc0de90ac6382edb17933230ff3e
dbarkowsky <dylanbarkowsky@gmail.com>
2024-09-09 13:23:07 -0700

    make contract conditional task optional

--- express-api/src/typeorm/Migrations/1724686432998-UpdateTaskStatus.ts
+++ express-api/src/typeorm/Migrations/1724686432998-UpdateTaskStatus.ts
@@ -3,31 +3,37 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class UpdateTaskStatus1724686432998 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-          UPDATE task SET status_id = 14 WHERE id = 3;
-        `);
+      UPDATE task SET status_id = 14 WHERE id = 3;
+    `);
     await queryRunner.query(`
-          UPDATE task SET status_id = 15 WHERE id = 4;
-        `);
+      UPDATE task SET status_id = 15 WHERE id = 4;
+    `);
     await queryRunner.query(`
-          UPDATE timestamp_type SET status_id = 21, is_optional = false WHERE id = 11;
-        `);
+      UPDATE task SET is_optional = true WHERE id = 24;
+    `);
     await queryRunner.query(`
-          UPDATE task SET name = 'Preparation and Due Diligence complete', is_optional = false WHERE id IN (9, 19);
-        `);
+      UPDATE timestamp_type SET status_id = 21, is_optional = false WHERE id = 11;
+    `);
+    await queryRunner.query(`
+      UPDATE task SET name = 'Preparation and Due Diligence complete', is_optional = false WHERE id IN (9, 19);
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-          UPDATE task SET status_id = 7 WHERE id = 3;
-        `);
+      UPDATE task SET status_id = 7 WHERE id = 3;
+    `);
     await queryRunner.query(`
-          UPDATE task SET status_id = 8 WHERE id = 4;
-        `);
+      UPDATE task SET status_id = 8 WHERE id = 4;
+    `);
     await queryRunner.query(`
-          UPDATE timestamp_type SET status_id = 14, is_optional = true WHERE id = 11;
-        `);
+      UPDATE task SET is_optional = false WHERE id = 24;
+    `);
     await queryRunner.query(`
-          UPDATE task SET name = 'Preparation and due diligence', is_optional = true WHERE id IN (9, 19);
-        `);
+      UPDATE timestamp_type SET status_id = 14, is_optional = true WHERE id = 11;
+    `);
+    await queryRunner.query(`
+      UPDATE task SET name = 'Preparation and due diligence', is_optional = true WHERE id IN (9, 19);
+    `);
   }
 }
