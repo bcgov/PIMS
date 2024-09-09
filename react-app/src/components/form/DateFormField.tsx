132f2b74a0d0ec793c5281063dd670aa5669aba4
LawrenceLau2020 <68400651+LawrenceLau2020@users.noreply.github.com>
2024-09-09 13:42:15 -0700

    PIMS-2000: Make non-optional checkboxes mandatory (#2640)
    Co-authored-by: dbarkowsky <dylanbarkowsky@gmail.com>
    Co-authored-by: Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>

--- react-app/src/components/form/DateFormField.tsx
+++ react-app/src/components/form/DateFormField.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 import { DateField, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { Box, Typography, useTheme } from '@mui/material';
 
 type DateFieldFormProps = {
   name: string;
   label: string;
   disabled?: boolean;
+  required?: boolean;
 };
 
 const DateFormField = (props: DateFieldFormProps) => {
   const { control } = useFormContext();
-  const { name, label, disabled } = props;
+  const theme = useTheme();
+  const { name, label, disabled, required } = props;
   return (
     <Controller
       control={control}
       name={name}
-      render={({ field: { onChange, value } }) => {
+      rules={{ validate: (value) => !required || value || 'Required field.' }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => {
         return (
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DateField
@@ -26,7 +30,18 @@ const DateFormField = (props: DateFieldFormProps) => {
               value={value}
               label={label}
               format={'LL'}
+              required={required}
+              slotProps={{ textField: { error: !!error && !!error.message } }}
             />
+            {!!error && !!error.message ? (
+              <Box>
+                <Typography ml={'3.2em'} fontSize={'smaller'} color={theme.palette.error.main}>
+                  {error.message}
+                </Typography>
+              </Box>
+            ) : (
+              <></>
+            )}
           </LocalizationProvider>
         );
       }}
