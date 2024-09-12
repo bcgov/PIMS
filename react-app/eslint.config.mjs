a75a88e1b1c17f0cff076a97b65abb09dead867f
dbarkowsky <dylanbarkowsky@gmail.com>
2024-09-12 09:26:10 -0700

    clean old comments from eslint

--- react-app/eslint.config.mjs
+++ react-app/eslint.config.mjs
@@ -11,21 +11,6 @@ export default tseslint.config(
     languageOptions: {
       parser: tseslint.parser,
     },
-    /*  globals: {
-    module: 'readonly',
-  },
-  env: {
-    node: true,
-  },
-  extends: [
-    // By extending from a plugin config, we can get recommended rules without having to add them manually.
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
-    // Make sure it's always the last config, so it gets the chance to override other configs.
-    'eslint-config-prettier',
-  ],*/
     plugins: {
       'typescript-eslint': tseslint,
       prettier: prettierPlugin,
