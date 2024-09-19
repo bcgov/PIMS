25ce76c8a3db17af4dad2cace0a285c629585594
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-09-19 14:23:53 -0700

    PIMS-2083 Remove Keycloak Roles Code (#2676)
    Co-authored-by: LawrenceLau2020 <68400651+LawrenceLau2020@users.noreply.github.com>

--- express-api/src/middleware/keycloak/keycloakOptions.ts
+++ express-api/src/middleware/keycloak/keycloakOptions.ts
@@ -1,6 +1,5 @@
 import { SSOOptions, SSOUser } from '@bcgov/citz-imb-sso-express';
 import logger from '@/utilities/winstonLogger';
-import KeycloakService from '@/services/keycloak/keycloakService';
 import { AppDataSource } from '@/appDataSource';
 import { User } from '@/typeorm/Entities/User';
 
@@ -16,14 +15,6 @@ export const SSO_OPTIONS: SSOOptions = {
       // Update last login date
       if (await users.exists({ where: { Username: user.preferred_username } })) {
         await users.update({ Username: user.preferred_username }, { LastLogin: new Date() });
-      }
-      // Try to sync the user's roles from Keycloak
-      try {
-        await KeycloakService.syncKeycloakUser(user.preferred_username);
-      } catch (e) {
-        logger.warn(
-          `Could not sync roles for user ${user.preferred_username}. Error: ${(e as Error).message}`,
-        );
       }
     }
   },
