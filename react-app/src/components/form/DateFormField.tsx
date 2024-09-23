5b0ad48f50babc5437d79c18ba28bce8064307c4
LawrenceLau2020 <68400651+LawrenceLau2020@users.noreply.github.com>
2024-09-23 11:45:47 -0700

    Update react-app/src/components/form/DateFormField.tsx
    Co-authored-by: Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>

--- react-app/src/components/form/DateFormField.tsx
+++ react-app/src/components/form/DateFormField.tsx
@@ -41,7 +41,7 @@ const DateFormField = (props: DateFieldFormProps) => {
                   InputLabelProps: {
                     style: { fontSize: '14px' }, // Font size for label
                   },
-                  required: required ? true : undefined,
+                  required,
                 },
               }}
             />
