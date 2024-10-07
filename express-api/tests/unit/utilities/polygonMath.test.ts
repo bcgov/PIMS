8540829090df4e392e32b5ddc1609a60fdc36d40
dbarkowsky <dylanbarkowsky@gmail.com>
2024-10-07 12:03:47 -0700

    more polygon math tests

--- express-api/tests/unit/utilities/polygonMath.test.ts
+++ express-api/tests/unit/utilities/polygonMath.test.ts
@@ -1,4 +1,4 @@
-import { isPointInPolygon } from '@/utilities/polygonMath';
+import { isPointInMultiPolygon, isPointInPolygon } from '@/utilities/polygonMath';
 
 describe('isPointInPolygon', () => {
   const polygon = [
@@ -17,5 +17,41 @@ describe('isPointInPolygon', () => {
   it('should return false if the point is outside the polygon', () => {
     const point = { x: -125.135, y: 48.8274 };
     expect(isPointInPolygon(point, polygon)).toEqual(false);
+  });
+});
+
+describe('isPointInMultiPolygon', () => {
+  const multiPolygon = [
+    [
+      { x: 0, y: 0 },
+      { x: 0, y: 3 },
+      { x: 3, y: 3 },
+      { x: 3, y: 0 },
+    ],
+    [
+      { x: 5, y: 0 },
+      { x: 5, y: 3 },
+      { x: 8, y: 3 },
+      { x: 8, y: 0 },
+    ],
+  ];
+  it('should return true when in the left polygon', () => {
+    const point = { x: 1, y: 1 };
+    expect(isPointInMultiPolygon(point, multiPolygon)).toEqual(true);
+  });
+
+  it('should return true when in the right polygon', () => {
+    const point = { x: 6, y: 1 };
+    expect(isPointInMultiPolygon(point, multiPolygon)).toEqual(true);
+  });
+
+  it('should return false when outside of both polygons', () => {
+    const point = { x: -1, y: 24 };
+    expect(isPointInMultiPolygon(point, multiPolygon)).toEqual(false);
+  });
+
+  it('should return false when between both polygons', () => {
+    const point = { x: 4, y: 1 };
+    expect(isPointInMultiPolygon(point, multiPolygon)).toEqual(false);
   });
 });
