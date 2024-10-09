214c732ed44fbb638ff360dd4cbb8f91a78a30c4
dbarkowsky <dylanbarkowsky@gmail.com>
2024-10-09 11:29:03 -0700

    fix env base_url

--- e2e/env.ts
+++ e2e/env.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-export const BASE_URL = process.env.URL ?? 'localhost:3000';
+export const BASE_URL = process.env.BASE_URL ?? 'localhost:3000';
 export const BCSC_USERNAME = process.env.BCSC_USERNAME ?? '';
 export const BCSC_PASSWORD = process.env.BCSC_PASSWORD ?? '';
 export const BCEID_USERNAME = process.env.BCEID_USERNAME ?? '';
