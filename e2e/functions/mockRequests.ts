765eff5cd7fca151c20254a368ba52aee7053926
dbarkowsky <dylanbarkowsky@gmail.com>
2024-10-09 12:03:28 -0700

    change mockSelf behaviour

--- e2e/functions/mockRequests.ts
+++ e2e/functions/mockRequests.ts
@@ -58,8 +58,14 @@ const createSelfResponse = (props?: Partial<User>): User => ({
  * @param status An optional status number. Defaults to 200.
  */
 export const mockSelf = async (page: Page, self?: Partial<User>, status: number = 200) => {
-  await page.route('**/users/self', route => route.fulfill({
-    status,
-    body: JSON.stringify(createSelfResponse(self)),
-  }))
+  // Make the actual call, but then edit it with any partial user passed in before it gets back to the page
+  await page.route('**/users/self', async route => {
+    const response = await route.fetch();
+    let json = await response.json();
+    json = {
+      ...json,
+      ...self
+    }
+    await route.fulfill({ response, json });
+  })
 };
