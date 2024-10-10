1fe9955f27d9a42a1f61e06460deaf29fadcec9d
dbarkowsky <dylanbarkowsky@gmail.com>
2024-10-10 12:02:32 -0700

    add some agency table tests

--- e2e/functions/mockRequests.ts
+++ e2e/functions/mockRequests.ts
@@ -23,34 +23,6 @@ interface User {
 }
 
 /**
- * For creating a user object normally returned by the /self route
- * @param props A Partial user object
- * @returns 
- */
-const createSelfResponse = (props?: Partial<User>): User => ({
-  "CreatedById": "00000000-0000-0000-0000-000000000000",
-  "CreatedOn": "2023-07-25T00:32:41.938Z",
-  "UpdatedById": "a619c656-b263-4a17-9b87-03459f43b349",
-  "UpdatedOn": "2024-10-09T23:49:47.650Z",
-  "Id": "a619c656-b263-4a17-9b87-03459f43b352",
-  "Username": "a619c656b2634a179b8703459f43b529@idir",
-  "FirstName": "Joe",
-  "MiddleName": null,
-  "LastName": "Tester",
-  "Email": "user.email@gov.bc.ca",
-  "Position": "Developer",
-  "Note": null,
-  "LastLogin": "2024-10-09T16:49:47.629Z",
-  "ApprovedById": "80e0111a-b56c-48dd-b55e-f99f2205c4bd",
-  "ApprovedOn": "2023-08-26T03:03:47.911Z",
-  "KeycloakUserId": "a619c656-b263-4a17-9b87-03459f43b352",
-  "AgencyId": 2,
-  "RoleId": "00000000-0000-0000-0000-000000000000",
-  "Status": "Active",
-  ...props,
-})
-
-/**
  * Use this at the start of a test to set your user information if needed.
  * Otherwise, your user information will reflect the actual user credentials used in testing.
  * @param page Page of e2e test.
---
+++ e2e/tests/agencies.spec.ts
@@ -0,0 +1,62 @@
+import { test, expect, Page } from '@playwright/test'
+import { BASE_URL } from '../env';
+import { loginIDIR } from '../functions/login';
+import { mockSelf } from '../functions/mockRequests';
+
+const getToAgencies = async (page: Page) => {
+  await page.goto(BASE_URL);
+  await mockSelf(page, { RoleId: "00000000-0000-0000-0000-000000000000" }); // guarantee admin status
+  await loginIDIR({ page });
+
+  await page.getByRole('heading', { name: 'Administration' }).click();
+  await page.getByRole('menuitem', { name: 'Agencies' }).click();
+  await page.getByText('Agencies Overview');
+}
+
+test('user can navigate to agencies page as an admin', async ({ page }) => {
+  await getToAgencies(page);
+  await expect(page.getByText('Agencies Overview')).toBeVisible();
+})
+
+test('user can filter using the keyword search', async ({ page }) => {
+  await getToAgencies(page);
+
+  await page.getByTestId('SearchIcon').click();
+  await page.getByPlaceholder('Search...').click();
+  await page.getByPlaceholder('Search...').fill('real');
+  await expect(page.getByRole('gridcell', { name: 'Real Property Division' })).toBeVisible();
+  await page.getByLabel('Clear Filter').click();
+  await expect(page.getByPlaceholder('Search...')).toBeEmpty();
+  await expect(page.getByLabel('Clear Filter')).not.toBeVisible();
+})
+
+test('user can filter using the select dropdown', async ({ page }) => {
+  await getToAgencies(page);
+
+  await page.getByText('All Agencies').click();
+  await page.getByRole('option', { name: 'Disabled', exact: true }).click();
+  await expect(page.getByTestId('FilterAltIcon')).toBeVisible(); // The icon in the column header
+  await expect(page.getByLabel('Clear Filter')).toBeVisible();
+  await page.getByLabel('Clear Filter').click();
+  await expect(page.getByLabel('Clear Filter')).not.toBeVisible();
+})
+
+test('user can filter using the column filter', async ({ page }) => {
+  await getToAgencies(page);
+
+  await page.getByText('Name', { exact: true }).hover();
+  await page.getByRole('button', { name: 'Menu' }).click();
+  await page.getByRole('menuitem', { name: 'Filter' }).click();
+  await page.getByPlaceholder('Filter value').click();
+  await page.getByPlaceholder('Filter value').fill('real');
+  await page.getByText('Agencies Overview (1 rows)All').click(); // To get filter off screen
+  await expect(page.getByRole('gridcell', { name: 'Real Property Division' })).toBeVisible();
+})
+
+test('user select a row and go to that agency', async ({ page }) => {
+  await getToAgencies(page);
+
+  await page.getByRole('gridcell', { name: 'Advanced Education & Skills' }).first().click();
+  await expect(page.getByText('Agency Details')).toBeVisible();
+  await expect(page.getByText('Advanced Education & Skills')).toBeVisible();
+})
