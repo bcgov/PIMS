9320dd41506ac537372938f936ed6b8c82290ca6
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2024-10-21 09:52:40 -0700

    PIMS-1946 Cancel/Stop Notifications (#2726)
    Co-authored-by: LawrenceLau2020 <68400651+LawrenceLau2020@users.noreply.github.com>

--- express-api/src/constants/switches.ts
+++ express-api/src/constants/switches.ts
@@ -1,5 +1,5 @@
 const { NODE_ENV } = process.env;
 
 export default {
-  TESTING: NODE_ENV === 'test', // Can be used to disable certain features when testing
+  TESTING: NODE_ENV === 'test' || process.env.TESTING === 'true', // Can be used to disable certain features when testing
 };
