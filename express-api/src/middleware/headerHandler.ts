805e6e299dc371e078a490705c5f5d761872a302
Lawrence Lau <lawrence.lau@gov.bc.ca>
2024-10-31 14:40:09 -0700

    initial commit

--- express-api/src/middleware/headerHandler.ts
+++ express-api/src/middleware/headerHandler.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Response } from 'express';
 
 const headerHandler: unknown = (req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
   next();
