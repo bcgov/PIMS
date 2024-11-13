f573d93c09e9119ef7f91a64e2602eef86e8fa83
TaylorFries <78506153+TaylorFries@users.noreply.github.com>
2024-11-13 10:38:06 -0800

    PIMS-2130: Update Python Create Jira Tickets Script (#2772)
    Co-authored-by: Sharala-Perumal <80914899+Sharala-Perumal@users.noreply.github.com>

--- .github/helpers/npm-deps/create-report-issues.cjs
+++ .github/helpers/npm-deps/create-report-issues.cjs
@@ -1,4 +1,5 @@
-const path = require('path');
+// NOTE: these cant be imports as they are outside a module
+const path = require("path");
 const outputText = require(path.resolve(__dirname, `../../../outputText.json`));
 
 /**
@@ -6,20 +7,26 @@ const outputText = require(path.resolve(__dirname, `../../../outputText.json`));
  * Place within .github/helpers/npm-deps/
  */
 
-// Get package.json paths from env.
+// Get package.json paths and comment contents from env.
 const packageJsonPaths = JSON.parse(process.env.packageJsonPaths);
+const commentIn = JSON.parse(process.env.commentContents);
 
 (async () => {
-  const module = await import('../github-api/create-and-close-existing-issue.mjs');
-  const createAndCloseExistingIssue = module.default;
-  // Create an array of promises for each packageJsonPath.
-  const promises = packageJsonPaths.map(async (packagePath) => {
-    const issueTitle =
-      packagePath !== '.' ? `${packagePath} NPM Dependency Report` : 'NPM Dependency Report';
-    // Await the completion of create and close existing issue.
-    await createAndCloseExistingIssue(issueTitle, outputText[packagePath]);
-  });
+	const module = await import("../github-api/update-issue-and-comment.mjs");
+	const updateIssueAndComment = module.default;
+	// Create an array of promises for each packageJsonPath.
+	const promises = packageJsonPaths.map(async (packagePath) => {
+		// get the comment for this folder
+		const comment = commentIn[packagePath];
+		// create title
+		const issueTitle =
+			packagePath !== "."
+				? `${packagePath} NPM Dependency Report`
+				: "NPM Dependency Report";
 
-  // Wait for all issues to be created.
-  await Promise.all(promises);
+		await updateIssueAndComment(issueTitle, outputText[packagePath], comment);
+	});
+
+	// Wait for all issues to be created.
+	await Promise.all(promises);
 })();
