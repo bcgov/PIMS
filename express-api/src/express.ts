2b03f7ca5c1e10af3c88ec8db1f84dd14962a06f
dbarkowsky <dylanbarkowsky@gmail.com>
2025-01-30 15:47:02 -0800

    Express v4 Rollback

--- express-api/src/express.ts
+++ express-api/src/express.ts
@@ -17,7 +17,6 @@ import nunjucks from 'nunjucks';
 import OPENAPI_OPTIONS from '@/swagger/swaggerConfig';
 import userAuthCheck from '@/middleware/userAuthCheck';
 import { Roles } from '@/constants/roles';
-import { PathParams } from 'express-serve-static-core';
 
 const app: Application = express();
 
@@ -111,11 +110,9 @@ app.use(`/v2/reports`, protectedRoute(), userAuthCheck(), router.reportsRouter);
 app.use(`/v2/tools`, protectedRoute(), router.toolsRouter);
 
 // If a non-existent route is called. Must go after other routes.
-// As of Express v5, *splat replaces the wildcard *.
-app.use('/*splat', (_req, _res, next) => next(EndpointNotFound404));
+app.use('*', (_req, _res, next) => next(EndpointNotFound404));
 
 // Request error handler. Must go last.
-// As of Express v5, the typing of this argument has changed but functionality has not.
-app.use(errorHandler as unknown as PathParams);
+app.use(errorHandler);
 
 export default app;
