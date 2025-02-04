52f5db6c3a65e9b4a715c83489f6e4b9866e1128
Dylan Barkowsky <37922247+dbarkowsky@users.noreply.github.com>
2025-02-04 13:05:52 -0800

    PIMS-2270 Fiscal Years and Risk (#2905)

--- react-app/src/components/projects/ProjectForms.tsx
+++ react-app/src/components/projects/ProjectForms.tsx
@@ -1,4 +1,4 @@
-import { Grid, InputAdornment, Typography } from '@mui/material';
+import { Box, Grid, InputAdornment, Tooltip, Typography } from '@mui/material';
 import React, { useContext } from 'react';
 import AutocompleteFormField from '../form/AutocompleteFormField';
 import { UserContext } from '@/contexts/userContext';
@@ -7,6 +7,10 @@ import { ISelectMenuItem } from '../form/SelectFormField';
 import SingleSelectBoxFormField from '../form/SingleSelectBoxFormField';
 import { LookupContext } from '@/contexts/lookupContext';
 import { Roles } from '@/constants/roles';
+import { useFormContext } from 'react-hook-form';
+import { formatFiscalYear } from '@/utilities/formatters';
+import { generateNumberList } from '@/utilities/helperFunctions';
+import Help from '@mui/icons-material/Help';
 
 interface IProjectGeneralInfoForm {
   projectStatuses: ISelectMenuItem[];
@@ -16,6 +20,7 @@ export const ProjectGeneralInfoForm = (props: IProjectGeneralInfoForm) => {
   const { data: lookupData } = useContext(LookupContext);
   const { pimsUser } = useContext(UserContext);
   const canEdit = pimsUser.hasOneOfRoles([Roles.ADMIN]);
+  const { getValues } = useFormContext();
 
   return (
     <Grid mt={'1rem'} spacing={2} container>
@@ -57,6 +62,66 @@ export const ProjectGeneralInfoForm = (props: IProjectGeneralInfoForm) => {
       <Grid item xs={12}>
         <TextFormField fullWidth multiline name={'Description'} label={'Description'} minRows={3} />
       </Grid>
+      {canEdit && (
+        <>
+          <Grid item xs={12}>
+            <AutocompleteFormField
+              name={'AgencyId'}
+              label={'Agency'}
+              options={
+                lookupData?.Agencies.map((agc) => ({ value: agc.Id, label: agc.Name })) ?? []
+              }
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <AutocompleteFormField
+              fullWidth
+              name={'ReportedFiscalYear'}
+              label={'Reported Fiscal Year'}
+              disabled
+              options={[getValues()['ReportedFiscalYear']].map((year) => ({
+                value: year,
+                label: formatFiscalYear(year),
+              }))}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <AutocompleteFormField
+              fullWidth
+              name={'ActualFiscalYear'}
+              label={'Fiscal Year of Disposal'}
+              options={generateNumberList(
+                (getValues()['ActualFiscalYear'] ?? new Date().getUTCFullYear()) - 1,
+                new Date().getUTCFullYear() + 1,
+              ).map((year) => ({
+                value: year,
+                label: formatFiscalYear(year),
+              }))}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <AutocompleteFormField
+              name={'RiskId'}
+              label={
+                <Box display={'inline-flex'} alignItems={'center'}>
+                  Risk Level
+                  <Tooltip title="The risk associated with completion of the sale of a property during the forecasted fiscal year. Risk status on property sales can change through the sales process.">
+                    <Help sx={{ ml: '4px' }} fontSize="small" />
+                  </Tooltip>
+                </Box>
+              }
+              required
+              options={
+                lookupData?.Risks.map((risk) => ({
+                  value: risk.Id,
+                  label: risk.Name,
+                  tooltip: risk.Description,
+                })) ?? []
+              }
+            />
+          </Grid>
+        </>
+      )}
     </Grid>
   );
 };
